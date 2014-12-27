bb70e1ac9db4e36de2cb6c950c4f3a4b01e91859
Kentaro Hatori <hatori@hatochan.dyndns.org>
2014-12-27 19:39:47 +0900

    Update lxqt-config-cursor_ja.ts
    I translate remaining Japanese words ａｎｄ sentences.

--- liblxqt-config-cursor/translations/lxqt-config-cursor_ja.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_ja.ts
@@ -3,46 +3,46 @@
     <name>SelectWnd</name>
     <message>
         <source>LXQt Mouse Theme Configuration</source>
-        <translation type="unfinished"/>
+        <translation>LXQtマウステーマの設定</translation>
     </message>
     <message>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation type="unfinished"/>
+        <translation>使いたいカーソルテーマを選択 (プレビューをテストカーソルに合わせます). &lt;b&gt;LXQtセッションはこの変更後に再起動が必要です&lt;/b&gt;:</translation>
     </message>
     <message>
         <source>&amp;Install New Theme...</source>
-        <translation type="unfinished"/>
+        <translation>&amp;新しいテーマのインストール...</translation>
     </message>
     <message>
         <source>&amp;Set Theme</source>
-        <translation type="unfinished"/>
+        <translation>&amp;テーマの設定</translation>
     </message>
     <message>
         <source>&amp;Remove Theme</source>
-        <translation type="unfinished"/>
+        <translation>テーマの削除</translation>
     </message>
     <message>
         <source>&amp;Close</source>
-        <translation>閉める(&amp;C)</translation>
+        <translation>閉じる(&amp;C)</translation>
     </message>
     <message>
         <source>XCurTheme error</source>
-        <translation type="unfinished"/>
+        <translation>XCurテーマ・エラー</translation>
     </message>
     <message>
         <source>You can&apos;t remove active theme!</source>
-        <translation type="unfinished"/>
+        <translation>アクティブなテーマは削除できません!</translation>
     </message>
 </context>
 <context>
     <name>XCursorThemeModel</name>
     <message>
         <source>Name</source>
-        <translation type="unfinished"/>
+        <translation>名称</translation>
     </message>
     <message>
         <source>Description</source>
-        <translation type="unfinished"/>
+        <translation>説明</translation>
     </message>
 </context>
 </TS>
