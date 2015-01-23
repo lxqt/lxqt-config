3000c59960e48daaaa2fb1182b33898f15c6e32f
Thomas Viehweger <ThomasVie@users.noreply.github.com>
2015-01-23 23:18:09 +0100

    Added german translation for monitor settings.

---
+++ lxqt-config-monitor/translations/lxqt-config-monitor_de.ts
@@ -0,0 +1,220 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>MonitorInfo</name>
+    <message>
+        <location filename="../monitor.cpp" line="54"/>
+        <source>Laptop LCD Monitor</source>
+        <translation>Laptop-Bildschirm</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="56"/>
+        <source>External VGA Monitor</source>
+        <translation>Externer VGA-Bildschirm</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="56"/>
+        <source>VGA Monitor</source>
+        <translation>VGA-Bildschirm</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="58"/>
+        <source>External DVI Monitor</source>
+        <translation>Externer DVI-Bildschirm</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="58"/>
+        <source>DVI Monitor</source>
+        <translation>DVI-Bildschirm</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="60"/>
+        <source>TV</source>
+        <translation>Fernseher</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="62"/>
+        <source>Default Monitor</source>
+        <translation>Standardbildschirm</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorPictureDialog</name>
+    <message>
+        <location filename="../monitorpicture.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../monitorpicture.ui" line="27"/>
+        <source>Magnetic attraction</source>
+        <translation>Magnetische Anziehungskraft</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorSettingsDialog</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>Bildschirmeinstellungen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="29"/>
+        <source>Quick Options</source>
+        <translation>Schnellwahl</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="35"/>
+        <source>Show the same screen on both laptop LCD and external monitor</source>
+        <translation>Gleiches Bild auf Laptop und externem Bildschirm anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="42"/>
+        <source>Extended view</source>
+        <translation>Erweiterter Bildschirm</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="49"/>
+        <source>Turn off laptop LCD and use external monitor only</source>
+        <translation>Laptop-Bildschirm ausschalten und nur den externen Monitor verwenden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="56"/>
+        <source>Turn off external monitor and use laptop LCD only</source>
+        <translation>Externen Monitor ausschalten und nur den Laptop-Bildschirm verwenden</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="77"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="112"/>
+        <source>Unify all monitors</source>
+        <translation>Alle Bildschirme gleich behandeln</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="119"/>
+        <source>Monitor Position</source>
+        <translation>Bildschirmposition</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="139"/>
+        <source>Primary monitor:</source>
+        <translation>Hauptbildschirm:</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="266"/>
+        <source>Do you want to save changes?</source>
+        <translation>Einstellungen sichern?</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="267"/>
+        <source>Please, check your config before to save.</source>
+        <translation>Bitte Konfiguration vor dem Sichern überprüfen.</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="293"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="293"/>
+        <source>Config can not be saved</source>
+        <translation>Konfiguration kann nicht gespeichert werden</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorWidget</name>
+    <message>
+        <location filename="../monitorwidget.ui" line="23"/>
+        <source>Turn on</source>
+        <translation>Einschalten</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="30"/>
+        <source>Resolution:</source>
+        <translation>Auflösung:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="40"/>
+        <source>Rate:</source>
+        <translation>Bildwiederholfrequenz:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="58"/>
+        <source>Hz</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="67"/>
+        <source>Position:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="79"/>
+        <source>x:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="96"/>
+        <source>y:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="115"/>
+        <source>Brightness:</source>
+        <translation>Helligkeit:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="135"/>
+        <source>Gamma:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="154"/>
+        <location filename="../monitorwidget.ui" line="171"/>
+        <source>:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="193"/>
+        <source>Backlight:</source>
+        <translation>Hintergrundbeleuchtung:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="46"/>
+        <location filename="../monitorwidget.cpp" line="97"/>
+        <source>Auto</source>
+        <translation type="unfinished">Automatisch</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../xrandr.cpp" line="238"/>
+        <location filename="../xrandr.cpp" line="243"/>
+        <source>Auto</source>
+        <translation>Automatisch</translation>
+    </message>
+</context>
+<context>
+    <name>TimeoutDialog</name>
+    <message>
+        <location filename="../timeoutdialog.ui" line="14"/>
+        <source>Settings are changed</source>
+        <translation>Einstellungen wurden geändert</translation>
+    </message>
+    <message>
+        <location filename="../timeoutdialog.ui" line="23"/>
+        <source>Are the current settings OK for you?</source>
+        <translation>Sind die Einstellungen korrekt?</translation>
+    </message>
+    <message>
+        <location filename="../timeoutdialog.cpp" line="52"/>
+        <source>%1 second(s) remaining</source>
+        <translation>%1 Sekunde(n) verbleiben</translation>
+    </message>
+</context>
+</TS>
