01fd1aed979999fce481bc82cd472ab11d61d747
Stef <stef.binde@riseup.net>
2015-01-25 12:31:56 +0100

    Update lxqt-config-cursor_lt.ts
    updated italian translation

--- liblxqt-config-cursor/translations/lxqt-config-cursor_lt.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_lt.ts
@@ -1,41 +1,47 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="lt">
+<TS version="2.1" language="it_IT">
 <context>
     <name>SelectWnd</name>
     <message>
         <location filename="../ui/selectwnd.ui" line="14"/>
         <source>LXQt Mouse Theme Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurazione del aspetto del cursore</translation>
     </message>
     <message>
         <location filename="../ui/selectwnd.ui" line="20"/>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>hover seems not working, and restart seems not necessary to take effect</translatorcomment>
+        <translation type="unfinished">Seleziona il tema del cursore che vuoi utilizzare </translation>
     </message>
     <message>
         <location filename="../ui/selectwnd.ui" line="81"/>
         <source>&amp;Install New Theme...</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>&amp; to remove in source</translatorcomment>
+        <translation type="unfinished">Installa un nuovo tema...</translation>
+    </message>
+    <message>
+        <source>&amp;Set Theme</source>
+        <translation type="vanished">&amp;Imposta il tema</translation>
     </message>
     <message>
         <location filename="../ui/selectwnd.ui" line="88"/>
         <source>&amp;Remove Theme</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Rimuovi il tema</translation>
     </message>
     <message>
         <source>&amp;Close</source>
-        <translation type="vanished">&amp;Užverti</translation>
+        <translation type="vanished">&amp;Chiudi</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="208"/>
         <source>XCurTheme error</source>
-        <translation type="unfinished"></translation>
+        <translation>Errore tema X-cursore</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="209"/>
         <source>You can&apos;t remove active theme!</source>
-        <translation type="unfinished"></translation>
+        <translation>Non puoi rimuovere il tema attivo!</translation>
     </message>
 </context>
 <context>
@@ -48,12 +54,12 @@
     <message>
         <location filename="../ui/warninglabel.ui" line="44"/>
         <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt non ha trovato nessun tema per i cursori in queste directories: (Verrà usato il tema di default di X11):</translation>
     </message>
     <message>
         <location filename="../ui/warninglabel.ui" line="54"/>
         <source>Show...</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostra...</translation>
     </message>
 </context>
 <context>
@@ -61,12 +67,12 @@
     <message>
         <location filename="../thememodel.cpp" line="61"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nome</translation>
     </message>
     <message>
         <location filename="../thememodel.cpp" line="62"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Descrizione</translation>
     </message>
 </context>
 </TS>
