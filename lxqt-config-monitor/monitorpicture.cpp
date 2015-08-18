8116565720fdea29619afe5a6d2c9ad9194859ad
Alf Gaida <agaida@siduction.org>
2015-08-19 01:13:23 +0200

    fixes lxde/lxqt #785 AUTHORS changed, list the used licenses LXQt license header added

--- lxqt-config-monitor/monitorpicture.cpp
+++ lxqt-config-monitor/monitorpicture.cpp
@@ -1,3 +1,12 @@
+/*
+* Copyright (c) LXQt contributors.
+*
+* This file is part of the LXQt project. <http://lxqt.org>
+* It is distributed under the LGPL 2.1 or later license.
+* Please refer to the LICENSE file for a copy of the license, and
+* the AUTHORS file for copyright and authorship information.
+*/
+
 #include "monitorpicture.h"
 
 #include <QFont>
