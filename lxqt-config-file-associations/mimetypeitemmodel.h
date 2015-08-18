02fe04a4f2b307b02b99bd14c3d08d443d031eb3
Christian Surlykke <christian@surlykke.dk>
2015-08-18 07:19:54 +0200

    Add copyright headers

--- lxqt-config-file-associations/mimetypeitemmodel.h
+++ lxqt-config-file-associations/mimetypeitemmodel.h
@@ -1,9 +1,11 @@
 /*
- * File:   MimetypeItemModel.h
- * Author: christian
- *
- * Created on 5. maj 2013, 09:18
- */
+* Copyright (c) Christian Surlykke
+*
+* This file is part of the LXQt project. <http://lxqt.org>
+* It is distributed under the LGPL 2.1 or later license.
+* Please refer to the LICENSE file for a copy of the license, and
+* the AUTHORS file for copyright and authorship information.
+*/
 
 #ifndef MIMETYPEITEMMODEL_H
 #define	MIMETYPEITEMMODEL_H
