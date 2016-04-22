553d1d7931cd49294c2e30108a887eba6e68f2d6
Ilya87 <yast4ik@yahoo.com>
2016-04-22 19:12:49 +0300

    Russian translation update

---
+++ lxqt-config-brightness/translations/lxqt-config-brightness_ru.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>BrightnessSettings</name>
+    <message>
+        <location filename="../brightnesssettings.ui" line="14"/>
+        <source>Brightness Settings</source>
+        <translation>Настройки яркости</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="25"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Настройки подсветки и яркости:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Используйте клавиши стрелок для изменения настроек..&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>OutputWidget</name>
+    <message>
+        <location filename="../outputwidget.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../outputwidget.ui" line="38"/>
+        <source>&lt;b&gt;Output:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Вывод:&lt;/b&gt;</translation>
+    </message>
+</context>
+</TS>
