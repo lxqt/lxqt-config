12aaf7201893c5e1a8ebb2138bdac1535c69a0ae
Alf Gaida <agaida@siduction.org>
2016-05-10 18:13:41 +0200

    Moved all directories to lxqt-config

---
+++ lxqt-config-file-associations_de.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>ApplicationChooser</name>
+    <message>
+        <location filename="../applicationchooser.ui" line="14"/>
+        <source>ApplicationChooser</source>
+        <translation>Dateizuordnungen</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="46"/>
+        <source>Pick an application for:</source>
+        <translation>Anwendung auswählen für:</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="110"/>
+        <source>1</source>
+        <translation>1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="115"/>
+        <source>Applications that handle JPEG</source>
+        <translation>Anwendungen die JPEG verarbeiten</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="124"/>
+        <location filename="../applicationchooser.ui" line="129"/>
+        <location filename="../applicationchooser.ui" line="139"/>
+        <location filename="../applicationchooser.ui" line="144"/>
+        <source>New Subitem</source>
+        <translation>Neuer Unterpunkt</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="135"/>
+        <location filename="../applicationchooser.cpp" line="113"/>
+        <source>Other applications</source>
+        <translation>Andere Anwendung</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="114"/>
+        <source>Applications that handle %1</source>
+        <translation>Anwendungen die %1 verarbeiten</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="151"/>
+        <source>No applications found</source>
+        <translation>Keine Anwendung gefunden</translation>
+    </message>
+</context>
+<context>
+    <name>MimetypeViewer</name>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="157"/>
+        <location filename="../mimetypeviewer.cpp" line="208"/>
+        <source>None</source>
+        <translation>Keine</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="204"/>
+        <source>&amp;Change...</source>
+        <translation>Ä&amp;ndern...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="209"/>
+        <source>&amp;Choose...</source>
+        <translation>&amp;Auswählen...</translation>
+    </message>
+</context>
+<context>
+    <name>mimetypeviewer</name>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="20"/>
+        <source>File Associations</source>
+        <translation>Dateizuordnungen</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="56"/>
+        <source>Default application</source>
+        <translation>Default-Anwendungsprogramm</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="68"/>
+        <source>&amp;Choose...</source>
+        <translation>&amp;Auswählen...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="117"/>
+        <source>TextLabel</source>
+        <translation>Textfeld</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="124"/>
+        <source>None</source>
+        <translation>Keins</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="144"/>
+        <source>Patterns</source>
+        <translation>Dateimuster</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="156"/>
+        <source>*.txt *.xml</source>
+        <translation>*.txt *.xml</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="173"/>
+        <source>MIME type</source>
+        <translation>MIME-Typ</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="191"/>
+        <source>Icon</source>
+        <translation>Symbol</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="198"/>
+        <source>Description</source>
+        <translation>Beschreibung</translation>
+    </message>
+</context>
+</TS>
