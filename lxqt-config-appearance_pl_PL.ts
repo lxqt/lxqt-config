e82bcd7f214f38259368d882384a6f1d47363a26
Alf Gaida <agaida@siduction.org>
2016-05-10 18:13:41 +0200

    Moved all directories to lxqt-config

---
+++ lxqt-config-appearance_pl_PL.ts
@@ -0,0 +1,225 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>FontsConfig</name>
+    <message>
+        <location filename="../fontsconfig.ui" line="23"/>
+        <source>Font</source>
+        <translation>Czcionka</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="30"/>
+        <source>Default font for user interface</source>
+        <translation>Domyślna czcionka dla interfejsu użytkownika</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="36"/>
+        <source>Font name:</source>
+        <translation>Czcionka:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="46"/>
+        <source>Style:</source>
+        <translation>Styl:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="53"/>
+        <source>Point size:</source>
+        <translation>Rozmiar:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="68"/>
+        <source>Normal</source>
+        <translation>Normalna</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="73"/>
+        <source>Bold</source>
+        <translation>Pogrubiona</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="78"/>
+        <source>Italic</source>
+        <translation>Kursywa</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="83"/>
+        <source>Bold Italic</source>
+        <translation>Pogrubiona kursywa</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="94"/>
+        <source>The following settings only affect newly started applications</source>
+        <translation>Poniższe ustawienia dotyczą tylko nowo otwartych aplikacji</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="100"/>
+        <source>Use antialias fonts</source>
+        <translation>Wygładzanie czcionek (antyaliasing)</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="107"/>
+        <source>Font hinting style:</source>
+        <translation>Styl hintingu:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="118"/>
+        <location filename="../fontsconfig.ui" line="183"/>
+        <source>None</source>
+        <translation>Brak</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="123"/>
+        <source>Slight</source>
+        <translation>Delikatny</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="128"/>
+        <source>Medium</source>
+        <translation>Średni</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="133"/>
+        <source>Full</source>
+        <translation>Pełny</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="141"/>
+        <source>Font hinting</source>
+        <translation>Hinting czcionek</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="148"/>
+        <source>Resolution (DPI):</source>
+        <translation>Rozdzielczość (DPI):</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="155"/>
+        <source>Autohint</source>
+        <translation>Autohinting</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="172"/>
+        <source>Subpixel antialiasing:</source>
+        <translation>Antyaliasing subpikselowy:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="188"/>
+        <source>RGB</source>
+        <translation>RGB</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="193"/>
+        <source>BGR</source>
+        <translation>BGR</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="198"/>
+        <source>VRGB</source>
+        <translation>VRGB</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="203"/>
+        <source>VBGR</source>
+        <translation>VBGR</translation>
+    </message>
+</context>
+<context>
+    <name>IconThemeConfig</name>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="14"/>
+        <source>LXQt Appearance Configuration</source>
+        <translation>Konfiguracja wyglądu LXQt</translation>
+    </message>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="26"/>
+        <source>Icons Theme</source>
+        <translation>Motyw ikon</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtThemeConfig</name>
+    <message>
+        <location filename="../lxqtthemeconfig.ui" line="23"/>
+        <source>LXQt Theme</source>
+        <translation>Motyw LXQt</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="46"/>
+        <source>LXQt Appearance Configuration</source>
+        <translation>Konfiguracja wyglądu LXQt</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="52"/>
+        <source>Widget Style</source>
+        <translation>Widżety</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="56"/>
+        <source>Icons Theme</source>
+        <translation>Motyw ikon</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="60"/>
+        <source>LXQt Theme</source>
+        <translation>Motyw LXQt</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="64"/>
+        <source>Font</source>
+        <translation>Czcionka</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>Cursor</source>
+        <translation>Kursor</translation>
+    </message>
+</context>
+<context>
+    <name>StyleConfig</name>
+    <message>
+        <location filename="../styleconfig.ui" line="23"/>
+        <source>Widget Style</source>
+        <translation>Styl widżetów</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="54"/>
+        <source>Toolbar button style:</source>
+        <translation>Styl paska narzędziowego:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="62"/>
+        <source>Only display the icon</source>
+        <translation>Tylko ikony</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="67"/>
+        <source>Only display the text</source>
+        <translation>Tylko tekst</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="72"/>
+        <source>The text appears beside the icon</source>
+        <translation>Tekst obok ikon</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="77"/>
+        <source>The text appears under the icon</source>
+        <translation>Tekst poniżej ikon</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="82"/>
+        <source>Default</source>
+        <translation>Domyśłny</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="90"/>
+        <source>Activate item on single click</source>
+        <translation>Aktywacja pojedyńczym kliknięciem</translation>
+    </message>
+</context>
+</TS>
