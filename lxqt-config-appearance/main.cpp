c7f75e39948318a149fa5a4878abbeffc5d86d44
Palo Kisa <palo.kisa@gmail.com>
2016-05-20 11:18:36 +0200

    lxqt-config-appearance: Remove includes

--- lxqt-config-appearance/main.cpp
+++ lxqt-config-appearance/main.cpp
@@ -26,9 +26,7 @@
  * END_COMMON_COPYRIGHT_HEADER */
 
 #include <LXQt/SingleApplication>
-#include <QDebug>
 
-#include <XdgIcon>
 #include <LXQt/Settings>
 #include <LXQt/ConfigDialog>
 #include "iconthemeconfig.h"
