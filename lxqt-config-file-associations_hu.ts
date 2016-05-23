e1de016a869a2f8b53ae2271dd0b5db8742e09dd
Palo Kisa <palo.kisa@gmail.com>
2016-05-23 10:48:38 +0200

    Update all translations to current sources

--- lxqt-config-file-associations_hu.ts
+++ lxqt-config-file-associations_hu.ts
@@ -4,46 +4,46 @@
 <context>
     <name>ApplicationChooser</name>
     <message>
-        <location filename="../applicationchooser.ui" line="14"/>
+        <location filename="../../../applicationchooser.ui" line="14"/>
         <source>ApplicationChooser</source>
         <translation>Alkalmazás választó</translation>
     </message>
     <message>
-        <location filename="../applicationchooser.ui" line="46"/>
+        <location filename="../../../applicationchooser.ui" line="46"/>
         <source>Pick an application for:</source>
         <translation>Válassz alkalmazást ehhez:</translation>
     </message>
     <message>
-        <location filename="../applicationchooser.ui" line="110"/>
+        <location filename="../../../applicationchooser.ui" line="110"/>
         <source>1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../applicationchooser.ui" line="115"/>
+        <location filename="../../../applicationchooser.ui" line="115"/>
         <source>Applications that handle JPEG</source>
         <translation>JPEG kezelő alkalmazás</translation>
     </message>
     <message>
-        <location filename="../applicationchooser.ui" line="124"/>
-        <location filename="../applicationchooser.ui" line="129"/>
-        <location filename="../applicationchooser.ui" line="139"/>
-        <location filename="../applicationchooser.ui" line="144"/>
+        <location filename="../../../applicationchooser.ui" line="124"/>
+        <location filename="../../../applicationchooser.ui" line="129"/>
+        <location filename="../../../applicationchooser.ui" line="139"/>
+        <location filename="../../../applicationchooser.ui" line="144"/>
         <source>New Subitem</source>
         <translation>ÚJ alelem</translation>
     </message>
     <message>
-        <location filename="../applicationchooser.ui" line="135"/>
-        <location filename="../applicationchooser.cpp" line="113"/>
+        <location filename="../../../applicationchooser.ui" line="135"/>
+        <location filename="../../../applicationchooser.cpp" line="113"/>
         <source>Other applications</source>
         <translation>Más alkalmazás</translation>
     </message>
     <message>
-        <location filename="../applicationchooser.cpp" line="114"/>
+        <location filename="../../../applicationchooser.cpp" line="114"/>
         <source>Applications that handle %1</source>
         <translation>A %1 kezelő alkalmazás</translation>
     </message>
     <message>
-        <location filename="../applicationchooser.cpp" line="151"/>
+        <location filename="../../../applicationchooser.cpp" line="151"/>
         <source>No applications found</source>
         <translation>Nincs alkalmazás</translation>
     </message>
@@ -51,18 +51,18 @@
 <context>
     <name>MimetypeViewer</name>
     <message>
-        <location filename="../mimetypeviewer.cpp" line="157"/>
-        <location filename="../mimetypeviewer.cpp" line="208"/>
+        <location filename="../../../mimetypeviewer.cpp" line="157"/>
+        <location filename="../../../mimetypeviewer.cpp" line="208"/>
         <source>None</source>
         <translation>Nincs</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.cpp" line="204"/>
+        <location filename="../../../mimetypeviewer.cpp" line="204"/>
         <source>&amp;Change...</source>
         <translation>&amp;Változtat...</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.cpp" line="209"/>
+        <location filename="../../../mimetypeviewer.cpp" line="209"/>
         <source>&amp;Choose...</source>
         <translation>&amp;Választás...</translation>
     </message>
@@ -70,52 +70,52 @@
 <context>
     <name>mimetypeviewer</name>
     <message>
-        <location filename="../mimetypeviewer.ui" line="20"/>
+        <location filename="../../../mimetypeviewer.ui" line="20"/>
         <source>File Associations</source>
         <translation>Fájlhozzárendelések</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.ui" line="56"/>
+        <location filename="../../../mimetypeviewer.ui" line="56"/>
         <source>Default application</source>
         <translation>Alapértelmezett alkalmazás</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.ui" line="68"/>
+        <location filename="../../../mimetypeviewer.ui" line="68"/>
         <source>&amp;Choose...</source>
         <translation>&amp;Választás...</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.ui" line="117"/>
+        <location filename="../../../mimetypeviewer.ui" line="117"/>
         <source>TextLabel</source>
         <translation>Felirat</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.ui" line="124"/>
+        <location filename="../../../mimetypeviewer.ui" line="124"/>
         <source>None</source>
         <translation>Nincs</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.ui" line="144"/>
+        <location filename="../../../mimetypeviewer.ui" line="144"/>
         <source>Patterns</source>
         <translation>Szűrők</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.ui" line="156"/>
+        <location filename="../../../mimetypeviewer.ui" line="156"/>
         <source>*.txt *.xml</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.ui" line="173"/>
+        <location filename="../../../mimetypeviewer.ui" line="173"/>
         <source>MIME type</source>
         <translation>MIME típus</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.ui" line="191"/>
+        <location filename="../../../mimetypeviewer.ui" line="191"/>
         <source>Icon</source>
         <translation>Ikon</translation>
     </message>
     <message>
-        <location filename="../mimetypeviewer.ui" line="198"/>
+        <location filename="../../../mimetypeviewer.ui" line="198"/>
         <source>Description</source>
         <translation>Leírás</translation>
     </message>
