81485d1dc52aba406e676d2b3a53dcd26fb8183f
Palo Kisa <palo.kisa@gmail.com>
2016-05-23 10:48:38 +0200

    Update all translations to current sources

--- lxqt-config-locale.ts
+++ lxqt-config-locale.ts
@@ -4,92 +4,92 @@
 <context>
     <name>LocaleConfig</name>
     <message>
-        <location filename="../localeconfig.ui" line="47"/>
+        <location filename="../../../localeconfig.ui" line="47"/>
         <source>Re&amp;gion:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="76"/>
+        <location filename="../../../localeconfig.ui" line="76"/>
         <source>De&amp;tailed Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="83"/>
+        <location filename="../../../localeconfig.ui" line="83"/>
         <source>&amp;Numbers:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="112"/>
+        <location filename="../../../localeconfig.ui" line="112"/>
         <source>&amp;Time:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="141"/>
+        <location filename="../../../localeconfig.ui" line="141"/>
         <source>Currenc&amp;y:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="170"/>
+        <location filename="../../../localeconfig.ui" line="170"/>
         <source>Measurement &amp;Units:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="199"/>
+        <location filename="../../../localeconfig.ui" line="199"/>
         <source>Co&amp;llation and Sorting:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="238"/>
+        <location filename="../../../localeconfig.ui" line="238"/>
         <source>&lt;b&gt;Examples&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="248"/>
+        <location filename="../../../localeconfig.ui" line="248"/>
         <source>Numbers:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="268"/>
+        <location filename="../../../localeconfig.ui" line="268"/>
         <source>Time:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="288"/>
+        <location filename="../../../localeconfig.ui" line="288"/>
         <source>Currency:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.ui" line="308"/>
+        <location filename="../../../localeconfig.ui" line="308"/>
         <source>Measurement Units:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.cpp" line="119"/>
+        <location filename="../../../localeconfig.cpp" line="119"/>
         <source>No change</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.cpp" line="308"/>
+        <location filename="../../../localeconfig.cpp" line="308"/>
         <source>Format Settings Changed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.cpp" line="309"/>
+        <location filename="../../../localeconfig.cpp" line="309"/>
         <source>Save the settings ? (they will take effect the next time you log in)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.cpp" line="430"/>
+        <location filename="../../../localeconfig.cpp" line="430"/>
         <source>Imperial UK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.cpp" line="434"/>
+        <location filename="../../../localeconfig.cpp" line="434"/>
         <source>Imperial US</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../localeconfig.cpp" line="438"/>
+        <location filename="../../../localeconfig.cpp" line="438"/>
         <source>Metric</source>
         <translation type="unfinished"></translation>
     </message>
@@ -97,12 +97,12 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="38"/>
+        <location filename="../../../main.cpp" line="38"/>
         <source>LXQt Locale Configuration</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="43"/>
+        <location filename="../../../main.cpp" line="43"/>
         <source>Locale Settings</source>
         <translation type="unfinished"></translation>
     </message>
