f383467e827f5078a92916d2fa9e6423f15caacb
Palo Kisa <palo.kisa@gmail.com>
2016-05-23 10:48:38 +0200

    Update all translations to current sources

--- lxqt-config-brightness.ts
+++ lxqt-config-brightness.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU">
+<TS version="2.1">
 <context>
     <name>BrightnessSettings</name>
     <message>
         <location filename="../../../brightnesssettings.ui" line="14"/>
         <source>Brightness Settings</source>
-        <translation>Настройки яркости</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../../brightnesssettings.ui" line="25"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Настройки подсветки и яркости:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Используйте клавиши стрелок для изменения настроек..&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -24,7 +24,7 @@
     <message>
         <location filename="../../../outputwidget.ui" line="38"/>
         <source>&lt;b&gt;Output:&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Вывод:&lt;/b&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
