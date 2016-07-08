4bd2903fa8e7a78e461cfd61c150d9cc4d18657a
safaalfulaij <safa1996alfulaij@gmail.com>
2016-07-08 17:33:08 +0300

    Update Arabic translations

--- lxqt-config-appearance_ar.ts
+++ lxqt-config-appearance_ar.ts
@@ -6,123 +6,123 @@
     <message>
         <location filename="../../../fontsconfig.ui" line="23"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>الخطّ</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="30"/>
         <source>Default font for user interface</source>
-        <translation type="unfinished"></translation>
+        <translation>خطّ واجهة المستخدم الافتراضيّ</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="36"/>
         <source>Font name:</source>
-        <translation type="unfinished"></translation>
+        <translation>اسم الخطّ:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="46"/>
         <source>Style:</source>
-        <translation type="unfinished"></translation>
+        <translation>النّمط:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="53"/>
         <source>Point size:</source>
-        <translation type="unfinished"></translation>
+        <translation>حجم النّقطة:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="68"/>
         <source>Normal</source>
-        <translation type="unfinished"></translation>
+        <translation>عاديّ</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="73"/>
         <source>Bold</source>
-        <translation type="unfinished"></translation>
+        <translation>ثخين</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="78"/>
         <source>Italic</source>
-        <translation type="unfinished"></translation>
+        <translation>مائل</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="83"/>
         <source>Bold Italic</source>
-        <translation type="unfinished"></translation>
+        <translation>ثخين مائل</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="94"/>
         <source>The following settings only affect newly started applications</source>
-        <translation type="unfinished"></translation>
+        <translation>ستؤثّر التّعديلات الآتية على التّطبيقات التي تبدأ بعد تطبيقها</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="100"/>
         <source>Use antialias fonts</source>
-        <translation type="unfinished"></translation>
+        <translation>استخدم خطوطًا محسّنة حوافها</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="107"/>
         <source>Font hinting style:</source>
-        <translation type="unfinished"></translation>
+        <translation>نمط تلميحات الخطوط:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="118"/>
         <location filename="../../../fontsconfig.ui" line="183"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>بلا</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="123"/>
         <source>Slight</source>
-        <translation type="unfinished"></translation>
+        <translation>قليل</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="128"/>
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>وسط</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="133"/>
         <source>Full</source>
-        <translation type="unfinished"></translation>
+        <translation>كامل</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="141"/>
         <source>Font hinting</source>
-        <translation type="unfinished"></translation>
+        <translation>تلميحات الخطوط</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="148"/>
         <source>Resolution (DPI):</source>
-        <translation type="unfinished"></translation>
+        <translation>الميز (نقطة/بوصة):</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="155"/>
         <source>Autohint</source>
-        <translation type="unfinished"></translation>
+        <translation>التلميحات آليّة</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="172"/>
         <source>Subpixel antialiasing:</source>
-        <translation type="unfinished"></translation>
+        <translation>مضادّ تعرّج البكسلات الفرعيّة:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="188"/>
         <source>RGB</source>
-        <translation type="unfinished"></translation>
+        <translation>RGB</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="193"/>
         <source>BGR</source>
-        <translation type="unfinished"></translation>
+        <translation>BGR</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="198"/>
         <source>VRGB</source>
-        <translation type="unfinished"></translation>
+        <translation>VRGB</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="203"/>
         <source>VBGR</source>
-        <translation type="unfinished"></translation>
+        <translation>VBGR</translation>
     </message>
 </context>
 <context>
@@ -130,12 +130,12 @@
     <message>
         <location filename="../../../iconthemeconfig.ui" line="14"/>
         <source>LXQt Appearance Configuration</source>
-        <translation>تهيئة مظهر ريزر</translation>
+        <translation>ضبط لكسكيوت للمظهر</translation>
     </message>
     <message>
         <location filename="../../../iconthemeconfig.ui" line="26"/>
         <source>Icons Theme</source>
-        <translation>نماذج اﻷيقونات</translation>
+        <translation>سمة الأيقونات</translation>
     </message>
 </context>
 <context>
@@ -143,7 +143,7 @@
     <message>
         <location filename="../../../lxqtthemeconfig.ui" line="23"/>
         <source>LXQt Theme</source>
-        <translation>واجهة ريزر المخصَّصة</translation>
+        <translation>سمة لكسكيوت</translation>
     </message>
 </context>
 <context>
@@ -151,32 +151,32 @@
     <message>
         <location filename="../../../main.cpp" line="44"/>
         <source>LXQt Appearance Configuration</source>
-        <translation>تهيئة مظهر ريزر</translation>
+        <translation>ضبط لكسكيوت للمظهر</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="50"/>
         <source>Widget Style</source>
-        <translation type="unfinished"></translation>
+        <translation>نمط الودجات</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="54"/>
         <source>Icons Theme</source>
-        <translation>نماذج اﻷيقونات</translation>
+        <translation>سمة الأيقونات</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="58"/>
         <source>LXQt Theme</source>
-        <translation>واجهة ريزر المخصَّصة</translation>
+        <translation>سمة لكسكيوت</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="62"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>الخطّ</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="67"/>
         <source>Cursor</source>
-        <translation type="unfinished"></translation>
+        <translation>المؤشّر</translation>
     </message>
 </context>
 <context>
@@ -184,42 +184,42 @@
     <message>
         <location filename="../../../styleconfig.ui" line="23"/>
         <source>Widget Style</source>
-        <translation type="unfinished"></translation>
+        <translation>نمط الودجات</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="54"/>
         <source>Toolbar button style:</source>
-        <translation type="unfinished"></translation>
+        <translation>نمط زرّ شريط الأدوات:</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="62"/>
         <source>Only display the icon</source>
-        <translation type="unfinished"></translation>
+        <translation>اعرض الأيقونة فقط</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="67"/>
         <source>Only display the text</source>
-        <translation type="unfinished"></translation>
+        <translation>اعرض النّصّ فقط</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="72"/>
         <source>The text appears beside the icon</source>
-        <translation type="unfinished"></translation>
+        <translation>يظهر النّصّ بجانب الأيقونة</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="77"/>
         <source>The text appears under the icon</source>
-        <translation type="unfinished"></translation>
+        <translation>يظهر النّصّ أسفل الأيقونة</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="82"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>الافتراضيّ</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="90"/>
         <source>Activate item on single click</source>
-        <translation type="unfinished"></translation>
+        <translation>فعّل العنصر بالنّقر المفرد</translation>
     </message>
 </context>
 </TS>
