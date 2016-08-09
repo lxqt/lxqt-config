a91df9e6ce80dc8d15f747f615e3bb1bfcb0be20
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-09 20:10:03 +0200

    Add lxqt-config-brightness_ca.ts (Catalan)

---
+++ lxqt-config-brightness_ca.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>BrightnessSettings</name>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="14"/>
+        <source>Brightness Settings</source>
+        <translation>Ajusts de la brillantor</translation>
+    </message>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="25"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Ajusts de retroil·luminació i brillantor:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Utilitzeu les tecles de les fletxes per canviar els ajusts.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>OutputWidget</name>
+    <message>
+        <location filename="../../../outputwidget.ui" line="20"/>
+        <source>Form</source>
+        <translation>Formulari</translation>
+    </message>
+    <message>
+        <location filename="../../../outputwidget.ui" line="38"/>
+        <source>&lt;b&gt;Output:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Sortida:&lt;/b&gt;</translation>
+    </message>
+</context>
+</TS>
