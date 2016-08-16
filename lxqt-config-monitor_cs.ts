1ae7e85d2fcfb291f399c5f9aa359754325f858a
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

---
+++ lxqt-config-monitor_cs.ts
@@ -0,0 +1,304 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>FastMenu</name>
+    <message>
+        <location filename="../../../fastmenu.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formulář</translation>
+    </message>
+    <message>
+        <location filename="../../../fastmenu.ui" line="20"/>
+        <source>Fast options</source>
+        <translation>Rychlé volby</translation>
+    </message>
+    <message>
+        <location filename="../../../fastmenu.ui" line="40"/>
+        <source> ❶ ❷ Extended view</source>
+        <translation> ❶ ❷ Rozšířený pohled</translation>
+    </message>
+    <message>
+        <location filename="../../../fastmenu.ui" line="45"/>
+        <source> ❶ ❶ Unified view</source>
+        <translation> ❶ ❶ Jednotný pohled</translation>
+    </message>
+    <message>
+        <location filename="../../../fastmenu.ui" line="50"/>
+        <source> ❶     Only first</source>
+        <translation> ❶     Jen první pohled</translation>
+    </message>
+    <message>
+        <location filename="../../../fastmenu.ui" line="55"/>
+        <source>     ❷ Only second</source>
+        <translation>     ❷ Jen druhý pohled</translation>
+    </message>
+</context>
+<context>
+    <name>ManageSavedSettings</name>
+    <message>
+        <location filename="../../../managesavedsettings.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>Nastavení obrazovek</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.ui" line="27"/>
+        <source>Saved settings</source>
+        <translation>Uložená nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.ui" line="38"/>
+        <source>Rename</source>
+        <translation>Přejmenovat</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.ui" line="45"/>
+        <source>Delete</source>
+        <translation>Smazat</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.ui" line="52"/>
+        <source>Apply</source>
+        <translation>Použít</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.cpp" line="64"/>
+        <source>None</source>
+        <translation>Žádný</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.cpp" line="67"/>
+        <source>Left</source>
+        <translation>Vlevo</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.cpp" line="70"/>
+        <source>Inverted</source>
+        <translation>Obrácený</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.cpp" line="73"/>
+        <source>Right</source>
+        <translation>Vpravo</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.cpp" line="77"/>
+        <location filename="../../../managesavedsettings.cpp" line="78"/>
+        <source>True</source>
+        <translation>Pravda</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.cpp" line="77"/>
+        <location filename="../../../managesavedsettings.cpp" line="78"/>
+        <source>False</source>
+        <translation>Nepravda</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.cpp" line="140"/>
+        <source>Name</source>
+        <translation>Název</translation>
+    </message>
+    <message>
+        <location filename="../../../managesavedsettings.cpp" line="140"/>
+        <source>Name:</source>
+        <translation>Název:</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorPictureDialog</name>
+    <message>
+        <location filename="../../../monitorpicture.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorpicture.ui" line="27"/>
+        <source>Keep monitors attached</source>
+        <translation>Zachovat obrazovky připojené</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorSettingsDialog</name>
+    <message>
+        <location filename="../../../monitorsettingsdialog.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>Nastavení obrazovek</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorsettingsdialog.ui" line="71"/>
+        <source>Settings</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorsettingsdialog.cpp" line="111"/>
+        <source>Fast Menu</source>
+        <translation>Rychlá nabídka</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorsettingsdialog.cpp" line="115"/>
+        <source>Set position</source>
+        <translation>Nastavit polohu</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorsettingsdialog.cpp" line="244"/>
+        <source>Advanced settings</source>
+        <translation>Pokročilá nastavení</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorWidget</name>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="21"/>
+        <source>Setup</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="29"/>
+        <source> (x)</source>
+        <translation> (x)</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="45"/>
+        <source> (y)</source>
+        <translation> (y)</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="78"/>
+        <source>Resolution:</source>
+        <translation>Rozlišení:</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="90"/>
+        <source>Enable this display</source>
+        <translation>Povolit tuto obrazovku</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="98"/>
+        <source>This is my primary display</source>
+        <translation>Hlavní obrazovka</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="103"/>
+        <source>This screen extends another display</source>
+        <translation>Rozšiřující obrazovka</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="125"/>
+        <source>Advanced</source>
+        <translation>Pokročilá nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="146"/>
+        <source>Refresh rate:</source>
+        <translation>Obnovovací kmitočet obrazu:</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="153"/>
+        <source>Rotation:</source>
+        <translation>Otočení:</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="176"/>
+        <source>Info</source>
+        <translation>Informace</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.ui" line="182"/>
+        <source>Display information</source>
+        <translation>Informace o obrazovce</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="135"/>
+        <source>Name: %1
+</source>
+        <translation>Název: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="136"/>
+        <source>Vendor: %1
+</source>
+        <translation>Dodavatel: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="137"/>
+        <source>Serial: %1
+</source>
+        <translation>Série: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="138"/>
+        <source>Display size: %1cm x %2cm
+</source>
+        <translation>Velikost obrazovky: %1cm x %2cm
+</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="139"/>
+        <source>Serial number: %1
+</source>
+        <translation>Sériové číslo: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="140"/>
+        <source>EISA device ID: %1
+</source>
+        <translation>ID zařízení EISA: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="159"/>
+        <source>None</source>
+        <translation>Žádný</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="160"/>
+        <source>Left</source>
+        <translation>Vlevo</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="161"/>
+        <source>Right</source>
+        <translation>Vpravo</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="162"/>
+        <source>Inverted</source>
+        <translation>Obrácený</translation>
+    </message>
+    <message>
+        <location filename="../../../monitorwidget.cpp" line="252"/>
+        <source>%1 Hz</source>
+        <translation>%1 Hz</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../../settingsdialog.cpp" line="33"/>
+        <source>Manage Saved Settings</source>
+        <translation>Spravovat uložená nastavení</translation>
+    </message>
+</context>
+<context>
+    <name>TimeoutDialog</name>
+    <message>
+        <location filename="../../../timeoutdialog.ui" line="14"/>
+        <source>Settings are changed</source>
+        <translation>Nastavení jsou změněna</translation>
+    </message>
+    <message>
+        <location filename="../../../timeoutdialog.ui" line="46"/>
+        <source>Are the current settings OK for you?</source>
+        <translation>Jsou nynější nastavení správná?</translation>
+    </message>
+    <message>
+        <location filename="../../../timeoutdialog.cpp" line="62"/>
+        <source>%1 second(s) remaining</source>
+        <translation>Zbývá %1 sekund</translation>
+    </message>
+</context>
+</TS>
