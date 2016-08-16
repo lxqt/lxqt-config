e8eea2958506093dba4e916d4ad5afb0b4cd531b
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

--- lxqt-config_cs.ts
+++ lxqt-config_cs.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../mainwindow.ui" line="14"/>
         <source>Configuration Center</source>
-        <translation>Nastavení</translation>
+        <translation>Nastavovací středisko</translation>
     </message>
 </context>
 </TS>
