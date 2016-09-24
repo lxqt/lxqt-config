e8adbde3600fdf3ce5cd69e329bd13612a3cd67e
Sérgio Marques <smarquespt@gmail.com>
2016-09-24 18:38:49 +0100

    Massive update for Portuguese

---
+++ lxqt-config-brightness-pt.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>BrightnessSettings</name>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="14"/>
+        <source>Brightness Settings</source>
+        <translation>Definições de brilho</translation>
+    </message>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="25"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Definições de brilho e de iluminação:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Utilize as teclas de seta para alterar as definições.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>OutputWidget</name>
+    <message>
+        <location filename="../../../outputwidget.ui" line="20"/>
+        <source>Form</source>
+        <translation>Formulário</translation>
+    </message>
+    <message>
+        <location filename="../../../outputwidget.ui" line="38"/>
+        <source>&lt;b&gt;Output:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Destino:&lt;/b&gt;</translation>
+    </message>
+</context>
+</TS>
