81cb0c0a4769858d436b3513022bae7eaf293eb2
Pierre-Yves <py@localhost.localdomain>
2016-10-04 09:55:50 +0200

    lxqt-config: Add/Update Frech translation

--- lxqt-config-input_fr.ts
+++ lxqt-config-input_fr.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../../../keyboardconfig.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Formulaire</translation>
     </message>
     <message>
         <location filename="../../../keyboardconfig.ui" line="20"/>
         <source>Cursor flash time:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Clignotement du curseur:</translation>
     </message>
     <message>
         <location filename="../../../keyboardconfig.ui" line="27"/>
@@ -53,18 +53,19 @@
     <message>
         <location filename="../../../keyboardconfig.ui" line="122"/>
         <source>Repeat interval:</source>
-        <translation type="unfinished">Intervalle de répétition :</translation>
+        <translation>Délai de répétition :</translation>
     </message>
     <message>
         <location filename="../../../keyboardconfig.ui" line="129"/>
         <source>Type in the following box to test your keyboard settings</source>
-        <translation type="unfinished">Tapez dans le champ ci-dessous pour tester vos paramètres clavier</translation>
+        <translatorcomment>Tapez dans le champ ci-dessous pour tester vos paramètres clavier</translatorcomment>
+        <translation type="unfinished">Saisissez une phrase pour tester les réglages</translation>
     </message>
     <message>
         <location filename="../../../keyboardconfig.ui" line="150"/>
         <location filename="../../../keyboardconfig.ui" line="157"/>
         <source>0</source>
-        <translation type="unfinished">0</translation>
+        <translation>0</translation>
     </message>
     <message>
         <location filename="../../../keyboardconfig.ui" line="167"/>
@@ -127,7 +128,7 @@
     <message>
         <location filename="../../../keyboardlayoutconfig.ui" line="141"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Note&lt;/span&gt;: If you are using an &lt;span style=&quot; font-weight:600;&quot;&gt;input method&lt;/span&gt;, such as IBus, uim, fcitx, or gcin, the settings here &lt;span style=&quot; font-weight:600;&quot;&gt;might not work&lt;/span&gt; because they are overridden by the input methods.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Note&lt;/span&gt;: Si vous utilisez une &lt;span style=&quot; font-weight:600;&quot;&gt;méthode de saisie&lt;/span&gt;, telle que IBus, uim, fcitx, ou gcin, les réglages suivants &lt;span style=&quot; font-weight:600;&quot;&gt;peuvent ne pas fonctionner&lt;/span&gt; parce qu&apos;ils sont substitués par celle-ci.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -214,7 +215,7 @@
     <message>
         <location filename="../../../mouseconfig.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished">Formulaire</translation>
+        <translation>Formulaire</translation>
     </message>
     <message>
         <location filename="../../../mouseconfig.ui" line="23"/>
@@ -224,7 +225,7 @@
     <message>
         <location filename="../../../mouseconfig.ui" line="30"/>
         <source>Motion</source>
-        <translation type="unfinished">Mouvement</translation>
+        <translation>Mouvement</translation>
     </message>
     <message>
         <location filename="../../../mouseconfig.ui" line="36"/>
@@ -234,22 +235,22 @@
     <message>
         <location filename="../../../mouseconfig.ui" line="43"/>
         <source>Fast</source>
-        <translation type="unfinished">Rapide</translation>
+        <translation>Rapide</translation>
     </message>
     <message>
         <location filename="../../../mouseconfig.ui" line="50"/>
         <source>Sensitivity:</source>
-        <translation type="unfinished">Sensibilité :</translation>
+        <translation>Sensibilité :</translation>
     </message>
     <message>
         <location filename="../../../mouseconfig.ui" line="57"/>
         <source>Low</source>
-        <translation type="unfinished">Faible</translation>
+        <translation>Faible</translation>
     </message>
     <message>
         <location filename="../../../mouseconfig.ui" line="83"/>
         <source>Acceleration:</source>
-        <translation type="unfinished">Accélération :</translation>
+        <translation>Accélération :</translation>
     </message>
     <message>
         <location filename="../../../mouseconfig.ui" line="109"/>
@@ -260,12 +261,12 @@
         <location filename="../../../mouseconfig.ui" line="116"/>
         <location filename="../../../mouseconfig.ui" line="123"/>
         <source>0</source>
-        <translation type="unfinished">0</translation>
+        <translation>0</translation>
     </message>
     <message>
         <location filename="../../../mouseconfig.ui" line="133"/>
         <source>Double click interval:</source>
-        <translation type="unfinished">Durée d'un double-clic:</translation>
+        <translation type="unfinished">Durée d&apos;un double-clic:</translation>
     </message>
     <message>
         <location filename="../../../mouseconfig.ui" line="140"/>
@@ -280,7 +281,7 @@
     <message>
         <location filename="../../../mouseconfig.ui" line="160"/>
         <source>Left handed (Swap left and right mouse buttons)</source>
-        <translation type="unfinished">Gaucher (inverse les boutons droit et gauche de la souris)</translation>
+        <translation>Gaucher (inverse les boutons droit et gauche de la souris)</translation>
     </message>
     <message>
         <location filename="../../../mouseconfig.ui" line="167"/>
@@ -293,27 +294,27 @@
     <message>
         <location filename="../../../lxqt-config-input.cpp" line="34"/>
         <source>Keyboard and Mouse Settings</source>
-        <translation type="unfinished">Paramètres Clavier et Souris</translation>
+        <translation>Paramètres clavier et souris</translation>
     </message>
     <message>
         <location filename="../../../lxqt-config-input.cpp" line="39"/>
         <source>Mouse</source>
-        <translation type="unfinished">Souris</translation>
+        <translation>Souris</translation>
     </message>
     <message>
         <location filename="../../../lxqt-config-input.cpp" line="44"/>
         <source>Cursor</source>
-        <translation type="unfinished">Curseur</translation>
+        <translation>Curseur</translation>
     </message>
     <message>
         <location filename="../../../lxqt-config-input.cpp" line="47"/>
         <source>Keyboard</source>
-        <translation type="unfinished">Clavier</translation>
+        <translation>Clavier</translation>
     </message>
     <message>
         <location filename="../../../lxqt-config-input.cpp" line="51"/>
         <source>Keyboard Layout</source>
-        <translation type="unfinished">Disposition du clavier</translation>
+        <translation>Disposition du clavier</translation>
     </message>
 </context>
 <context>
@@ -321,12 +322,12 @@
     <message>
         <location filename="../../../selectkeyboardlayoutdialog.ui" line="14"/>
         <source>Select a keyboard layout</source>
-        <translation type="unfinished">Choisissez une disposition de clavier</translation>
+        <translation>Choisissez une disposition de clavier</translation>
     </message>
     <message>
         <location filename="../../../selectkeyboardlayoutdialog.ui" line="20"/>
         <source>Keyboard layout</source>
-        <translation type="unfinished">Disposition du clavier</translation>
+        <translation>Disposition du clavier</translation>
     </message>
     <message>
         <location filename="../../../selectkeyboardlayoutdialog.ui" line="27"/>
