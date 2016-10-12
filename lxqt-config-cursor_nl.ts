b6ead2c84e0fa1878ad0f6fbd55a9b8579d9a78c
Pjotr123 <pjotrvertaalt@gmail.com>
2016-10-12 21:08:25 +0200

    Update lxqt-config-cursor_nl.ts
    Complete Dutch translation

--- lxqt-config-cursor_nl.ts
+++ lxqt-config-cursor_nl.ts
@@ -6,36 +6,32 @@
     <message>
         <location filename="../../../selectwnd.ui" line="14"/>
         <source>LXQt Mouse Theme Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt instellingen voor muisthema</translation>
     </message>
     <message>
         <location filename="../../../selectwnd.ui" line="20"/>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kies het muisthema dat u wilt gebruiken (zweef-voorbeeldweergave om de aanwijzer uit te proberen). &lt;b&gt;De LXQt-sessie moet na deze verandering worden herstart&lt;/b&gt;:</translation>
     </message>
     <message>
         <location filename="../../../selectwnd.ui" line="81"/>
         <source>&amp;Install New Theme...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Installeer nieuw thema...</translation>
     </message>
     <message>
         <location filename="../../../selectwnd.ui" line="88"/>
         <source>&amp;Remove Theme</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&amp;Close</source>
-        <translation type="vanished">&amp;Sluiten</translation>
+        <translation>V&amp;ijder thema</translation>
     </message>
     <message>
         <location filename="../../../selectwnd.cpp" line="208"/>
         <source>XCurTheme error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fout van XcurTheme</translation>
     </message>
     <message>
         <location filename="../../../selectwnd.cpp" line="209"/>
         <source>You can&apos;t remove active theme!</source>
-        <translation type="unfinished"></translation>
+        <translation>U kunt het actieve thema niet verwijderen!</translation>
     </message>
 </context>
 <context>
@@ -43,17 +39,17 @@
     <message>
         <location filename="../../../warninglabel.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialoogvenster</translation>
     </message>
     <message>
         <location filename="../../../warninglabel.ui" line="44"/>
         <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt kon geen aanwijzerthema vinden. In plaats daarvan zal het standaardthema van X11 worden gebruikt. LXQt zocht in de volgende mappen:</translation>
     </message>
     <message>
         <location filename="../../../warninglabel.ui" line="54"/>
         <source>Show...</source>
-        <translation type="unfinished"></translation>
+        <translation>Tonen...</translation>
     </message>
 </context>
 <context>
@@ -61,12 +57,12 @@
     <message>
         <location filename="../../../thememodel.cpp" line="61"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Naam</translation>
     </message>
     <message>
         <location filename="../../../thememodel.cpp" line="62"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Beschrijving</translation>
     </message>
 </context>
 </TS>
