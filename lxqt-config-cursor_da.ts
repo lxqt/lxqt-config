f8236c2a8d275f47af80cb2a089561bbd896b759
scootergrisen <scootergrisen@gmail.com>
2016-12-13 02:29:38 +0100

    First attempt to change all danish translation files from using i mix of "da" and "da_DK" and both, to only using "da"

---
+++ lxqt-config-cursor_da.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>SelectWnd</name>
+    <message>
+        <location filename="../../../selectwnd.ui" line="14"/>
+        <source>LXQt Mouse Theme Configuration</source>
+        <translation>LXQt Musemarkør Tema Indstillinger</translation>
+    </message>
+    <message>
+        <location filename="../../../selectwnd.ui" line="20"/>
+        <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
+        <translation>Vælg det markør tema, du vil bruge (forhåndsvisning for at teste markør).
+&lt;b&gt; LXQt session skal genstartes efter denne ændring &lt;/ b&gt;:</translation>
+    </message>
+    <message>
+        <location filename="../../../selectwnd.ui" line="81"/>
+        <source>&amp;Install New Theme...</source>
+        <translation>&amp;Installer Nyt Tema...</translation>
+    </message>
+    <message>
+        <source>&amp;Set Theme</source>
+        <translation type="vanished">&amp;Sæt Tema</translation>
+    </message>
+    <message>
+        <location filename="../../../selectwnd.ui" line="88"/>
+        <source>&amp;Remove Theme</source>
+        <translation>&amp;Fjern Tema</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation type="vanished">&amp;Afslut</translation>
+    </message>
+    <message>
+        <location filename="../../../selectwnd.cpp" line="208"/>
+        <source>XCurTheme error</source>
+        <translation>XCurTheme fejl</translation>
+    </message>
+    <message>
+        <location filename="../../../selectwnd.cpp" line="209"/>
+        <source>You can&apos;t remove active theme!</source>
+        <translation>Det aktive tema kan ikke fjernes!</translation>
+    </message>
+</context>
+<context>
+    <name>WarningLabel</name>
+    <message>
+        <location filename="../../../warninglabel.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../warninglabel.ui" line="44"/>
+        <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../warninglabel.ui" line="54"/>
+        <source>Show...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>XCursorThemeModel</name>
+    <message>
+        <location filename="../../../thememodel.cpp" line="61"/>
+        <source>Name</source>
+        <translation>Navn</translation>
+    </message>
+    <message>
+        <location filename="../../../thememodel.cpp" line="62"/>
+        <source>Description</source>
+        <translation>Beskrivelse</translation>
+    </message>
+</context>
+</TS>
