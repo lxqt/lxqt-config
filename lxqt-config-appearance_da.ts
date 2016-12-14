7d809748a6afe11f5de6e53d98734b1880d56ab2
scootergrisen <scootergrisen@gmail.com>
2016-12-14 02:46:15 +0100

    Update Danish translations

--- lxqt-config-appearance_da.ts
+++ lxqt-config-appearance_da.ts
@@ -6,123 +6,123 @@
     <message>
         <location filename="../../../fontsconfig.ui" line="23"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Skrifttype</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="30"/>
         <source>Default font for user interface</source>
-        <translation type="unfinished"></translation>
+        <translation>Standardskrifttype til brugerflade</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="36"/>
         <source>Font name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Skrifttypenavn:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="46"/>
         <source>Style:</source>
-        <translation type="unfinished"></translation>
+        <translation>Stil:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="53"/>
         <source>Point size:</source>
-        <translation type="unfinished"></translation>
+        <translation>Punktstørrelse:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="68"/>
         <source>Normal</source>
-        <translation type="unfinished"></translation>
+        <translation>Normal</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="73"/>
         <source>Bold</source>
-        <translation type="unfinished"></translation>
+        <translation>Fed</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="78"/>
         <source>Italic</source>
-        <translation type="unfinished"></translation>
+        <translation>Kursiv</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="83"/>
         <source>Bold Italic</source>
-        <translation type="unfinished"></translation>
+        <translation>Fed kursiv</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="94"/>
         <source>The following settings only affect newly started applications</source>
-        <translation type="unfinished"></translation>
+        <translation>Følgende indstillinger påvirker kun nyligt startet programmer</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="100"/>
         <source>Use antialias fonts</source>
-        <translation type="unfinished"></translation>
+        <translation>Brug antialias-skrifttyper</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="107"/>
         <source>Font hinting style:</source>
-        <translation type="unfinished"></translation>
+        <translation>Skrifttype hinting-stil:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="118"/>
         <location filename="../../../fontsconfig.ui" line="183"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Ingen</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="123"/>
         <source>Slight</source>
-        <translation type="unfinished"></translation>
+        <translation>Let</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="128"/>
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Medium</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="133"/>
         <source>Full</source>
-        <translation type="unfinished"></translation>
+        <translation>Fuld</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="141"/>
         <source>Font hinting</source>
-        <translation type="unfinished"></translation>
+        <translation>Skrifttype hintning</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="148"/>
         <source>Resolution (DPI):</source>
-        <translation type="unfinished"></translation>
+        <translation>Opløsning (DPI):</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="155"/>
         <source>Autohint</source>
-        <translation type="unfinished"></translation>
+        <translation>Autohint</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="172"/>
         <source>Subpixel antialiasing:</source>
-        <translation type="unfinished"></translation>
+        <translation>Underpixel-antialiasing:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="188"/>
         <source>RGB</source>
-        <translation type="unfinished"></translation>
+        <translation>RGB</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="193"/>
         <source>BGR</source>
-        <translation type="unfinished"></translation>
+        <translation>BGR</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="198"/>
         <source>VRGB</source>
-        <translation type="unfinished"></translation>
+        <translation>VRGB</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="203"/>
         <source>VBGR</source>
-        <translation type="unfinished"></translation>
+        <translation>VBGR</translation>
     </message>
 </context>
 <context>
@@ -130,7 +130,7 @@
     <message>
         <location filename="../../../iconthemeconfig.ui" line="14"/>
         <source>LXQt Appearance Configuration</source>
-        <translation>LXQt Indstillinger for Udseende</translation>
+        <translation>Konfiguraton af LXQt udseende</translation>
     </message>
     <message>
         <location filename="../../../iconthemeconfig.ui" line="26"/>
@@ -143,7 +143,7 @@
     <message>
         <location filename="../../../lxqtthemeconfig.ui" line="23"/>
         <source>LXQt Theme</source>
-        <translation>LXQt Tema</translation>
+        <translation>LXQt-tema</translation>
     </message>
 </context>
 <context>
@@ -151,12 +151,12 @@
     <message>
         <location filename="../../../main.cpp" line="44"/>
         <source>LXQt Appearance Configuration</source>
-        <translation>LXQt Indstillinger for Udseende</translation>
+        <translation>Konfiguraton af LXQt udseende</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="50"/>
         <source>Widget Style</source>
-        <translation type="unfinished"></translation>
+        <translation>Widget-stil</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="54"/>
@@ -166,17 +166,17 @@
     <message>
         <location filename="../../../main.cpp" line="58"/>
         <source>LXQt Theme</source>
-        <translation>LXQt Tema</translation>
+        <translation>LXQt-tema</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="62"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Skrifttype</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="67"/>
         <source>Cursor</source>
-        <translation type="unfinished"></translation>
+        <translation>Markør</translation>
     </message>
 </context>
 <context>
@@ -184,42 +184,42 @@
     <message>
         <location filename="../../../styleconfig.ui" line="23"/>
         <source>Widget Style</source>
-        <translation type="unfinished"></translation>
+        <translation>Widget-stil</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="54"/>
         <source>Toolbar button style:</source>
-        <translation type="unfinished"></translation>
+        <translation>Værktøjslinje knap-stil:</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="62"/>
         <source>Only display the icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis kun ikonet</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="67"/>
         <source>Only display the text</source>
-        <translation type="unfinished"></translation>
+        <translation>Vis kun teksten</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="72"/>
         <source>The text appears beside the icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Teksten vises ved siden af ikonet</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="77"/>
         <source>The text appears under the icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Teksten vises under ikonet</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="82"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Standard</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="90"/>
         <source>Activate item on single click</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktivér objekt med ét klik</translation>
     </message>
 </context>
 </TS>
