21f313b09b9a36b924aabf486391d8fdecb9d0a7
scootergrisen <scootergrisen@gmail.com>
2016-12-14 02:46:15 +0100

    Update Danish translations

---
+++ lxqt-config-brightness_da.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>BrightnessSettings</name>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="14"/>
+        <source>Brightness Settings</source>
+        <translation>Lysstyrkeindstillinger</translation>
+    </message>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="25"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Indstillinger for backlight og lysstyrke:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Brug piletasterne for at ændre indstillingerne.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>OutputWidget</name>
+    <message>
+        <location filename="../../../outputwidget.ui" line="20"/>
+        <source>Form</source>
+        <translation>Formular</translation>
+    </message>
+    <message>
+        <location filename="../../../outputwidget.ui" line="38"/>
+        <source>&lt;b&gt;Output:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Output:&lt;/b&gt;</translation>
+    </message>
+</context>
+</TS>
