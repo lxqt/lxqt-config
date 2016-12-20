a42450cf6532f60f3cb341282198675d94325626
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-12-21 00:07:49 +0200

    Add/update Turkish translations

---
+++ lxqt-config-brightness_tr.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>BrightnessSettings</name>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="14"/>
+        <source>Brightness Settings</source>
+        <translation>Parlaklık Ayarları</translation>
+    </message>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="25"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Arka aydınlatma ve parlaklık ayarları:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Ayarları değiştirmek için ok tuşlarını kullanın.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>OutputWidget</name>
+    <message>
+        <location filename="../../../outputwidget.ui" line="20"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../../../outputwidget.ui" line="38"/>
+        <source>&lt;b&gt;Output:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Çıktı:&lt;/b&gt;</translation>
+    </message>
+</context>
+</TS>
