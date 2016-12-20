ce71506f2e7f125f5cba3bc85cf999752ba34ecd
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2016-12-21 00:07:49 +0200

    Add/update Turkish translations

--- lxqt-config-cursor_tr.ts
+++ lxqt-config-cursor_tr.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../../../selectwnd.ui" line="14"/>
         <source>LXQt Mouse Theme Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQT Fare Teması Yapılandırma</translation>
     </message>
     <message>
         <location filename="../../../selectwnd.ui" line="20"/>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanmak istediğiniz temayı seçin (önizleme yapmak için fareyi simgelerin üzerinde tutun). &lt;b&gt;LXQT oturumu bu değişiklikten sonra yeniden başlatılmalı&lt;/b&gt;:</translation>
     </message>
     <message>
         <location filename="../../../selectwnd.ui" line="81"/>
         <source>&amp;Install New Theme...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Yeni Tema Yükle...</translation>
     </message>
     <message>
         <location filename="../../../selectwnd.ui" line="88"/>
         <source>&amp;Remove Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Temayı &amp;Kaldır</translation>
     </message>
     <message>
         <source>&amp;Close</source>
@@ -30,12 +30,12 @@
     <message>
         <location filename="../../../selectwnd.cpp" line="208"/>
         <source>XCurTheme error</source>
-        <translation type="unfinished"></translation>
+        <translation>XCurTema hatası</translation>
     </message>
     <message>
         <location filename="../../../selectwnd.cpp" line="209"/>
         <source>You can&apos;t remove active theme!</source>
-        <translation type="unfinished"></translation>
+        <translation>Etkin temayı kaldıramazsınız!</translation>
     </message>
 </context>
 <context>
@@ -43,17 +43,17 @@
     <message>
         <location filename="../../../warninglabel.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Form</translation>
     </message>
     <message>
         <location filename="../../../warninglabel.ui" line="44"/>
         <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQT bir imleç teması buladı. Öntanımlı X11 imleç teması kullanılacak. LXQT aşağıdaki dizinlere baktı:</translation>
     </message>
     <message>
         <location filename="../../../warninglabel.ui" line="54"/>
         <source>Show...</source>
-        <translation type="unfinished"></translation>
+        <translation>Göster...</translation>
     </message>
 </context>
 <context>
@@ -61,12 +61,12 @@
     <message>
         <location filename="../../../thememodel.cpp" line="61"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Adı</translation>
     </message>
     <message>
         <location filename="../../../thememodel.cpp" line="62"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Açıklama</translation>
     </message>
 </context>
 </TS>
