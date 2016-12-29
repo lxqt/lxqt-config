dec2ce412cb0c2ee0a2a073d970095f54c12f691
Luís Pereira <luis.artur.pereira@gmail.com>
2016-12-29 18:06:57 +0000

    Fix self icontheme update (#113)
    * lxqt-config-appearance: Update own icons on icon theme change event
    
    The hack to do it was removed by commit lxde/lxqt-config@1cac4ae.
    As the XdgIconLoader is the backend for all QIcon stuff (via lxqt-qtplugin)
    we can simply use `QIcon::setThemeName()`.
    
    Closes https://github.com/lxde/lxqt/issues/1220
    
    * lxqt-config-appearance: Use const theme variable
    
    A by-the-way commit.

--- lxqt-config-appearance/iconthemeconfig.cpp
+++ lxqt-config-appearance/iconthemeconfig.cpp
@@ -138,9 +138,12 @@ IconThemeConfig::~IconThemeConfig()
 void IconThemeConfig::iconThemeSelected(QTreeWidgetItem *item, int column)
 {
     Q_UNUSED(column);
-    QString theme = item->data(0, Qt::UserRole).toString();
+    const QString theme = item->data(0, Qt::UserRole).toString();
     if (!theme.isEmpty())
     {
+        // Ensure that this widget also updates it's own icons
+        QIcon::setThemeName(theme);
+
         m_settings->setValue("icon_theme",  theme);
         m_settings->sync();
     }
