b0fe56b341c61ea826b03f6749411c9400af0c0b
Alf Gaida <agaida@siduction.org>
2017-06-09 21:02:53 +0200

    Added QCommandLineParser and basic cli interface to lxqt-config-file-associations

--- lxqt-config-file-associations/main.cpp
+++ lxqt-config-file-associations/main.cpp
@@ -32,6 +32,7 @@
 #include <QIODevice>
 #include <QSettings>
 #include <QVariant>
+#include <QCommandLineParser>
 
 #include <LXQt/Settings>
 #include <XdgDesktopFile>
@@ -41,6 +42,16 @@
 int main (int argc, char **argv)
 {
     LXQt::SingleApplication app(argc, argv);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QStringLiteral("LXQt Config File Associations"));
+    const QString VERINFO = LXQT_CONFIG_VERSION \
+                            "\n\nliblxqt:   " LXQT_VERSION\
+                            "\nQt:        " QT_VERSION_STR;
+    app.setApplicationVersion(VERINFO);
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.process(app);
 
     MimetypeViewer mimetypeViewer;
     app.setActivationWindow(&mimetypeViewer);
