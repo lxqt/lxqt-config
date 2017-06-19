12820c69c1df4e77835e13be30b56335e4520768
Demiray <mdemiray@msn.com>
2017-06-19 10:06:33 +0300

    Turkish translation works...
    Turkish translation works...
    _______________
    Demiray Muhterem
    codename: tulliana
    tulliana@archman.org

--- lxqt-config-appearance_tr.ts
+++ lxqt-config-appearance_tr.ts
@@ -6,123 +6,123 @@
     <message>
         <location filename="../../../fontsconfig.ui" line="23"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazı tipi</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="30"/>
         <source>Default font for user interface</source>
-        <translation type="unfinished"></translation>
+        <translation>Kullanıcı arabirimi için varsayılan yazı tipi</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="36"/>
         <source>Font name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazı tipi:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="46"/>
         <source>Style:</source>
-        <translation type="unfinished"></translation>
+        <translation>Tarz:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="53"/>
         <source>Point size:</source>
-        <translation type="unfinished"></translation>
+        <translation>Punto boyutu:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="68"/>
         <source>Normal</source>
-        <translation type="unfinished"></translation>
+        <translation>Normal</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="73"/>
         <source>Bold</source>
-        <translation type="unfinished"></translation>
+        <translation>Kalın</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="78"/>
         <source>Italic</source>
-        <translation type="unfinished"></translation>
+        <translation>Eğik</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="83"/>
         <source>Bold Italic</source>
-        <translation type="unfinished"></translation>
+        <translation>Kalın eğik</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="94"/>
         <source>The following settings only affect newly started applications</source>
-        <translation type="unfinished"></translation>
+        <translation>Aşağıdaki ayarlar yalnızca yeni başlatılan uygulamaları etkiler</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="100"/>
         <source>Use antialias fonts</source>
-        <translation type="unfinished"></translation>
+        <translation>Antialias yazıtipi kullan</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="107"/>
         <source>Font hinting style:</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazı tipi imleme stili:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="118"/>
         <location filename="../../../fontsconfig.ui" line="183"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Yok</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="123"/>
         <source>Slight</source>
-        <translation type="unfinished"></translation>
+        <translation>Slayt</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="128"/>
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Orta</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="133"/>
         <source>Full</source>
-        <translation type="unfinished"></translation>
+        <translation>Tam</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="141"/>
         <source>Font hinting</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazı tipi ipucu</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="148"/>
         <source>Resolution (DPI):</source>
-        <translation type="unfinished"></translation>
+        <translation>Çözünürlük (DPI):</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="155"/>
         <source>Autohint</source>
-        <translation type="unfinished"></translation>
+        <translation>Otomatik ipucu</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="172"/>
         <source>Subpixel antialiasing:</source>
-        <translation type="unfinished"></translation>
+        <translation>Altpiksel anti-aliasing:</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="188"/>
         <source>RGB</source>
-        <translation type="unfinished"></translation>
+        <translation>RGB</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="193"/>
         <source>BGR</source>
-        <translation type="unfinished"></translation>
+        <translation>BGR</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="198"/>
         <source>VRGB</source>
-        <translation type="unfinished"></translation>
+        <translation>VRGB</translation>
     </message>
     <message>
         <location filename="../../../fontsconfig.ui" line="203"/>
         <source>VBGR</source>
-        <translation type="unfinished"></translation>
+        <translation>VBGR</translation>
     </message>
 </context>
 <context>
@@ -156,7 +156,7 @@
     <message>
         <location filename="../../../main.cpp" line="50"/>
         <source>Widget Style</source>
-        <translation type="unfinished"></translation>
+        <translation>Gereç Tarzı</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="54"/>
@@ -171,12 +171,12 @@
     <message>
         <location filename="../../../main.cpp" line="62"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazıtipi</translation>
     </message>
     <message>
         <location filename="../../../main.cpp" line="67"/>
         <source>Cursor</source>
-        <translation type="unfinished"></translation>
+        <translation>İmleç</translation>
     </message>
 </context>
 <context>
@@ -184,42 +184,42 @@
     <message>
         <location filename="../../../styleconfig.ui" line="23"/>
         <source>Widget Style</source>
-        <translation type="unfinished"></translation>
+        <translation>Gereç Stili</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="54"/>
         <source>Toolbar button style:</source>
-        <translation type="unfinished"></translation>
+        <translation>Araç Çubuğu düğme stili:</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="62"/>
         <source>Only display the icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Sadece simgeyi göster</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="67"/>
         <source>Only display the text</source>
-        <translation type="unfinished"></translation>
+        <translation>Sadece metni göster</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="72"/>
         <source>The text appears beside the icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Simgenin yanında metin göster</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="77"/>
         <source>The text appears under the icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Simge altında metin göster</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="82"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Varsayılan</translation>
     </message>
     <message>
         <location filename="../../../styleconfig.ui" line="90"/>
         <source>Activate item on single click</source>
-        <translation type="unfinished"></translation>
+        <translation>Tek tıklamayla öğeyi etkinleştir</translation>
     </message>
 </context>
 </TS>
