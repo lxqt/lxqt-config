5ad7ae6cb27fa7505d4e7b6fe4ede12218f897d9
m4sk1n <me@m4sk.in>
2017-08-04 23:37:16 +0200

    Improve Polish translation

--- lxqt-config_pl.ts
+++ lxqt-config_pl.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../../../mainwindow.ui" line="14"/>
         <source>Configuration Center</source>
-        <translation>Centrum konfiguracji</translation>
+        <translation>Panel sterowania</translation>
     </message>
 </context>
 </TS>
