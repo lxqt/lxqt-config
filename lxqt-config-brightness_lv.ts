a81e276265dfcdff254e7c38febfbb0ac30c492a
Toms Tilgalis <ttilgalis@gmail.com>
2017-09-06 00:33:49 +0300

    Latvian - Admin time, Cursor, Brightness, File Assoc., Config

---
+++ lxqt-config-brightness_lv.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lv">
+<context>
+    <name>BrightnessSettings</name>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="14"/>
+        <source>Brightness Settings</source>
+        <translation>Spilgtuma Iestatījumi</translation>
+    </message>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="25"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Apgaismojuma un spilgtuma iestatījumi:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Lieto bultiņu taustiņus, lai mainītu vērtības.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../brightnesssettings.ui" line="35"/>
+        <source>Require confirmation after settings change</source>
+        <translation>Nepieciešams apstiprinājums pēc iestatījumu maiņas</translation>
+    </message>
+    <message>
+        <location filename="../../../brightnesssettings.cpp" line="68"/>
+        <source>Brightness settings changed</source>
+        <translation>Spligtuma iestatījumi saglabāti</translation>
+    </message>
+    <message>
+        <location filename="../../../brightnesssettings.cpp" line="69"/>
+        <source>Confirmation required. Are the settings correct?</source>
+        <translation>Nepieciešams apstiprinājums. Vai iestatījumi ir pareizi?</translation>
+    </message>
+</context>
+<context>
+    <name>OutputWidget</name>
+    <message>
+        <source>Form</source>
+        <translation type="vanished">Forma</translation>
+    </message>
+    <message>
+        <location filename="../../../outputwidget.ui" line="38"/>
+        <source>&lt;b&gt;Output:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Izeja:&lt;/b&gt;</translation>
+    </message>
+</context>
+</TS>
