1b0a33fe9c1de44621afdea4a12f11a1b2e92bbb
Arif Budiman <arifpedia@gmail.com>
2017-10-01 01:20:06 +0700

    Indonesian translation (#260)
    * Update compton-conf_id.ts
    * Create libfm-qt_id.ts
    * Added liblxqt/liblxqt_id.ts
    * Added lximage-qt/lximage-qt_id.ts
    * Added lxqt-about/lxqt-about_id.ts
    * Added lxqt-admin/lxqt-admin-time/lxqt-admin-time_id.ts
    * Added lxqt-admin/lxqt-admin-user/lxqt-admin-user_id.ts
    * Added lxqt-config/liblxqt-config-cursor/lxqt-config-cursor_id.ts
    * Added lxqt-config/lxqt-config/lxqt-config_id.ts
    * Added lxqt-config/lxqt-config-appearance/lxqt-config-appearance_id.ts
    * Added lxqt-config/lxqt-config-brightness/lxqt-config-brightness_id.ts
    * Added lxqt-config/lxqt-config-file-associations/lxqt-config-file-associations_id.ts
    * Update lxqt-config/lxqt-config-input/lxqt-config-input_id.ts
    * Added lxqt-config/lxqt-config-locale/lxqt-config-locale_id.ts
    * Added lxqt-config/lxqt-config-monitor/lxqt-config-monitor_id.ts
    * Added lxqt-config-globalkeyshortcuts_id.ts
    * Added lxqt-config-notificationd_id.ts
    * Added lxqt-notificationd_id.ts
    * Added lxqt-openssh-askpass_id.ts
    * Added lxqt-panel_id.ts
    * Added clock_id.ts
    * Added cpuload_id.ts
    * Added desktopswitch_id.ts
    * Added directorymenu_id.ts
    * Added dom_id.ts
    * Added kbindicator_id.ts
    * Added mainmenu_id.ts
    * Added mount_id.ts
    * Added networkmonitor_id.ts
    * Added quicklaunch_id.ts
    * Added sensors_id.ts
    * Added showdesktop_id.ts
    * Added spacer_id.ts
    * Added sysstat_id.ts
    * Added taskbar_id.ts
    * Added volume_id.ts
    * Added worldclock_id.ts
    * Added lxqt-policykit-agent_id.ts
    * Added lxqt-config-powermanagement_id.ts
    * Added lxqt-powermanagement_id.ts
    * Added lxqt-runner_id.ts
    * Added lxqt-config-session_id.ts
    * Added lxqt-leave_id.ts
    * Added lxqt-session_id.ts
    * Added lxqt-sudo_id.ts
    * Remove all Indonesian translation files using "id_ID"

---
+++ lxqt-config-locale_id.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="id">
+<context>
+    <name>LocaleConfig</name>
+    <message>
+        <location filename="../../../localeconfig.ui" line="47"/>
+        <source>Re&amp;gion:</source>
+        <translation>Wilayah:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="76"/>
+        <source>De&amp;tailed Settings</source>
+        <translation>Pengaturan Terperinci</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="83"/>
+        <source>&amp;Numbers:</source>
+        <translation>Angka:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="112"/>
+        <source>&amp;Time:</source>
+        <translation>Waktu:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="141"/>
+        <source>Currenc&amp;y:</source>
+        <translation>Mata uang:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="170"/>
+        <source>Measurement &amp;Units:</source>
+        <translation>&amp;Unit Pengukuran:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="199"/>
+        <source>Co&amp;llation and Sorting:</source>
+        <translation>Kolasi dan Sortasi:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="238"/>
+        <source>&lt;b&gt;Examples&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Contoh&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="248"/>
+        <source>Numbers:</source>
+        <translation>Angka:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="268"/>
+        <source>Time:</source>
+        <translation>Waktu:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="288"/>
+        <source>Currency:</source>
+        <translation>Mata uang:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.ui" line="308"/>
+        <source>Measurement Units:</source>
+        <translation>Unit Pengukuran:</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.cpp" line="119"/>
+        <source>No change</source>
+        <translation>Tidak ada perubahan</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.cpp" line="308"/>
+        <source>Format Settings Changed</source>
+        <translation>Pengaturan Format telah diubah</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.cpp" line="309"/>
+        <source>Save the settings ? (they will take effect the next time you log in)</source>
+        <translation>Simpan pengaturan ? (mereka akan berlaku setelah Anda masuk ulang)</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.cpp" line="430"/>
+        <source>Imperial UK</source>
+        <translation>Imperial UK</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.cpp" line="434"/>
+        <source>Imperial US</source>
+        <translation>Imperial US</translation>
+    </message>
+    <message>
+        <location filename="../../../localeconfig.cpp" line="438"/>
+        <source>Metric</source>
+        <translation>Metric</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../../main.cpp" line="50"/>
+        <source>LXQt Locale Configuration</source>
+        <translation>Konfigurasi Locale LXQt</translation>
+    </message>
+    <message>
+        <location filename="../../../main.cpp" line="55"/>
+        <source>Locale Settings</source>
+        <translation>Pengaturan Locale</translation>
+    </message>
+</context>
+</TS>
