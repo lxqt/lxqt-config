791a63e6e90b6b8b30c2a37a8960a1e4a0305abe
attus <attus@enterpriseforever.com>
2017-12-06 22:07:51 +0100

    upd: hungarian translations

--- lxqt-config-monitor_hu.ts
+++ lxqt-config-monitor_hu.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../../../fastmenu.ui" line="20"/>
         <source>Fast options</source>
-        <translation type="unfinished"></translation>
+        <translation>Gyors lehetőségek</translation>
     </message>
     <message>
         <location filename="../../../fastmenu.ui" line="40"/>
         <source> ❶ ❷ Extended view</source>
-        <translation type="unfinished"></translation>
+        <translation> ❶ ❷ Kiterjesztett nézet</translation>
     </message>
     <message>
         <location filename="../../../fastmenu.ui" line="45"/>
         <source> ❶ ❶ Unified view</source>
-        <translation type="unfinished"></translation>
+        <translation> ❶ ❶ Egyesített nézet</translation>
     </message>
     <message>
         <location filename="../../../fastmenu.ui" line="50"/>
         <source> ❶     Only first</source>
-        <translation type="unfinished"></translation>
+        <translation> ❶     Csak az első</translation>
     </message>
     <message>
         <location filename="../../../fastmenu.ui" line="55"/>
         <source>     ❷ Only second</source>
-        <translation type="unfinished"></translation>
+        <translation>     ❷ Csak a második</translation>
     </message>
 </context>
 <context>
@@ -34,96 +34,73 @@
     <message>
         <location filename="../../../managesavedsettings.ui" line="14"/>
         <source>Monitor Settings</source>
-        <translation type="unfinished">Monitor beállítás</translation>
+        <translation>Monitor beállítás</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.ui" line="27"/>
         <source>Saved settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Mentett beállítások</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.ui" line="38"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Átnevez</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.ui" line="45"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Töröl</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.ui" line="52"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Alkalmaz</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.cpp" line="64"/>
         <source>None</source>
-        <translation type="unfinished">Semmi</translation>
+        <translation>Semmi</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.cpp" line="67"/>
         <source>Left</source>
-        <translation type="unfinished">Bal</translation>
+        <translation>Bal</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.cpp" line="70"/>
         <source>Inverted</source>
-        <translation type="unfinished">Inverz</translation>
+        <translation>Inverz</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.cpp" line="73"/>
         <source>Right</source>
-        <translation type="unfinished">Jobb</translation>
+        <translation>Jobb</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.cpp" line="77"/>
         <location filename="../../../managesavedsettings.cpp" line="78"/>
         <source>True</source>
-        <translation type="unfinished"></translation>
+        <translation>Igaz</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.cpp" line="77"/>
         <location filename="../../../managesavedsettings.cpp" line="78"/>
         <source>False</source>
-        <translation type="unfinished"></translation>
+        <translation>Hamis</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.cpp" line="140"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Név</translation>
     </message>
     <message>
         <location filename="../../../managesavedsettings.cpp" line="140"/>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>MonitorInfo</name>
-    <message>
-        <source>Laptop LCD Monitor</source>
-        <translation type="vanished">Beépített LCD Monitor</translation>
-    </message>
-    <message>
-        <source>External VGA Monitor</source>
-        <translation type="vanished">Külső VGA Monitor</translation>
-    </message>
-    <message>
-        <source>External DVI Monitor</source>
-        <translation type="vanished">Külső DVI Monitor</translation>
-    </message>
-    <message>
-        <source>Default Monitor</source>
-        <translation type="vanished">Alapértelmezett Monitor</translation>
+        <translation>Név:</translation>
     </message>
 </context>
 <context>
     <name>MonitorPictureDialog</name>
-    <message>
-        <source>Dialog</source>
-        <translation type="vanished">Párbeszédablak</translation>
-    </message>
     <message>
         <location filename="../../../monitorpicture.ui" line="27"/>
         <source>Keep monitors attached</source>
@@ -145,20 +122,12 @@
     <message>
         <location filename="../../../monitorsettingsdialog.cpp" line="111"/>
         <source>Fast Menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Gyors menü</translation>
     </message>
     <message>
         <location filename="../../../monitorsettingsdialog.cpp" line="115"/>
         <source>Set position</source>
-        <translation>Pozíció</translation>
-    </message>
-    <message>
-        <source>Error</source>
-        <translation type="vanished">Hiba</translation>
-    </message>
-    <message>
-        <source>Config can not be saved</source>
-        <translation type="vanished">A beállítás menthetetlen</translation>
+        <translation>Pozícionálás</translation>
     </message>
     <message>
         <location filename="../../../monitorsettingsdialog.cpp" line="244"/>
@@ -204,30 +173,6 @@
         <translation>Kiterjesztődik más megjelenítőre is</translation>
     </message>
     <message>
-        <source>This screen clones another display</source>
-        <translation type="vanished">Amásik megjelenítővel azonos</translation>
-    </message>
-    <message>
-        <source>Right of...</source>
-        <translation type="vanished">Jobbról...</translation>
-    </message>
-    <message>
-        <source>Left of...</source>
-        <translation type="vanished">Balról...</translation>
-    </message>
-    <message>
-        <source>Above...</source>
-        <translation type="vanished">Felett...</translation>
-    </message>
-    <message>
-        <source>Below...</source>
-        <translation type="vanished">Belül...</translation>
-    </message>
-    <message>
-        <source>Positioned manually</source>
-        <translation type="vanished">Kézzel pozícionált</translation>
-    </message>
-    <message>
         <location filename="../../../monitorwidget.ui" line="125"/>
         <source>Advanced</source>
         <translation>Haladó</translation>
@@ -235,7 +180,7 @@
     <message>
         <location filename="../../../monitorwidget.ui" line="146"/>
         <source>Refresh rate:</source>
-        <translation>Frissítés:</translation>
+        <translation>Frissítési ütem:</translation>
     </message>
     <message>
         <location filename="../../../monitorwidget.ui" line="153"/>
@@ -256,31 +201,36 @@
         <location filename="../../../monitorwidget.cpp" line="135"/>
         <source>Name: %1
 </source>
-        <translation>Név: %1</translation>
+        <translation>Név: %1
+</translation>
     </message>
     <message>
         <location filename="../../../monitorwidget.cpp" line="136"/>
         <source>Vendor: %1
 </source>
-        <translation>Forgalmazó: %1</translation>
+        <translation>Forgalmazó: %1
+</translation>
     </message>
     <message>
         <location filename="../../../monitorwidget.cpp" line="137"/>
         <source>Serial: %1
 </source>
-        <translation>Sorszám: %1</translation>
+        <translation>Sorszám: %1
+</translation>
     </message>
     <message>
         <location filename="../../../monitorwidget.cpp" line="138"/>
         <source>Display size: %1cm x %2cm
 </source>
-        <translation>Képméret: %1cm x %2cm</translation>
+        <translation>Képméret: %1cm x %2cm
+</translation>
     </message>
     <message>
         <location filename="../../../monitorwidget.cpp" line="139"/>
         <source>Serial number: %1
 </source>
-        <translation>Sorozatszám:%1</translation>
+        <translation>Sorozatszám: %1
+</translation>
     </message>
     <message>
         <location filename="../../../monitorwidget.cpp" line="140"/>
@@ -291,7 +241,7 @@
     <message>
         <location filename="../../../monitorwidget.cpp" line="161"/>
         <source>None</source>
-        <translation>Semmi</translation>
+        <translation>semmi</translation>
     </message>
     <message>
         <location filename="../../../monitorwidget.cpp" line="162"/>
@@ -319,7 +269,7 @@
     <message>
         <location filename="../../../settingsdialog.cpp" line="33"/>
         <source>Manage Saved Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Mentett beállítások kezelése</translation>
     </message>
 </context>
 <context>
@@ -327,21 +277,18 @@
     <message>
         <location filename="../../../timeoutdialog.ui" line="14"/>
         <source>Settings are changed</source>
-        <translation>Beállítások változtatása</translation>
+        <translation>A beállítások változtak</translation>
     </message>
     <message>
         <location filename="../../../timeoutdialog.ui" line="46"/>
         <source>Are the current settings OK for you?</source>
         <translation>Jók a beállítások?</translation>
     </message>
-    <message>
-        <source>%1 second(s) remaining</source>
-        <translation type="vanished">%1 másodperc van hátra</translation>
-    </message>
     <message numerus="yes">
         <location filename="../../../timeoutdialog.cpp" line="62"/>
         <source>%n second(s) remaining</source>
-        <translation type="unfinished">
+        <translatorcomment>%n másodperc van hátra</translatorcomment>
+        <translation>
             <numerusform></numerusform>
         </translation>
     </message>
