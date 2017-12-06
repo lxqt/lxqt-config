edeea83fbeb370b9f732cd48e73c82fcca07f069
attus <attus@enterpriseforever.com>
2017-12-06 22:07:51 +0100

    upd: hungarian translations

--- lxqt-config-locale_hu.ts
+++ lxqt-config-locale_hu.ts
@@ -80,7 +80,7 @@
     <message>
         <location filename="../../../localeconfig.cpp" line="309"/>
         <source>Do you want to save your changes? They will take effect the next time you log in.</source>
-        <translation type="unfinished"></translation>
+        <translation>Változtatások mentése? Ez a legközelebbi bejelentkezéskor lesz hatásos.</translation>
     </message>
     <message>
         <location filename="../../../localeconfig.cpp" line="430"/>
