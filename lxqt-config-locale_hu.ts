01f14f3f6292eddd88449136e5f62e74be836cf7
attuska <attuska@users.noreply.github.com>
2017-12-07 14:05:02 +0100

    Hungarian translations finished (#309)
    * hu: typos fix
    
    * hu: fix more typos
    
    * hu translations completed

--- lxqt-config-locale_hu.ts
+++ lxqt-config-locale_hu.ts
@@ -66,12 +66,12 @@
     <message>
         <location filename="../../../localeconfig.cpp" line="119"/>
         <source>No change</source>
-        <translation>Véltozatlan</translation>
+        <translation>Változatlan</translation>
     </message>
     <message>
         <location filename="../../../localeconfig.cpp" line="308"/>
         <source>Format Settings Changed</source>
-        <translation>Formátumváltozott</translation>
+        <translation>Formátum változott</translation>
     </message>
     <message>
         <source>Save the settings ? (they will take effect the next time you log in)</source>
