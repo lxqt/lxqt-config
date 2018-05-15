722baedccc60e11cd4dddd312877ff0ea119b13c
P.L. Lucas <selairi@gmail.com>
2018-05-15 16:34:56 +0200

    lxqt-config-appearance: GTK3 and GTK2 themes can be set. Warning message is shown and backup of gtk config files is done.

--- lxqt-config-appearance/configothertoolkits.cpp
+++ lxqt-config-appearance/configothertoolkits.cpp
@@ -33,6 +33,7 @@
 #include <QDir>
 #include <QFileInfo>
 #include <QFont>
+#include <QDateTime>
 
 static const char *GTK2_CONFIG = R"GTK2_CONFIG(
 # Created by lxqt-config-appearance (DO NOT EDIT!)
@@ -95,155 +96,57 @@ static QString _get_config_path(QString path)
     return path;
 }
 
+void ConfigOtherToolKits::backupGTKSettings()
+{
+    // GTK2 backup
+    {
+        QString gtkrcPath = _get_config_path("$GTK2_RC_FILES");
+        QFile file(gtkrcPath);
+        if(file.exists())
+            file.copy(gtkrcPath + "-" + QString::number(QDateTime::currentSecsSinceEpoch()) +"~");
+    }
+    // GTK3 backup
+    {
+        QString gtkrcPath = _get_config_path("$XDG_CONFIG_HOME/gtk-3.0/settings.ini");
+        QFile file(gtkrcPath);
+        if(file.exists())
+            file.copy(gtkrcPath + "-" + QString::number(QDateTime::currentSecsSinceEpoch()) + "~");
+    }
+}
+
 void ConfigOtherToolKits::setConfig()
 {
     updateConfigFromSettings();
-    setGTKConfig("2.0");
+    mConfig.styleTheme = getGTKThemeFromRCFile("3.0");
     setGTKConfig("3.0");
-    // setGTKConfig("4.0");
+    mConfig.styleTheme = getGTKThemeFromRCFile("2.0");
+    setGTKConfig("2.0");
+    setXSettingsConfig();
+}
+
+void ConfigOtherToolKits::setXSettingsConfig()
+{
+    updateConfigFromSettings();
+    mConfig.styleTheme = getGTKThemeFromRCFile("3.0");
+    if(mConfig.styleTheme.isEmpty())
+        mConfig.styleTheme = getGTKThemeFromRCFile("2.0");
     writeConfig("~/.xsettingsd", XSETTINGS_CONFIG);
     // Reload settings. xsettingsd must be installed.
     QProcess::startDetached("xsettingsd");
 }
 
-void ConfigOtherToolKits::setGTKConfig(QString version)
+void ConfigOtherToolKits::setGTKConfig(QString version, QString theme)
 {
     updateConfigFromSettings();
-    mSettings->beginGroup(QLatin1String("Themes"));
+    if(!theme.isEmpty())
+        mConfig.styleTheme = theme;
     if(version == "2.0") {
-        if(! mConfig.useGlobalTheme)
-            mConfig.styleTheme = mSettings->value("GTK2ThemeName").toString();
-        bool rcFileContainsTheme = false;
-        bool rcFileContainsFont = false;
-        bool rcFileContainsIcons = false;
-        bool rcFileContainsMenuImages = false;
-        bool rcFileContainsToolBarStyle = false;
         QString gtkrcPath = _get_config_path("$GTK2_RC_FILES");
-        QFile file(gtkrcPath);
-        if(! file.exists() )
-            writeConfig(gtkrcPath, GTK2_CONFIG);
-        else {
-            if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-                return;
-             while (!file.atEnd()) {
-                QByteArray line = file.readLine().trimmed();
-                rcFileContainsTheme = rcFileContainsTheme || line.startsWith("gtk-theme-name");
-                rcFileContainsFont = rcFileContainsFont || line.startsWith("gtk-font-name");
-                rcFileContainsIcons = rcFileContainsIcons || line.startsWith("gtk-icon-theme-name");
-                rcFileContainsMenuImages = rcFileContainsMenuImages || line.startsWith("gtk-menu-images");
-                rcFileContainsToolBarStyle = rcFileContainsToolBarStyle || line.startsWith("gtk-toolbar-style");
-            }
-            file.close();
-            QByteArray gtkrc;
-            if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-                return;
-            while (!file.atEnd()) {
-                QByteArray line = file.readLine();
-                QByteArray line_trimmed = line.trimmed();
-                if(line_trimmed.startsWith("gtk-theme-name"))
-                    gtkrc.append("gtk-theme-name = \"%1\"\n");
-                else if(line_trimmed.startsWith("gtk-font-name"))
-                    gtkrc.append("gtk-font-name = \"%3\"\n");
-                else if(line_trimmed.startsWith("gtk-icon-theme-name"))
-                    gtkrc.append("gtk-icon-theme-name = \"%2\"\n");
-                else if(line_trimmed.startsWith("gtk-menu-images"))
-                    rcFileContainsMenuImages = false;
-                else if(line_trimmed.startsWith("gtk-button-images"))
-                    rcFileContainsMenuImages = false;
-                else if(line_trimmed.startsWith("gtk-toolbar-style"))
-                    gtkrc.append("gtk-toolbar-style = %5\n");
-                else
-                    gtkrc.append(line);
-            }
-            file.close();
-            if(! rcFileContainsTheme)
-                gtkrc.append("gtk-theme-name = \"%1\"\n");
-            if(! rcFileContainsFont)
-                gtkrc.append("gtk-font-name = \"%3\"\n");
-            if(! rcFileContainsMenuImages)
-                gtkrc.append("gtk-button-images = %4\ngtk-menu-images = %4\n");
-            if(! rcFileContainsIcons)
-                gtkrc.append("gtk-icon-theme-name = \"%2\"\n");
-            if(! rcFileContainsToolBarStyle)
-                gtkrc.append("gtk-toolbar-style = %5\n");
-            writeConfig(gtkrcPath, gtkrc.data());
-        }
+        writeConfig(gtkrcPath, GTK2_CONFIG);
     } else {
-        if(! mConfig.useGlobalTheme)
-            mConfig.styleTheme = mSettings->value("GTK3ThemeName").toString();
-        bool rcFileContainsTheme = false;
-        bool rcFileContainsFont = false;
-        bool rcFileContainsIcons = false;
-        bool rcFileContainsMenuImages = false;
-        bool rcFileContainsToolBarStyle = false;
         QString gtkrcPath = _get_config_path(QString("$XDG_CONFIG_HOME/gtk-%1/settings.ini").arg(version));
-        QFile file(gtkrcPath);
-        if(! file.exists() )
-            writeConfig(gtkrcPath, GTK3_CONFIG);
-        else {
-            if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-                return;
-            bool settingsFound = false;
-            while (!file.atEnd()) {
-                QByteArray line = file.readLine().trimmed();
-                if(line.startsWith("[Settings]"))
-                    settingsFound = true;
-                else if(line.startsWith("[") && line.endsWith("]"))
-                    settingsFound = false;
-                else if(settingsFound) {
-                    rcFileContainsTheme = rcFileContainsTheme || line.startsWith("gtk-theme-name");
-                    rcFileContainsFont = rcFileContainsFont || line.startsWith("gtk-font-name");
-                    rcFileContainsIcons = rcFileContainsIcons || line.startsWith("gtk-icon-theme-name");
-                    rcFileContainsMenuImages = rcFileContainsMenuImages || line.startsWith("gtk-menu-images");
-                    rcFileContainsToolBarStyle = rcFileContainsToolBarStyle || line.startsWith("gtk-toolbar-style");
-                }
-            }
-            file.close();
-            QByteArray gtkrc;
-            if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-                return;
-            settingsFound = false;
-            while (!file.atEnd()) {
-                QByteArray line = file.readLine();
-                QByteArray line_trimmed = line.trimmed();
-                if(line_trimmed.startsWith("[Settings]")) {
-                    gtkrc.append(line);
-                    settingsFound = true;
-                    if(! rcFileContainsTheme)
-                        gtkrc.append("gtk-theme-name = %1\n");
-                    if(! rcFileContainsFont)
-                        gtkrc.append("gtk-font-name = %3\n");
-                    if(! rcFileContainsMenuImages)
-                        gtkrc.append("gtk-button-images = %4\ngtk-menu-images = %4\n");
-                    if(! rcFileContainsIcons)
-                        gtkrc.append("gtk-icon-theme-name = %2\n");
-                    if(! rcFileContainsToolBarStyle)
-                        gtkrc.append("gtk-toolbar-style = %5\n");
-                }
-                else if(line_trimmed.startsWith("[") && line_trimmed.endsWith("]"))
-                    settingsFound = false;
-                else if(settingsFound) {
-                    if(line_trimmed.startsWith("gtk-theme-name"))
-                        gtkrc.append("gtk-theme-name = %1\n");
-                    else if(line_trimmed.startsWith("gtk-font-name"))
-                        gtkrc.append("gtk-font-name = %3\n");
-                    else if(line_trimmed.startsWith("gtk-icon-theme-name"))
-                        gtkrc.append("gtk-icon-theme-name = %2\n");
-                    else if(line_trimmed.startsWith("gtk-menu-images"))
-                        gtkrc.append("gtk-button-images = %4\ngtk-menu-images = %4\n");
-                    else if(line_trimmed.startsWith("gtk-button-images"))
-                        ; // This line is not included in gtkrc
-                    else if(line_trimmed.startsWith("gtk-toolbar-style"))
-                        gtkrc.append("gtk-toolbar-style = %5\n");
-                }
-                else
-                    gtkrc.append(line);
-            }
-            file.close();
-            writeConfig(gtkrcPath, gtkrc.data());
-        }
+        writeConfig(gtkrcPath, GTK3_CONFIG);
     }
-    mSettings->endGroup();
 }
 
 void ConfigOtherToolKits::writeConfig(QString path, const char *configString)
@@ -261,64 +164,6 @@ void ConfigOtherToolKits::writeConfig(QString path, const char *configString)
         );
     out.flush();
     file.close();
-}
-
-void ConfigOtherToolKits::updateConfigFromSettings()
-{
-    mSettings->beginGroup(QLatin1String("Themes"));
-    mConfig.styleTheme = mSettings->value("GlobalThemeName").toString();
-    mConfig.useGlobalTheme = mSettings->value("GlobalThemeEnabled").toBool();
-    mSettings->endGroup();
-    mSettings->beginGroup(QLatin1String("Qt"));
-    QFont font;
-    font.fromString(mSettings->value("font").toString());
-    // Font name from: https://developer.gnome.org/pango/stable/pango-Fonts.html#pango-font-description-from-string
-    // FAMILY-LIST [SIZE]", where FAMILY-LIST is a comma separated list of families optionally terminated by a comma, 
-    // STYLE_OPTIONS is a whitespace separated list of words where each word describes one of style, variant, weight, stretch, or gravity, and 
-    // SIZE is a decimal number (size in points) or optionally followed by the unit modifier "px" for absolute size. 
-    mConfig.fontName = QString("%1%2%3 %4")
-        .arg(font.family())                                 //%1
-        .arg(font.style()==QFont::StyleNormal?"":" Italic") //%2
-        .arg(font.weight()==QFont::Normal?"":" Bold")       //%3
-        .arg(font.pointSize());                             //%4
-    mSettings->endGroup();
-    {
-        // Find Gtk theme
-        QStringList dataPaths = QStandardPaths::standardLocations(QStandardPaths::GenericDataLocation);
-        for(QString dataPath: dataPaths) {
-            QDir themesPath(dataPath + "/themes");
-            QStringList themes = themesPath.entryList(QDir::Dirs);
-            for(QString theme: themes) {
-                if(mConfig.styleTheme.toLower() == theme.toLower()) {
-                    mConfig.styleTheme = theme;
-                    break;
-                }
-            }
-        }
-    }
-    mConfig.iconTheme = mSettings->value("icon_theme").toString();
-    {
-        // Tool button style
-        QByteArray tb_style = mSettings->value("tool_button_style").toByteArray();
-        // convert toolbar style name to value
-        QMetaEnum me = QToolBar::staticMetaObject.property(QToolBar::staticMetaObject.indexOfProperty("toolButtonStyle")).enumerator();
-        int val = me.keyToValue(tb_style.constData());
-        mConfig.buttonStyle = 1;
-        switch(val) {
-            case Qt::ToolButtonIconOnly:
-                mConfig.toolButtonStyle = "GTK_TOOLBAR_ICONS";
-                break;
-            case Qt::ToolButtonTextOnly:
-                mConfig.toolButtonStyle = "GTK_TOOLBAR_TEXT";
-                mConfig.buttonStyle = 0;
-                break;
-            case Qt::ToolButtonTextUnderIcon:
-                mConfig.toolButtonStyle = "GTK_TOOLBAR_BOTH";
-                break;
-            default:
-                mConfig.toolButtonStyle = "GTK_TOOLBAR_BOTH_HORIZ";
-        }
-    }
 }
 
 QStringList ConfigOtherToolKits::getGTKThemes(QString version)
@@ -384,5 +229,46 @@ QString ConfigOtherToolKits::getGTKThemeFromRCFile(QString version)
         }
     }
     return QString();
+}
+
+void ConfigOtherToolKits::updateConfigFromSettings()
+{
+    mSettings->beginGroup(QLatin1String("Qt"));
+    QFont font;
+    font.fromString(mSettings->value("font").toString());
+    // Font name from: https://developer.gnome.org/pango/stable/pango-Fonts.html#pango-font-description-from-string
+    // FAMILY-LIST [SIZE]", where FAMILY-LIST is a comma separated list of families optionally terminated by a comma, 
+    // STYLE_OPTIONS is a whitespace separated list of words where each word describes one of style, variant, weight, stretch, or gravity, and 
+    // SIZE is a decimal number (size in points) or optionally followed by the unit modifier "px" for absolute size. 
+    mConfig.fontName = QString("%1%2%3 %4")
+        .arg(font.family())                                 //%1
+        .arg(font.style()==QFont::StyleNormal?"":" Italic") //%2
+        .arg(font.weight()==QFont::Normal?"":" Bold")       //%3
+        .arg(font.pointSize());                             //%4
+    mSettings->endGroup();
+    
+    mConfig.iconTheme = mSettings->value("icon_theme").toString();
+    {
+        // Tool button style
+        QByteArray tb_style = mSettings->value("tool_button_style").toByteArray();
+        // convert toolbar style name to value
+        QMetaEnum me = QToolBar::staticMetaObject.property(QToolBar::staticMetaObject.indexOfProperty("toolButtonStyle")).enumerator();
+        int val = me.keyToValue(tb_style.constData());
+        mConfig.buttonStyle = 1;
+        switch(val) {
+            case Qt::ToolButtonIconOnly:
+                mConfig.toolButtonStyle = "GTK_TOOLBAR_ICONS";
+                break;
+            case Qt::ToolButtonTextOnly:
+                mConfig.toolButtonStyle = "GTK_TOOLBAR_TEXT";
+                mConfig.buttonStyle = 0;
+                break;
+            case Qt::ToolButtonTextUnderIcon:
+                mConfig.toolButtonStyle = "GTK_TOOLBAR_BOTH";
+                break;
+            default:
+                mConfig.toolButtonStyle = "GTK_TOOLBAR_BOTH_HORIZ";
+        }
+    }
 }
 
--- lxqt-config-appearance/styleconfig.cpp
+++ lxqt-config-appearance/styleconfig.cpp
@@ -36,6 +36,7 @@
 #include <QMetaProperty>
 #include <QMetaEnum>
 #include <QToolBar>
+#include <QMessageBox>
 
 #ifdef Q_WS_X11
 extern void qt_x11_apply_settings_in_all_apps();
@@ -52,12 +53,11 @@ StyleConfig::StyleConfig(LXQt::Settings* settings, QSettings* qtSettings, QWidge
 
     initControls();
     
-    connect(ui->globalThemeComboBox, SIGNAL(activated(const QString &)), this, SLOT(globalThemeSelected(const QString &)));
-    connect(ui->gtkComboBox, SIGNAL(activated(const QString &)), this, SLOT(gtkStyleSelected(const QString &)));
+    connect(ui->gtk2ComboBox, SIGNAL(activated(const QString &)), this, SLOT(gtk2StyleSelected(const QString &)));
+    connect(ui->gtk3ComboBox, SIGNAL(activated(const QString &)), this, SLOT(gtk3StyleSelected(const QString &)));
     connect(ui->qtComboBox, SIGNAL(activated(const QString &)), this, SLOT(qtStyleSelected(const QString &)));
     
     connect(ui->advancedOptionsGroupBox, SIGNAL(toggled(bool)), this, SLOT(showAdvancedOptions(bool)));
-    connect(ui->advancedOptionsGroupBox, SIGNAL(toggled(bool)), this, SLOT(setAdvancedSettings(bool)));
     
     connect(ui->toolButtonStyle, SIGNAL(currentIndexChanged(int)), SLOT(toolButtonStyleSelected(int)));
     connect(ui->singleClickActivate, SIGNAL(toggled(bool)), SLOT(singleClickActivateToggled(bool)));
@@ -77,30 +77,16 @@ void StyleConfig::initControls()
     QStringList qtThemes = QStyleFactory::keys();
     QStringList gtk2Themes = mConfigOtherToolKits->getGTKThemes("2.0");
     QStringList gtk3Themes = mConfigOtherToolKits->getGTKThemes("3.0");
-    QStringList globalThemeList;
-    // Common themes
-    for(QString qtTheme : qtThemes) {
-        if( !globalThemeList.contains(qtTheme) &&  gtk3Themes.contains(qtTheme)  &&  gtk2Themes.contains(qtTheme) )
-            globalThemeList.append(qtTheme);
-    }
-    if(qtThemes.contains("gtk2")) {
-        // GTK2 and GTK3 themes. Qt style will be set to gtk2.
-        for(QString theme : gtk2Themes) {
-            if( !globalThemeList.contains(theme) &&  gtk3Themes.contains(theme) )
-                globalThemeList.append(theme);
-        }
-    }
     
-    ui->globalThemeComboBox->addItems( globalThemeList );
-    mSettings->beginGroup(QLatin1String("Themes"));
-    ui->globalThemeComboBox->setCurrentText(mSettings->value("GlobalThemeName").toString());
-    if(!mSettings->contains("GlobalThemeEnabled"))
-        mSettings->setValue("GlobalThemeEnabled", true);
-    bool GlobalThemeEnabledd = mSettings->value("GlobalThemeEnabled").toBool();
-    mSettings->endGroup();
-    
-    showAdvancedOptions(!GlobalThemeEnabledd);
-    ui->advancedOptionsGroupBox->setChecked(!GlobalThemeEnabledd);
+    // mSettings->beginGroup(QLatin1String("Themes"));
+    // ui->globalThemeComboBox->setCurrentText(mSettings->value("GlobalThemeName").toString());
+    // if(!mSettings->contains("GlobalThemeEnabled"))
+    //     mSettings->setValue("GlobalThemeEnabled", true);
+    // bool GlobalThemeEnabledd = mSettings->value("GlobalThemeEnabled").toBool();
+    // mSettings->endGroup();
+    // 
+    // showAdvancedOptions(!GlobalThemeEnabledd);
+    // ui->advancedOptionsGroupBox->setChecked(!GlobalThemeEnabledd);
 
     // read other widget related settings from LXQt settings.
     QByteArray tb_style = mSettings->value("tool_button_style").toByteArray();
@@ -119,48 +105,16 @@ void StyleConfig::initControls()
     ui->qtComboBox->addItems(qtThemes);
     
     // Fill GTK themes
-    ui->gtkComboBox->addItems(gtk3Themes);
+    ui->gtk2ComboBox->addItems(gtk2Themes);
+    ui->gtk3ComboBox->addItems(gtk3Themes);
     
-    mSettings->beginGroup(QLatin1String("Themes"));
-    if(! mSettings->contains("GTK3ThemeName")) {
-        // Set default values
-        QString themeName = mConfigOtherToolKits->getGTKThemeFromRCFile("3.0");
-        mSettings->setValue("GTK3ThemeName", themeName);
-        mSettings->setValue("GTK2ThemeName", themeName);
-    }
-    if(! mSettings->contains("QtThemeName")) {
-        // Set default values
-        mSettings->endGroup();
-        mQtSettings->beginGroup(QLatin1String("Qt"));
-        QString styleName = mQtSettings->value("style").toString();
-        mQtSettings->endGroup();
-        mSettings->beginGroup(QLatin1String("Themes"));
-        mSettings->setValue("QtThemeName", styleName);
-    }
-    ui->gtkComboBox->setCurrentText(mSettings->value("GTK3ThemeName").toString());
-    ui->qtComboBox->setCurrentText(mSettings->value("QtThemeName").toString());
+    ui->gtk2ComboBox->setCurrentText(mConfigOtherToolKits->getGTKThemeFromRCFile("2.0"));
+    ui->gtk3ComboBox->setCurrentText(mConfigOtherToolKits->getGTKThemeFromRCFile("3.0"));
+    mSettings->beginGroup(QLatin1String("Qt"));
+    ui->qtComboBox->setCurrentText(mSettings->value("style").toString());
     mSettings->endGroup();
 
     update();
-}
-
-void StyleConfig::globalThemeSelected(const QString &themeName)
-{
-    mSettings->beginGroup(QLatin1String("Themes"));
-    mSettings->setValue("GlobalThemeName", themeName);
-    mSettings->endGroup();
-    mSettings->sync();
-
-    mQtSettings->beginGroup(QLatin1String("Qt"));
-    mQtSettings->setValue("style", QStyleFactory::keys().contains(themeName) ? themeName : "gtk2");
-    mQtSettings->endGroup();
-    mQtSettings->sync();
-    
-#ifdef Q_WS_X11
-    qt_x11_apply_settings_in_all_apps();
-#endif
-    
-    mConfigOtherToolKits->setConfig();
 }
 
 void StyleConfig::toolButtonStyleSelected(int index)
@@ -190,49 +144,29 @@ void StyleConfig::qtStyleSelected(const QString &themeName)
     mQtSettings->setValue("style", themeName);
     mQtSettings->endGroup();
     mQtSettings->sync();
-    mSettings->beginGroup(QLatin1String("Themes"));
-    mSettings->setValue("QtThemeName", themeName);
-    mSettings->endGroup();
-    mSettings->sync();
 }
 
-void StyleConfig::gtkStyleSelected(const QString &themeName)
+void StyleConfig::gtk3StyleSelected(const QString &themeName)
 {
-    mSettings->beginGroup(QLatin1String("Themes"));
-    mSettings->setValue("GTK3ThemeName", themeName);
-    mSettings->setValue("GTK2ThemeName", themeName);
-    mSettings->endGroup();
-    mSettings->sync();
+    mConfigOtherToolKits->setGTKConfig("3.0", themeName);
+    mConfigOtherToolKits->setXSettingsConfig();
+}
 
-    mConfigOtherToolKits->setConfig();
+void StyleConfig::gtk2StyleSelected(const QString &themeName)
+{
+    mConfigOtherToolKits->setGTKConfig("2.0", themeName);
+    mConfigOtherToolKits->setXSettingsConfig();
 }
 
 void StyleConfig::showAdvancedOptions(bool on)
 {
-    ui->globalThemeLabel->setEnabled(!on);
-    ui->globalThemeComboBox->setEnabled(!on);
-}
-
-void StyleConfig::setAdvancedSettings(bool on)
-{
-    mSettings->beginGroup(QLatin1String("Themes"));
-    mSettings->setValue("GlobalThemeEnabled", !on);
-    mSettings->endGroup();
-    mSettings->sync();
-    if(!on) {
-        // Use global theme
-        mSettings->beginGroup(QLatin1String("Themes"));
-        QString globalThemeName = mSettings->value("GlobalThemeName").toString();
-        mSettings->endGroup();
-        globalThemeSelected(globalThemeName);
-        mConfigOtherToolKits->setConfig();
-    } else {
-        mSettings->beginGroup(QLatin1String("Themes"));
-        QString gtk2ThemeName = mSettings->value("GTK2ThemeName").toString();
-        QString gtk3ThemeName = mSettings->value("GTK3ThemeName").toString();
-        QString qtThemeName = mSettings->value("QtThemeName").toString();
-        mSettings->endGroup();
-        gtkStyleSelected(gtk3ThemeName);
-        qtStyleSelected(qtThemeName);
+    if(on) {
+        QMessageBox::warning(this, tr("GTK themes"),
+            tr("~/.gtkrc-2.0 and ~/.config/settings.ini will be changed. "
+            "Please, if you have changed them, you should save them.<br/>"
+            "Select the same style/theme (if available) in all lists to attempt uniform theming.<br/>"
+            "Make sure 'xsettingsd' is installed to help GTK applications apply themes on the fly."),
+            QMessageBox::Ok);
+        mConfigOtherToolKits->backupGTKSettings();
     }
 }
