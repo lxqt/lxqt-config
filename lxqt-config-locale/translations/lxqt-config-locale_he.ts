18de92b12190cab4a502cc0fe9d1c7b18e2bc52a
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-13 04:18:29 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: LXQt/LXQt Config (Locale)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-locale/he/

--- lxqt-config-locale/translations/lxqt-config-locale_he.ts
+++ lxqt-config-locale/translations/lxqt-config-locale_he.ts
@@ -6,92 +6,92 @@
     <message>
         <location filename="../localeconfig.ui" line="47"/>
         <source>Re&amp;gion:</source>
-        <translation></translation>
+        <translation>א&amp;זור:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="76"/>
         <source>De&amp;tailed Settings</source>
-        <translation></translation>
+        <translation>הגדרות מ&amp;פורטות</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="83"/>
         <source>&amp;Numbers:</source>
-        <translation></translation>
+        <translation>מ&amp;ספרים:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="112"/>
         <source>&amp;Time:</source>
-        <translation></translation>
+        <translation>&amp;זמן:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="141"/>
         <source>Currenc&amp;y:</source>
-        <translation></translation>
+        <translation>מ&amp;טבע:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="170"/>
         <source>Measurement &amp;Units:</source>
-        <translation></translation>
+        <translation>י&amp;חידות מידה:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="199"/>
         <source>Co&amp;llation and Sorting:</source>
-        <translation></translation>
+        <translation>&amp;איסוף ומיון:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="238"/>
         <source>&lt;b&gt;Examples&lt;/b&gt;</source>
-        <translation></translation>
+        <translation>&lt;b&gt;דוגמאות&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="248"/>
         <source>Numbers:</source>
-        <translation></translation>
+        <translation>מספרים:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="268"/>
         <source>Time:</source>
-        <translation></translation>
+        <translation>זמן:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="288"/>
         <source>Currency:</source>
-        <translation></translation>
+        <translation>מטבע:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="308"/>
         <source>Measurement Units:</source>
-        <translation></translation>
+        <translation>יחידות מידה:</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="125"/>
         <source>No change</source>
-        <translation></translation>
+        <translation>אין שינוי</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="314"/>
         <source>Format Settings Changed</source>
-        <translation></translation>
+        <translation>הגדרות התבנית השתנו</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="315"/>
         <source>Do you want to save your changes? They will take effect the next time you log in.</source>
-        <translation></translation>
+        <translation>לשמור את השינויים? הם ייכנסו לתוקף עם הכניסה הבאה.</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="436"/>
         <source>Imperial UK</source>
-        <translation></translation>
+        <translation>אימפריאלית בריטית</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="440"/>
         <source>Imperial US</source>
-        <translation></translation>
+        <translation>אימפריאלית אמריקאית</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="444"/>
         <source>Metric</source>
-        <translation></translation>
+        <translation>מטרית</translation>
     </message>
 </context>
 <context>
@@ -99,12 +99,12 @@
     <message>
         <location filename="../main.cpp" line="50"/>
         <source>LXQt Locale Configuration</source>
-        <translation></translation>
+        <translation>הגדרות מקומיות של LXQt</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="55"/>
         <source>Locale Settings</source>
-        <translation></translation>
+        <translation>הגדרות מקומיות</translation>
     </message>
 </context>
 </TS>
