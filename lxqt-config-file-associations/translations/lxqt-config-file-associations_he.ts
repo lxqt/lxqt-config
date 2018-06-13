40a286d53375fc3a857f422b779fae57b9910e7c
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-13 04:02:00 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Config (File-Associations)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-file-associations/he/

--- lxqt-config-file-associations/translations/lxqt-config-file-associations_he.ts
+++ lxqt-config-file-associations/translations/lxqt-config-file-associations_he.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../applicationchooser.ui" line="14"/>
         <source>ApplicationChooser</source>
-        <translation></translation>
+        <translation>בוחר יישומים</translation>
     </message>
     <message>
         <location filename="../applicationchooser.ui" line="46"/>
         <source>Pick an application for:</source>
-        <translation></translation>
+        <translation>נא לבחור יישום עבור:</translation>
     </message>
     <message>
         <location filename="../applicationchooser.ui" line="110"/>
@@ -21,7 +21,7 @@
     <message>
         <location filename="../applicationchooser.ui" line="115"/>
         <source>Applications that handle JPEG</source>
-        <translation></translation>
+        <translation>יישומים שמטפלים ב־JPEG</translation>
     </message>
     <message>
         <location filename="../applicationchooser.ui" line="124"/>
@@ -29,23 +29,23 @@
         <location filename="../applicationchooser.ui" line="139"/>
         <location filename="../applicationchooser.ui" line="144"/>
         <source>New Subitem</source>
-        <translation></translation>
+        <translation>תת־פריט חדש</translation>
     </message>
     <message>
         <location filename="../applicationchooser.ui" line="135"/>
         <location filename="../applicationchooser.cpp" line="113"/>
         <source>Other applications</source>
-        <translation></translation>
+        <translation>יישומים אחרים</translation>
     </message>
     <message>
         <location filename="../applicationchooser.cpp" line="114"/>
         <source>Applications that handle %1</source>
-        <translation></translation>
+        <translation>יישומים שמטפלים ב־%1</translation>
     </message>
     <message>
         <location filename="../applicationchooser.cpp" line="151"/>
         <source>No applications found</source>
-        <translation></translation>
+        <translation>לא נמצאו יישומים</translation>
     </message>
 </context>
 <context>
@@ -54,17 +54,17 @@
         <location filename="../mimetypeviewer.cpp" line="156"/>
         <location filename="../mimetypeviewer.cpp" line="207"/>
         <source>None</source>
-        <translation></translation>
+        <translation>אין</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.cpp" line="203"/>
         <source>&amp;Change...</source>
-        <translation></translation>
+        <translation>ה&amp;חלפה…</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.cpp" line="208"/>
         <source>&amp;Choose...</source>
-        <translation></translation>
+        <translation>ה&amp;חלפה…</translation>
     </message>
 </context>
 <context>
@@ -72,52 +72,52 @@
     <message>
         <location filename="../mimetypeviewer.ui" line="20"/>
         <source>File Associations</source>
-        <translation></translation>
+        <translation>שיוכי קבצים</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="56"/>
         <source>Default application</source>
-        <translation></translation>
+        <translation>יישום בררת מחדל</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="68"/>
         <source>&amp;Choose...</source>
-        <translation></translation>
+        <translation>&amp;בחירה…</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="117"/>
         <source>TextLabel</source>
-        <translation></translation>
+        <translation>תווית טקסט</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="124"/>
         <source>None</source>
-        <translation></translation>
+        <translation>אין</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="144"/>
         <source>Patterns</source>
-        <translation></translation>
+        <translation>תבניות</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="156"/>
         <source>*.txt *.xml</source>
-        <translation></translation>
+        <translation>‎*.txt *.xml</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="173"/>
         <source>MIME type</source>
-        <translation></translation>
+        <translation>סוג MIME</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="191"/>
         <source>Icon</source>
-        <translation></translation>
+        <translation>סמל</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="198"/>
         <source>Description</source>
-        <translation></translation>
+        <translation>תיאור</translation>
     </message>
 </context>
 </TS>
