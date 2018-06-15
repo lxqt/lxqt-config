3ce17740b4b45cf933e42afdf843c08b2391b4c0
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-15 06:02:01 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (44 of 44 strings)
    
    Translation: LXQt/LXQt Config (Appearance)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-appearance/he/

--- lxqt-config-appearance/translations/lxqt-config-appearance_he.ts
+++ lxqt-config-appearance/translations/lxqt-config-appearance_he.ts
@@ -6,103 +6,103 @@
     <message>
         <location filename="../fontsconfig.ui" line="23"/>
         <source>Font</source>
-        <translation></translation>
+        <translation>גופן</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="30"/>
         <source>Default font for user interface</source>
-        <translation></translation>
+        <translation>גופן בררת המחדל למנשק המשתמש</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="36"/>
         <source>Font name:</source>
-        <translation></translation>
+        <translation>שם הגופן:</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="46"/>
         <source>Style:</source>
-        <translation></translation>
+        <translation>סגנון:</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="53"/>
         <source>Point size:</source>
-        <translation></translation>
+        <translation>גודל נקודה:</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="68"/>
         <source>Normal</source>
-        <translation></translation>
+        <translation>רגיל</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="73"/>
         <source>Bold</source>
-        <translation></translation>
+        <translation>מודגש</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="78"/>
         <source>Italic</source>
-        <translation></translation>
+        <translation>נטוי</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="83"/>
         <source>Bold Italic</source>
-        <translation></translation>
+        <translation>מודגש נטוי</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="94"/>
         <source>The following settings only affect newly started applications</source>
-        <translation></translation>
+        <translation>ההגדרות הבאות חלות רק על יישומים חדשים שנפתחים</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="100"/>
         <source>Use antialias fonts</source>
-        <translation></translation>
+        <translation>שימוש בהחלקת קצוות גופנים</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="107"/>
         <source>Font hinting style:</source>
-        <translation></translation>
+        <translation>סגנון עיבוד גופן:</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="118"/>
         <location filename="../fontsconfig.ui" line="183"/>
         <source>None</source>
-        <translation></translation>
+        <translation>ללא</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="123"/>
         <source>Slight</source>
-        <translation></translation>
+        <translation>עדין</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="128"/>
         <source>Medium</source>
-        <translation></translation>
+        <translation>בינוני</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="133"/>
         <source>Full</source>
-        <translation></translation>
+        <translation>מלא</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="141"/>
         <source>Font hinting</source>
-        <translation></translation>
+        <translation>עיבוד גופן</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="148"/>
         <source>Resolution (DPI):</source>
-        <translation></translation>
+        <translation>רזולוציה (DPI):</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="155"/>
         <source>Autohint</source>
-        <translation></translation>
+        <translation>עיבוד אוטומטי</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="172"/>
         <source>Subpixel antialiasing:</source>
-        <translation></translation>
+        <translation>החלקת תת־פיקסלים:</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="188"/>
@@ -130,22 +130,22 @@
     <message>
         <location filename="../iconthemeconfig.ui" line="14"/>
         <source>LXQt Appearance Configuration</source>
-        <translation></translation>
+        <translation>הגדרות מראה של LXQt</translation>
     </message>
     <message>
         <location filename="../iconthemeconfig.ui" line="26"/>
         <source>Icons Theme</source>
-        <translation></translation>
+        <translation>ערכת סמלים</translation>
     </message>
     <message>
         <location filename="../iconthemeconfig.ui" line="87"/>
         <source>The KDE extension of XDG icon themes -&gt; FollowsColorScheme</source>
-        <translation></translation>
+        <translation>הרחבת ה־KDE של ערכות הסמלים של XDG‏ -&gt; FollowsColorScheme</translation>
     </message>
     <message>
         <location filename="../iconthemeconfig.ui" line="90"/>
         <source>Colorize icons based on widget style (palette)</source>
-        <translation></translation>
+        <translation>צביעת הסמלים על בסיס סגנון וידג׳ט (ערכת צבעים)</translation>
     </message>
 </context>
 <context>
@@ -153,12 +153,12 @@
     <message>
         <location filename="../lxqtthemeconfig.ui" line="23"/>
         <source>LXQt Theme</source>
-        <translation></translation>
+        <translation>ערכת עיצוב של LXQt</translation>
     </message>
     <message>
         <location filename="../lxqtthemeconfig.ui" line="57"/>
         <source>Override user-defined wallpaper</source>
-        <translation></translation>
+        <translation>דריסת רקע שולחן העבודה שהוגדר על ידי המשתמש</translation>
     </message>
 </context>
 <context>
@@ -166,32 +166,32 @@
     <message>
         <location filename="../main.cpp" line="57"/>
         <source>LXQt Appearance Configuration</source>
-        <translation></translation>
+        <translation>הגדרות מראה של LXQt</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="63"/>
         <source>Widget Style</source>
-        <translation></translation>
+        <translation>סגנון וידג׳טים</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="67"/>
         <source>Icons Theme</source>
-        <translation></translation>
+        <translation>ערכת סמלים</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="71"/>
         <source>LXQt Theme</source>
-        <translation></translation>
+        <translation>ערכת עיצוב של LXQt</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="75"/>
         <source>Font</source>
-        <translation></translation>
+        <translation>גופן</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="80"/>
         <source>Cursor</source>
-        <translation></translation>
+        <translation>סמן עכבר</translation>
     </message>
 </context>
 <context>
@@ -199,42 +199,42 @@
     <message>
         <location filename="../styleconfig.ui" line="23"/>
         <source>Widget Style</source>
-        <translation></translation>
+        <translation>סגנון וידג׳טים</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="54"/>
         <source>Toolbar button style:</source>
-        <translation></translation>
+        <translation>סגנון כפתורי סרגל כלים:</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="62"/>
         <source>Only display the icon</source>
-        <translation></translation>
+        <translation>להציג סמל בלבד</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="67"/>
         <source>Only display the text</source>
-        <translation></translation>
+        <translation>להציג טקסט בלבד</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="72"/>
         <source>The text appears beside the icon</source>
-        <translation></translation>
+        <translation>טקסט לצד הסמל</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="77"/>
         <source>The text appears under the icon</source>
-        <translation></translation>
+        <translation>טקסט מתחת לסמל</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="82"/>
         <source>Default</source>
-        <translation></translation>
+        <translation>בררת מחדל</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="90"/>
         <source>Activate item on single click</source>
-        <translation></translation>
+        <translation>הפעלת פריט בלחיצה אחת</translation>
     </message>
 </context>
 </TS>
