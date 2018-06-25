402e40ac115659dac289e5a1f47830e8272f0b54
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-25 10:16:05 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: LXQt/LXQt Config (Brightness)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-brightness/he/

--- lxqt-config-brightness/translations/lxqt-config-brightness_he.ts
+++ lxqt-config-brightness/translations/lxqt-config-brightness_he.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../brightnesssettings.ui" line="14"/>
         <source>Brightness Settings</source>
-        <translation></translation>
+        <translation>הגדרות בהירות</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="24"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;הגדרות תאורה אחורית ובהירות:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;יש להשתמש במקשי החצים כדי לשנות הגדרות.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="41"/>
         <source>Require confirmation after settings change</source>
-        <translation></translation>
+        <translation>דרישת אישור לאחר שינוי הגדרות</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="88"/>
         <source>Brightness settings changed</source>
-        <translation></translation>
+        <translation>הגדרות הבהירות השתנו</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="89"/>
         <source>Confirmation required. Are the settings correct?</source>
-        <translation></translation>
+        <translation>נדרש אישור. ההגדרות האלו נכונות?</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../outputwidget.ui" line="38"/>
         <source>&lt;b&gt;Output:&lt;/b&gt;</source>
-        <translation></translation>
+        <translation>&lt;b&gt;פלט:&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../outputwidget.ui" line="67"/>
         <source>Right click to reset</source>
-        <translation></translation>
+        <translation>כפתור ימני לאיפוס</translation>
     </message>
 </context>
 </TS>
