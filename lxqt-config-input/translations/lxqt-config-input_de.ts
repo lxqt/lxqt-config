a35530fcddcf4789479d5baa951f0a3bdd6a12f5
Alf Gaida <agaida@siduction.org>
2018-06-26 23:45:06 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (49 of 49 strings)
    
    Translation: LXQt/LXQt Config (Input)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-input/de/

--- lxqt-config-input/translations/lxqt-config-input_de.ts
+++ lxqt-config-input/translations/lxqt-config-input_de.ts
@@ -102,12 +102,12 @@
     <message>
         <location filename="../keyboardlayoutconfig.ui" line="77"/>
         <source>Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach oben</translation>
     </message>
     <message>
         <location filename="../keyboardlayoutconfig.ui" line="84"/>
         <source>Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach unten</translation>
     </message>
     <message>
         <source>up</source>
