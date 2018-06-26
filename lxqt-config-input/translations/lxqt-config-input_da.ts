581a19975d0c34fd6895c2d8ad83a42e72e9ad90
scootergrisen <scootergrisen@gmail.com>
2018-06-26 15:43:21 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (49 of 49 strings)
    
    Translation: LXQt/LXQt Config (Input)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-input/da/

--- lxqt-config-input/translations/lxqt-config-input_da.ts
+++ lxqt-config-input/translations/lxqt-config-input_da.ts
@@ -102,12 +102,12 @@
     <message>
         <location filename="../keyboardlayoutconfig.ui" line="77"/>
         <source>Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Op</translation>
     </message>
     <message>
         <location filename="../keyboardlayoutconfig.ui" line="84"/>
         <source>Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Ned</translation>
     </message>
     <message>
         <source>up</source>
