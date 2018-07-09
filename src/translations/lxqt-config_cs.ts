ab1a658d6c18998ebdca6bd9d63b8ba69d4f62a8
Helix <HelixBot@users.noreply.github.com>
2018-07-09 06:44:51 +0200

    Translated using Weblate (Czech) (#210)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: LXQt/LXQt Config
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config/cs/

--- src/translations/lxqt-config_cs.ts
+++ src/translations/lxqt-config_cs.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>Configuration Center</source>
-        <translation>Nastavovací středisko</translation>
+        <translation>Centrum nastavování</translation>
     </message>
 </context>
 </TS>
