3b6f36b3489ce7d143097f5160800d9498dde5bb
Safa Alfulaij <safa1996alfulaij@gmail.com>
2018-07-16 09:14:26 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: LXQt/LXQt Config
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config/ar/

--- src/translations/lxqt-config_ar.ts
+++ src/translations/lxqt-config_ar.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>Configuration Center</source>
-        <translation>مركز الضّبط</translation>
+        <translation>مركز الضبط</translation>
     </message>
 </context>
 </TS>
