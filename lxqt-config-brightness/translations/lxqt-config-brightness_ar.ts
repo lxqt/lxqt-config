03994ec35e542dbfb21945c45ee501bd5ee849ae
Safa Alfulaij <safa1996alfulaij@gmail.com>
2018-07-16 10:05:48 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: LXQt/LXQt Config (Brightness)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-brightness/ar/

--- lxqt-config-brightness/translations/lxqt-config-brightness_ar.ts
+++ lxqt-config-brightness/translations/lxqt-config-brightness_ar.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../brightnesssettings.ui" line="14"/>
         <source>Brightness Settings</source>
-        <translation>إعدادات السّطوع</translation>
+        <translation>إعدادات السطوع</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="24"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;إعدادات الإضاءة الخلفيّة والسّطوع:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;استخدم مفاتيح الأسهم لتغيير الإعدادات.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;إعدادات الإضاءة الخلفية والسطوع:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;استخدم مفاتيح الأسهم لتغيير الإعدادات.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="41"/>
         <source>Require confirmation after settings change</source>
-        <translation>طلب تأكيد بعد تغيير الإعدادات</translation>
+        <translation>اطلب التأكيد بعد تغيّر الإعدادات</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="88"/>
         <source>Brightness settings changed</source>
-        <translation>تم تعديل إعدادات السّطوع</translation>
+        <translation>تغيّرت إعدادات السطوع</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="89"/>
         <source>Confirmation required. Are the settings correct?</source>
-        <translation>التأكيد مطلوب. هل الإعدادات صحيحة ؟</translation>
+        <translation>التأكيد مطلوب. هل الإعدادات صحيحة؟</translation>
     </message>
 </context>
 <context>
@@ -39,7 +39,7 @@
     <message>
         <location filename="../outputwidget.ui" line="67"/>
         <source>Right click to reset</source>
-        <translation>النقر بزر اليمين لإعادة التعيين</translation>
+        <translation>انقر باليمين للتصفير</translation>
     </message>
 </context>
 </TS>
