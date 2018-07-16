8cf33404eab2bf160ddc8200c8551599b19e14c0
p-bo <pavel.borecki@gmail.com>
2018-07-16 13:14:11 +0000

    Translated using Weblate (Čeština)
    Currently translated at 100,0% (11 of 11 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/cs/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_cs.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_cs.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../selectwnd.ui" line="14"/>
         <source>LXQt Mouse Theme Configuration</source>
-        <translation>Nastavení vzhledu ukazovátka myši</translation>
+        <translation>Nastavení vzhledu ukazatele myši</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="20"/>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation>Vyberte vzhled ukazovátka, který chcete používat (vyzkoušíte najetím myší na náhled). &lt;b&gt;Po provedení změny je třeba restartovat sezení LXQt&lt;/b&gt;:</translation>
+        <translation>Vyberte vzhled ukazatele, který chcete používat (vyzkoušíte najetím myší na náhled). &lt;b&gt;Po provedení změny je třeba restartovat sezení LXQt&lt;/b&gt;:</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="81"/>
@@ -21,7 +21,7 @@
     <message>
         <location filename="../selectwnd.ui" line="88"/>
         <source>&amp;Remove Theme</source>
-        <translation>&amp;Odstranit vzhled</translation>
+        <translation>Odst&amp;ranit vzhled</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="212"/>
@@ -44,12 +44,12 @@
     <message>
         <location filename="../warninglabel.ui" line="44"/>
         <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
-        <translation>LXQt se nepodařilo najít žádný vzhled ukazovátka. Bude se používat výchozí vzhled ukazovátka X11. LXQt hledalo v následujících složkách:</translation>
+        <translation>LXQt se nepodařilo najít žádný vzhled ukazatele. Bude se používat výchozí vzhled graf. serveru X11. LXQt hledalo v následujících složkách:</translation>
     </message>
     <message>
         <location filename="../warninglabel.ui" line="54"/>
         <source>Show...</source>
-        <translation>Ukázat…</translation>
+        <translation>Zobrazit…</translation>
     </message>
 </context>
 <context>
