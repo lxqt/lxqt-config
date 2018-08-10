481727f8406adcbd6fe2ef2548c720313b1a680b
Carl Schwan <schwancarl@protonmail.com>
2018-08-10 12:27:12 +0000

    Translated using Weblate (French)
    Currently translated at 100,0% (7 of 7 strings)
    
    Translation: LXQt/LXQt Config (Brightness)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-brightness/fr/

--- lxqt-config-brightness/translations/lxqt-config-brightness_fr.ts
+++ lxqt-config-brightness/translations/lxqt-config-brightness_fr.ts
@@ -39,7 +39,7 @@
     <message>
         <location filename="../outputwidget.ui" line="67"/>
         <source>Right click to reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Click droit pour réinitialiser</translation>
     </message>
 </context>
 </TS>
