1dceed214011ee517d0b8f9c638c64694edcd4a8
Yaron Shahrabani <sh.yaron@gmail.com>
2018-09-05 04:32:12 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 86.3% (44 of 51 strings)
    
    Translation: LXQt/LXQt Config (Appearance)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-appearance/he/

--- lxqt-config-appearance/translations/lxqt-config-appearance_he.ts
+++ lxqt-config-appearance/translations/lxqt-config-appearance_he.ts
@@ -191,7 +191,7 @@
     <message>
         <location filename="../main.cpp" line="87"/>
         <source>Cursor</source>
-        <translation>סמן עכבר</translation>
+        <translation>סמן</translation>
     </message>
 </context>
 <context>
