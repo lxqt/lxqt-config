1a8b64849c29ce45cd84e46e642daaaf03cb817b
Einar Mostad <einar@mostad.eu>
2018-10-26 21:47:48 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (51 of 51 strings)
    
    Translation: LXQt/LXQt Config (Appearance)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-appearance/nb_NO/

--- lxqt-config-appearance/translations/lxqt-config-appearance_nb_NO.ts
+++ lxqt-config-appearance/translations/lxqt-config-appearance_nb_NO.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../fontsconfig.ui" line="23"/>
         <source>Font</source>
-        <translation></translation>
+        <translation>Skrift</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="30"/>
         <source>Default font for user interface</source>
-        <translation></translation>
+        <translation>Standard skrift for brukergrensesnittet</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="36"/>
         <source>Font name:</source>
-        <translation></translation>
+        <translation>Skriftsnavn:</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="46"/>
         <source>Style:</source>
-        <translation></translation>
+        <translation>Stil:</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="53"/>
         <source>Point size:</source>
-        <translation></translation>
+        <translation>Skriftstørrelse:</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="68"/>
@@ -36,43 +36,43 @@
     <message>
         <location filename="../fontsconfig.ui" line="73"/>
         <source>Bold</source>
-        <translation></translation>
+        <translation>Fet</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="78"/>
         <source>Italic</source>
-        <translation></translation>
+        <translation>Kursiv</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="83"/>
         <source>Bold Italic</source>
-        <translation></translation>
+        <translation>Fet kursiv</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="94"/>
         <source>The following settings only affect newly started applications</source>
-        <translation></translation>
+        <translation>De følgende innstillingene påvirker bare nystartede programmer</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="100"/>
         <source>Use antialias fonts</source>
-        <translation></translation>
+        <translation>Bruk antialiasfonter</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="107"/>
         <source>Font hinting style:</source>
-        <translation></translation>
+        <translation>Skriftshinting stil:</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="118"/>
         <location filename="../fontsconfig.ui" line="183"/>
         <source>None</source>
-        <translation></translation>
+        <translation>Ingen</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="123"/>
         <source>Slight</source>
-        <translation></translation>
+        <translation>Litt</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="128"/>
@@ -87,12 +87,12 @@
     <message>
         <location filename="../fontsconfig.ui" line="141"/>
         <source>Font hinting</source>
-        <translation></translation>
+        <translation>Skriftshinting</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="148"/>
         <source>Resolution (DPI):</source>
-        <translation></translation>
+        <translation>Oppløsning (DPI):</translation>
     </message>
     <message>
         <location filename="../fontsconfig.ui" line="155"/>
@@ -130,22 +130,22 @@
     <message>
         <location filename="../iconthemeconfig.ui" line="14"/>
         <source>LXQt Appearance Configuration</source>
-        <translation></translation>
+        <translation>LXQt Utseendeinnstillinger</translation>
     </message>
     <message>
         <location filename="../iconthemeconfig.ui" line="26"/>
         <source>Icons Theme</source>
-        <translation></translation>
+        <translation>Symboltema</translation>
     </message>
     <message>
         <location filename="../iconthemeconfig.ui" line="87"/>
         <source>The KDE extension of XDG icon themes -&gt; FollowsColorScheme</source>
-        <translation></translation>
+        <translation>KDE tillegget for XDG symboltemaer -&gt; FollowsColorScheme</translation>
     </message>
     <message>
         <location filename="../iconthemeconfig.ui" line="90"/>
         <source>Colorize icons based on widget style (palette)</source>
-        <translation></translation>
+        <translation>Fargelegg symboler basert på widgetstil (palett)</translation>
     </message>
 </context>
 <context>
@@ -153,12 +153,12 @@
     <message>
         <location filename="../lxqtthemeconfig.ui" line="23"/>
         <source>LXQt Theme</source>
-        <translation></translation>
+        <translation>LXQt Tema</translation>
     </message>
     <message>
         <location filename="../lxqtthemeconfig.ui" line="57"/>
         <source>Override user-defined wallpaper</source>
-        <translation></translation>
+        <translation>Bruk i stedet for brukerdefinert skrivebordsbakgrunn</translation>
     </message>
 </context>
 <context>
@@ -166,32 +166,32 @@
     <message>
         <location filename="../main.cpp" line="58"/>
         <source>LXQt Appearance Configuration</source>
-        <translation></translation>
+        <translation>LXQt Utseendeinnstillinger</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="67"/>
         <source>Widget Style</source>
-        <translation></translation>
+        <translation>Widgetstil</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="72"/>
         <source>Icons Theme</source>
-        <translation></translation>
+        <translation>Symboltema</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="77"/>
         <source>LXQt Theme</source>
-        <translation></translation>
+        <translation>LXQt tema</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="81"/>
         <source>Font</source>
-        <translation></translation>
+        <translation>Skrift</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="87"/>
         <source>Cursor</source>
-        <translation></translation>
+        <translation>Peker</translation>
     </message>
 </context>
 <context>
@@ -199,22 +199,22 @@
     <message>
         <location filename="../styleconfig.ui" line="23"/>
         <source>Widget Style</source>
-        <translation></translation>
+        <translation>Widget stil</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="42"/>
         <source>Set GTK themes (GTK configuration files will be overwritten!)</source>
-        <translation></translation>
+        <translation>Velg GTK tema (GTK-innstillingsfiler blir overskrevet!)</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="62"/>
         <source>GTK 3 Theme</source>
-        <translation></translation>
+        <translation>GTK 3 tema</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="72"/>
         <source>GTK 2 Theme</source>
-        <translation></translation>
+        <translation>GTK 2 tema</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="82"/>
@@ -224,47 +224,52 @@
 
 Make sure &apos;xsettingsd&apos; is installed to help GTK applications apply
 themes on the fly.</source>
-        <translation></translation>
+        <translation>For å prøve å få helhetlige temainnstillinger, enten velg lignende
+stil eller tema (hvis tilgjengelig) på tvers av alle lister eller velg 
+&quot;gtk2&quot; Qt stil (hvis tilgjengelig) for å imitere GTK temaer.
+
+Pass på at &quot;xsettingsd&quot; er installert for å hjelpe GTK-programmer
+til å bruke temainnstillingene der og da.</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="99"/>
         <source>Toolbar button style:</source>
-        <translation></translation>
+        <translation>Knappstil for verktøylinje:</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="107"/>
         <source>Only display the icon</source>
-        <translation></translation>
+        <translation>Bare vis symbolet</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="112"/>
         <source>Only display the text</source>
-        <translation></translation>
+        <translation>Bare vis teksten</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="117"/>
         <source>The text appears beside the icon</source>
-        <translation></translation>
+        <translation>Tekst vises ved siden av symbolet</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="122"/>
         <source>The text appears under the icon</source>
-        <translation></translation>
+        <translation>Tekst vises under symbolet</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="127"/>
         <source>Default</source>
-        <translation></translation>
+        <translation>Forvalgt</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="135"/>
         <source>Activate item on single click</source>
-        <translation></translation>
+        <translation>Aktiver ved enkeltklikk</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="145"/>
         <source>Qt Style</source>
-        <translation></translation>
+        <translation>Qt stil</translation>
     </message>
 </context>
 <context>
@@ -272,12 +277,12 @@ themes on the fly.</source>
     <message>
         <location filename="../configothertoolkits.cpp" line="143"/>
         <source>GTK themes</source>
-        <translation></translation>
+        <translation>GTK temaer</translation>
     </message>
     <message>
         <location filename="../configothertoolkits.cpp" line="144"/>
         <source>&lt;p&gt;&apos;%1&apos; has been overwritten.&lt;/p&gt;&lt;p&gt;You can find a copy of your old settings in &apos;%2&apos;&lt;/p&gt;</source>
-        <translation></translation>
+        <translation>&lt;p&gt;&apos;%1&apos; har blitt overskrevet.&lt;/p&gt;&lt;p&gt;Du kan finne en kopi av dine gamle innstillinger i &apos;%2&apos;&lt;/p&gt;</translation>
     </message>
 </context>
 </TS>
