34d4ef47844e178fb80092c99502c119cb600ac4
Einar Mostad <einar@mostad.eu>
2018-11-05 14:10:57 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/nb_NO/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_nb_NO.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_nb_NO.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../selectwnd.ui" line="14"/>
         <source>LXQt Mouse Theme Configuration</source>
-        <translation></translation>
+        <translation>LXQt Mustemakonfigurasjon</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="20"/>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation></translation>
+        <translation>Velg pekertemaet du ønsker å bruke (hold musa over forhåndsvisning for å teste pekeren). &lt;b&gt;LXQt-sesjonen må omstartes etter denne forandringen&lt;/b&gt;:</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="81"/>
         <source>&amp;Install New Theme...</source>
-        <translation></translation>
+        <translation>&amp;Installer nytt tema...</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="88"/>
         <source>&amp;Remove Theme</source>
-        <translation></translation>
+        <translation>&amp;Fjern tema</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="212"/>
         <source>XCurTheme error</source>
-        <translation></translation>
+        <translation>XCurTheme-feil</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="213"/>
         <source>You can&apos;t remove active theme!</source>
-        <translation></translation>
+        <translation>Du kan ikke fjerne det aktive temaet!</translation>
     </message>
 </context>
 <context>
@@ -44,12 +44,12 @@
     <message>
         <location filename="../warninglabel.ui" line="44"/>
         <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
-        <translation></translation>
+        <translation>LXQt kunne ikke finne noe pekertema. Standard X11 pekertema kommer til å bli brukt i stedet. LXQt så i de følgende mappene:</translation>
     </message>
     <message>
         <location filename="../warninglabel.ui" line="54"/>
         <source>Show...</source>
-        <translation></translation>
+        <translation>Vis...</translation>
     </message>
 </context>
 <context>
@@ -57,12 +57,12 @@
     <message>
         <location filename="../thememodel.cpp" line="61"/>
         <source>Name</source>
-        <translation></translation>
+        <translation>Navn</translation>
     </message>
     <message>
         <location filename="../thememodel.cpp" line="62"/>
         <source>Description</source>
-        <translation></translation>
+        <translation>Beskrivelse</translation>
     </message>
 </context>
 </TS>
