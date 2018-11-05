298a48cc101adabee17d040a2f3121b4a134645c
Einar Mostad <einar@mostad.eu>
2018-11-05 14:03:40 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: LXQt/LXQt Config (Brightness)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-brightness/nb_NO/

--- lxqt-config-brightness/translations/lxqt-config-brightness_nb_NO.ts
+++ lxqt-config-brightness/translations/lxqt-config-brightness_nb_NO.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../brightnesssettings.ui" line="14"/>
         <source>Brightness Settings</source>
-        <translation></translation>
+        <translation>Lysstyrkeinnstillinger</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="24"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Bakgrunnsbelysnings- og lysstyrkeinnstillinger:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Bruk piltastene for å endre innstillinger.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="41"/>
         <source>Require confirmation after settings change</source>
-        <translation></translation>
+        <translation>Krev bekreftelse etter at innstillinger forandres</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="88"/>
         <source>Brightness settings changed</source>
-        <translation></translation>
+        <translation>Lysstyrkeinnstillinger forandret</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="89"/>
         <source>Confirmation required. Are the settings correct?</source>
-        <translation></translation>
+        <translation>Bekreftelse kreves. Er innstillingene riktige?</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../outputwidget.ui" line="38"/>
         <source>&lt;b&gt;Output:&lt;/b&gt;</source>
-        <translation></translation>
+        <translation>&lt;b&gt;Nivå:&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../outputwidget.ui" line="67"/>
         <source>Right click to reset</source>
-        <translation></translation>
+        <translation>Høyreklikk for å stille om</translation>
     </message>
 </context>
 </TS>
