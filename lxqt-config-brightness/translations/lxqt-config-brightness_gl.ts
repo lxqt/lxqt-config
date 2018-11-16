e41e1471cf27d7b2638ab46e8d6990141410f9b0
Miguel A. Bouzada <mbouzada@gmail.com>
2018-11-16 10:59:23 +0000

    Translated using Weblate (Galician)
    Currently translated at 100,0% (7 of 7 strings)
    
    Translation: LXQt/LXQt Config (Brightness)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-brightness/gl/

--- lxqt-config-brightness/translations/lxqt-config-brightness_gl.ts
+++ lxqt-config-brightness/translations/lxqt-config-brightness_gl.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../brightnesssettings.ui" line="14"/>
         <source>Brightness Settings</source>
-        <translation></translation>
+        <translation>Axustes do brillo</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="24"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Axustes da luz de fondo e do brillo:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use as teclas de frechas para modificar os axustes.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="41"/>
         <source>Require confirmation after settings change</source>
-        <translation></translation>
+        <translation>Esixir a confirmación despois da modificación dos axustes</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="88"/>
         <source>Brightness settings changed</source>
-        <translation></translation>
+        <translation>Cambiou a configuración do brillo</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="89"/>
         <source>Confirmation required. Are the settings correct?</source>
-        <translation></translation>
+        <translation>Requírese a confirmación. Son correctos os axustes?</translation>
     </message>
 </context>
 <context>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../outputwidget.ui" line="38"/>
         <source>&lt;b&gt;Output:&lt;/b&gt;</source>
-        <translation></translation>
+        <translation>&lt;b&gt;Saída:&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../outputwidget.ui" line="67"/>
         <source>Right click to reset</source>
-        <translation></translation>
+        <translation>Prema co botón dereito para reiniciar</translation>
     </message>
 </context>
 </TS>
