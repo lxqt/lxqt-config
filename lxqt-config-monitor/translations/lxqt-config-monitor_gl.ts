7b417917bf236d20187c617649f6550f6a76b1d2
Miguel A. Bouzada <mbouzada@gmail.com>
2018-11-20 16:44:58 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (53 of 53 strings)
    
    Translation: LXQt/LXQt Config (Monitor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-monitor/gl/

--- lxqt-config-monitor/translations/lxqt-config-monitor_gl.ts
+++ lxqt-config-monitor/translations/lxqt-config-monitor_gl.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../fastmenu.ui" line="20"/>
         <source>Fast options</source>
-        <translation></translation>
+        <translation>Opcións rápidas</translation>
     </message>
     <message>
         <location filename="../fastmenu.ui" line="40"/>
         <source> ❶ ❷ Extended view</source>
-        <translation></translation>
+        <translation> ❶ ❷ Vista estendida</translation>
     </message>
     <message>
         <location filename="../fastmenu.ui" line="45"/>
         <source> ❶ ❶ Unified view</source>
-        <translation></translation>
+        <translation> ❶ ❶ Vista unificada</translation>
     </message>
     <message>
         <location filename="../fastmenu.ui" line="50"/>
         <source> ❶     Only first</source>
-        <translation></translation>
+        <translation> ❶     Só o primeiro</translation>
     </message>
     <message>
         <location filename="../fastmenu.ui" line="55"/>
         <source>     ❷ Only second</source>
-        <translation></translation>
+        <translation>     ❷ Só o segundo</translation>
     </message>
 </context>
 <context>
@@ -34,69 +34,69 @@
     <message>
         <location filename="../managesavedsettings.ui" line="14"/>
         <source>Monitor Settings</source>
-        <translation></translation>
+        <translation>Axustes do monitor</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.ui" line="27"/>
         <source>Saved settings</source>
-        <translation></translation>
+        <translation>Axustes gardados</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.ui" line="38"/>
         <source>Rename</source>
-        <translation></translation>
+        <translation>Renomear</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.ui" line="45"/>
         <source>Delete</source>
-        <translation></translation>
+        <translation>Eliminar</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.ui" line="52"/>
         <source>Apply</source>
-        <translation></translation>
+        <translation>Aplicar</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="64"/>
         <source>None</source>
-        <translation></translation>
+        <translation>Ningunha</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="67"/>
         <source>Left</source>
-        <translation></translation>
+        <translation>Esquerda</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="70"/>
         <source>Inverted</source>
-        <translation></translation>
+        <translation>Invertida</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="73"/>
         <source>Right</source>
-        <translation></translation>
+        <translation>Dereita</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="77"/>
         <location filename="../managesavedsettings.cpp" line="78"/>
         <source>True</source>
-        <translation></translation>
+        <translation>Verdadeiro</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="77"/>
         <location filename="../managesavedsettings.cpp" line="78"/>
         <source>False</source>
-        <translation></translation>
+        <translation>Falso</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="140"/>
         <source>Name</source>
-        <translation></translation>
+        <translation>Nome</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="140"/>
         <source>Name:</source>
-        <translation></translation>
+        <translation>Nome:</translation>
     </message>
 </context>
 <context>
@@ -104,7 +104,7 @@
     <message>
         <location filename="../monitorpicture.ui" line="27"/>
         <source>Keep monitors attached</source>
-        <translation></translation>
+        <translation>Manter os monitores vencellados</translation>
     </message>
 </context>
 <context>
@@ -112,27 +112,27 @@
     <message>
         <location filename="../monitorsettingsdialog.ui" line="14"/>
         <source>Monitor Settings</source>
-        <translation></translation>
+        <translation>Axustes do monitor</translation>
     </message>
     <message>
         <location filename="../monitorsettingsdialog.ui" line="71"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>Axustes</translation>
     </message>
     <message>
         <location filename="../monitorsettingsdialog.cpp" line="111"/>
         <source>Fast Menu</source>
-        <translation></translation>
+        <translation>Menú rápido</translation>
     </message>
     <message>
         <location filename="../monitorsettingsdialog.cpp" line="115"/>
         <source>Set position</source>
-        <translation></translation>
+        <translation>Estabelecer a posición</translation>
     </message>
     <message>
         <location filename="../monitorsettingsdialog.cpp" line="244"/>
         <source>Advanced settings</source>
-        <translation></translation>
+        <translation>Axustes avanzados</translation>
     </message>
 </context>
 <context>
@@ -140,7 +140,7 @@
     <message>
         <location filename="../monitorwidget.ui" line="21"/>
         <source>Setup</source>
-        <translation></translation>
+        <translation>Axustes</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="29"/>
@@ -155,103 +155,109 @@
     <message>
         <location filename="../monitorwidget.ui" line="78"/>
         <source>Resolution:</source>
-        <translation></translation>
+        <translation>Resolución:</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="90"/>
         <source>Enable this display</source>
-        <translation></translation>
+        <translation>Activar esta pantalla</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="98"/>
         <source>This is my primary display</source>
-        <translation></translation>
+        <translation>Esta é a miña pantalla principal</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="103"/>
         <source>This screen extends another display</source>
-        <translation></translation>
+        <translation>Esta pantalla estende outra pantalla</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="125"/>
         <source>Advanced</source>
-        <translation></translation>
+        <translation>Avanzado</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="146"/>
         <source>Refresh rate:</source>
-        <translation></translation>
+        <translation>Taxa de actualización:</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="153"/>
         <source>Rotation:</source>
-        <translation></translation>
+        <translation>Rotación:</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="176"/>
         <source>Info</source>
-        <translation></translation>
+        <translation>Información</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="182"/>
         <source>Display information</source>
-        <translation></translation>
+        <translation>Información da pantalla</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="135"/>
         <source>Name: %1
 </source>
-        <translation></translation>
+        <translation>Nome: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="136"/>
         <source>Vendor: %1
 </source>
-        <translation></translation>
+        <translation>Fabricante: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="137"/>
         <source>Serial: %1
 </source>
-        <translation></translation>
+        <translation>Serie: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="138"/>
         <source>Display size: %1cm x %2cm
 </source>
-        <translation></translation>
+        <translation>Tamaño da pantalla: %1cm x %2cm
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="139"/>
         <source>Serial number: %1
 </source>
-        <translation></translation>
+        <translation>Número de serie: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="140"/>
         <source>EISA device ID: %1
 </source>
-        <translation></translation>
+        <translation>ID EISA do dispositivo: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="161"/>
         <source>None</source>
-        <translation></translation>
+        <translation>Ningunha</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="162"/>
         <source>Left</source>
-        <translation></translation>
+        <translation>Esquerda</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="163"/>
         <source>Right</source>
-        <translation></translation>
+        <translation>Dereita</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="164"/>
         <source>Inverted</source>
-        <translation></translation>
+        <translation>Invertida</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="255"/>
@@ -264,7 +270,7 @@
     <message>
         <location filename="../settingsdialog.cpp" line="33"/>
         <source>Manage Saved Settings</source>
-        <translation></translation>
+        <translation>Administrar os axustes gardados</translation>
     </message>
 </context>
 <context>
@@ -272,32 +278,31 @@
     <message>
         <location filename="../timeoutdialog.ui" line="14"/>
         <source>Settings are changed</source>
-        <translation></translation>
+        <translation>Cambiaron os axustes</translation>
     </message>
     <message>
         <location filename="../timeoutdialog.ui" line="46"/>
         <source>Are the current settings OK for you?</source>
-        <translation></translation>
+        <translation>Os axustes actuais son correctos para vostede?</translation>
     </message>
     <message numerus="yes">
         <location filename="../timeoutdialog.cpp" line="65"/>
         <source>%n second(s) remaining</source>
-        <translation>
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Resta %n segundo</numerusform>
+        <numerusform>Restan %n segundos</numerusform>
+        </translation></message>
 </context>
 <context>
     <name>SaveSettings</name>
     <message>
         <location filename="../savesettings.cpp" line="82"/>
         <source>Name</source>
-        <translation></translation>
+        <translation>Nome</translation>
     </message>
     <message>
         <location filename="../savesettings.cpp" line="82"/>
         <source>Name:</source>
-        <translation></translation>
+        <translation>Nome:</translation>
     </message>
 </context>
 </TS>
