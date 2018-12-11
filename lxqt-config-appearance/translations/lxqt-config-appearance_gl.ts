f3608a918a0439602a3fb5669aae7643e33a1e66
Miguel A. Bouzada <mbouzada@gmail.com>
2018-12-11 10:24:28 +0000

    Translated using Weblate (Galician)
    Currently translated at 100,0% (51 of 51 strings)
    
    Translation: LXQt/LXQt Config (Appearance)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-appearance/gl/

--- lxqt-config-appearance/translations/lxqt-config-appearance_gl.ts
+++ lxqt-config-appearance/translations/lxqt-config-appearance_gl.ts
@@ -234,7 +234,9 @@
         <source>To attempt uniform theming, either select similar style/theme (if available) across all lists, or select &apos;gtk2&apos; Qt style (if available) to mimic GTK themes.
 
 Make sure &apos;xsettingsd&apos; is installed to help GTK applications apply themes on the fly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Para tentar uniformar os temas, seleccione un estilo/tema similar (se está dispoñíbel) en todas as listas ou seleccione o estilo Qt «gtk2» (se está dispoñíbel) para imitar os temas GTK.
+
+Asegúrese de que «xsettingsd» está instalado para axudar ás aplicacións GTK a aplicar temas sobre a marcha.</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="99"/>
