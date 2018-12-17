9821448d7663e013978f371f7c07570e40d503fe
Ayhan YALÇINSOY PhD. (Dr.) <ayhanyalcinsoy@pisilinux.org>
2018-12-17 15:21:28 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (47 of 47 strings)
    
    Translation: LXQt/LXQt Config (Input)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-input/tr/

--- lxqt-config-input/translations/lxqt-config-input_tr.ts
+++ lxqt-config-input/translations/lxqt-config-input_tr.ts
@@ -191,7 +191,7 @@
     <message>
         <location filename="../mouseconfig.ui" line="140"/>
         <source> ms</source>
-        <translation>ms</translation>
+        <translation> ms</translation>
     </message>
     <message>
         <location filename="../mouseconfig.ui" line="150"/>
