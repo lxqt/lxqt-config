897f717119530140eab7148daf2b44f177c48bd5
Golubev Alexander <fatzer2@gmail.com>
2018-12-26 17:52:33 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (47 of 47 strings)
    
    Translation: LXQt/LXQt Config (Input)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-input/ru/

--- lxqt-config-input/translations/lxqt-config-input_ru.ts
+++ lxqt-config-input/translations/lxqt-config-input_ru.ts
@@ -102,12 +102,12 @@
     <message>
         <location filename="../keyboardlayoutconfig.ui" line="77"/>
         <source>Up</source>
-        <translation type="unfinished">Наверх</translation>
+        <translation>Вверх</translation>
     </message>
     <message>
         <location filename="../keyboardlayoutconfig.ui" line="84"/>
         <source>Down</source>
-        <translation type="unfinished">Вниз</translation>
+        <translation>Вниз</translation>
     </message>
     <message>
         <location filename="../keyboardlayoutconfig.ui" line="108"/>
