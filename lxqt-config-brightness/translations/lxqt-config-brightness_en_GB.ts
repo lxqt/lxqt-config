f611b075d7c108ffcb2ddb83220c5ebf24f5c7ae
Mark Clarkstone <hello@markclarkstone.co.uk>
2019-02-13 01:12:58 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: LXQt/LXQt Config (Brightness)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-brightness/en_GB/

--- lxqt-config-brightness/translations/lxqt-config-brightness_en_GB.ts
+++ lxqt-config-brightness/translations/lxqt-config-brightness_en_GB.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../brightnesssettings.ui" line="24"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Back-light and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="41"/>
