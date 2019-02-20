700ec7606cb070084f409c0289f6d591ceee5de4
Sérgio Marques <smarquespt@gmail.com>
2019-02-20 23:12:03 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: LXQt/LXQt Config
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config/pt/

--- src/translations/lxqt-config_pt.ts
+++ src/translations/lxqt-config_pt.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>Configuration Center</source>
-        <translation>Centro de Configuração</translation>
+        <translation>Centro de configuração</translation>
     </message>
 </context>
 </TS>
