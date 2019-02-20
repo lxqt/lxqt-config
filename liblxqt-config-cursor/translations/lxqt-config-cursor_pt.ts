7ee110f54c48d82c98483b7a4d991ee87e52668f
Sérgio Marques <smarquespt@gmail.com>
2019-02-20 23:21:08 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/pt/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_pt.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_pt.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../selectwnd.ui" line="14"/>
         <source>LXQt Mouse Theme Configuration</source>
-        <translation>Configuração do Tema de Cursor do LXQt</translation>
+        <translation>Configuração do tema de cursor LXQt</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="20"/>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation>Selecionar o tema de cursor que você deseja usar (passar em cima da pré-visualização para testar o cursor). &lt;b&gt;A sessão do LXQt precisa ser reiniciada após essa alteração&lt;/b&gt;:</translation>
+        <translation>Selecione o tema de cursor a utilizar (passe por cima para pré-visualizar). &lt;b&gt;Tem que reiniciar a sessão LXQt para aplicar as alterações&lt;/b&gt;:</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="81"/>
         <source>&amp;Install New Theme...</source>
-        <translation>&amp;Instalar Novo Tema...</translation>
+        <translation>&amp;Instalar novo tema...</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="88"/>
         <source>&amp;Remove Theme</source>
-        <translation>&amp;Remover Tema</translation>
+        <translation>&amp;Remover tema</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="212"/>
@@ -31,7 +31,7 @@
     <message>
         <location filename="../selectwnd.cpp" line="213"/>
         <source>You can&apos;t remove active theme!</source>
-        <translation>Você não pode remover o tema ativo!</translation>
+        <translation>Não pode remover o tema ativo!</translation>
     </message>
 </context>
 <context>
@@ -44,12 +44,12 @@
     <message>
         <location filename="../warninglabel.ui" line="44"/>
         <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
-        <translation>O LXQt não encontrou nenhum tema de cursor. O tema padrão do cursor X11 será usado no lugar. O LXQt procurou nos seguintes diretórios:</translation>
+        <translation>O LXQt não encontrou um tema de cursor. O tema de cursor padrão X11 será utilizado. O LXQt procurou nos seguintes diretórios:</translation>
     </message>
     <message>
         <location filename="../warninglabel.ui" line="54"/>
         <source>Show...</source>
-        <translation>Exibir...</translation>
+        <translation>Mostrar...</translation>
     </message>
 </context>
 <context>
