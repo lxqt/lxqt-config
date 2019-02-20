1f972299e7751fe854edea90177ea634bba7e0b1
Sérgio Marques <smarquespt@gmail.com>
2019-02-20 23:16:39 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: LXQt/LXQt Config (Brightness)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-brightness/pt/

--- lxqt-config-brightness/translations/lxqt-config-brightness_pt.ts
+++ lxqt-config-brightness/translations/lxqt-config-brightness_pt.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../brightnesssettings.ui" line="14"/>
         <source>Brightness Settings</source>
-        <translation>Configurações de Brilho</translation>
+        <translation>Definições de brilho</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="24"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Use arrow keys to change settings.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Configurações de brilho e iluminação:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Usar as teclas de seta para alterar as configurações.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Definições de brilho e iluminação:&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Utilize as teclas de seta para alterar as definições.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="41"/>
         <source>Require confirmation after settings change</source>
-        <translation>Requer confirmação após a alteração das configurações</translation>
+        <translation>Requer confirmação após a alteração das definições</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="88"/>
         <source>Brightness settings changed</source>
-        <translation>Configurações de brilho alteradas</translation>
+        <translation>Definições de brilho alteradas</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="89"/>
         <source>Confirmation required. Are the settings correct?</source>
-        <translation>Requer confirmação. As configurações estão corretas?</translation>
+        <translation>Requer confirmação. As definições estão corretas?</translation>
     </message>
 </context>
 <context>
@@ -39,7 +39,7 @@
     <message>
         <location filename="../outputwidget.ui" line="67"/>
         <source>Right click to reset</source>
-        <translation>Clique com o botão direito para redefinir</translation>
+        <translation>Clique com o botão direito para repor</translation>
     </message>
 </context>
 </TS>
