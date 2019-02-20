c03098aaab38926592fa53b748a43b050b50707f
roxfr <roxfr@outlook.fr>
2019-02-20 20:05:25 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/fr/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_fr.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_fr.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../selectwnd.ui" line="20"/>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation>Sélectionnez le thème de curseur que vous voulez utiliser (survolez pour tester). &lt;b&gt;Relancez LXQt session après ce changement&lt;/b&gt;:</translation>
+        <translation>Sélectionnez le thème de curseur que vous souhaitez utiliser (aperçu au survol pour tester le curseur). &lt;b&gt;La session LXQt doit être redémarrée après cette modification&lt;/b&gt; :</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="81"/>
@@ -44,7 +44,7 @@
     <message>
         <location filename="../warninglabel.ui" line="44"/>
         <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
-        <translation>LXQt ne trouve aucun thème. Le thème de curseur X11 sera utilisé. LXQT cherchait dans les répertoires suivants :</translation>
+        <translation>LXQt n&apos;a trouvé aucun thème de curseur. Le thème de curseur X11 par défaut sera utilisé à la place. LXQt a cherché dans les répertoires suivants :</translation>
     </message>
     <message>
         <location filename="../warninglabel.ui" line="54"/>
