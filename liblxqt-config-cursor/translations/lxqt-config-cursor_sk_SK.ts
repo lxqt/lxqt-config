4bdc1a23e51aba4948a0c3117ffd559c56bc3d05
Paolo Vigoroso <xpaolox@zoznam.sk>
2019-05-27 10:06:05 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/sk_SK/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_sk_SK.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_sk_SK.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../selectwnd.ui" line="14"/>
         <source>LXQt Mouse Theme Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavenie vzhľadu kurzora</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="20"/>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyberte vzhľad kurzora, ktorý chcete používať (vyskúšate prechodom myši na náhľad). &lt;b&gt;Po vykonaní zmeny je potrebné reštartovať sedenie LXQt&lt;/b&gt;:</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="81"/>
         <source>&amp;Install New Theme...</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Nainštalovať novú tému...</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="88"/>
         <source>&amp;Remove Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Odstrániť tému</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="212"/>
         <source>XCurTheme error</source>
-        <translation type="unfinished"></translation>
+        <translation>Chyba témy XCur</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="213"/>
         <source>You can&apos;t remove active theme!</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie je možné odstrániť práve používanú tému!</translation>
     </message>
 </context>
 <context>
@@ -39,17 +39,17 @@
     <message>
         <location filename="../warninglabel.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Formulár</translation>
     </message>
     <message>
         <location filename="../warninglabel.ui" line="44"/>
         <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt sa nepodarilo nájsť žiadnu tému kurzora. Bude použitá preddefinovaná téma grafického serveru X11. LXQt hľadalo v následujúcich priečinkoch:</translation>
     </message>
     <message>
         <location filename="../warninglabel.ui" line="54"/>
         <source>Show...</source>
-        <translation type="unfinished"></translation>
+        <translation>Zobraziť...</translation>
     </message>
 </context>
 <context>
@@ -57,12 +57,12 @@
     <message>
         <location filename="../thememodel.cpp" line="61"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Názov</translation>
     </message>
     <message>
         <location filename="../thememodel.cpp" line="62"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Popis</translation>
     </message>
 </context>
 </TS>
