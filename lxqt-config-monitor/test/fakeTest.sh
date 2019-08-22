5b6051297d994d1e5424aa9c01e1264ab0faee4e
P.L. Lucas <selairi@gmail.com>
2019-08-22 10:00:44 +0200

    lxqt-config-monitor: Better method to drag and drop monitors has been rewritten.

--- lxqt-config-monitor/test/fakeTest.sh
+++ lxqt-config-monitor/test/fakeTest.sh
@@ -1,5 +1,5 @@
 export KSCREEN_BACKEND=Fake
-export KSCREEN_BACKEND_ARGS=TEST_DATA=/home/lucas/prog/lxqt/libkscreen/autotests/configs/multipleoutput.json
+export KSCREEN_BACKEND_ARGS=TEST_DATA=`pwd`/multipleoutput3.json
 killall kscreen_backend_launcher
 #`find /usr -name 'kscreen_backend_launcher'` 
 lxqt-config-monitor
