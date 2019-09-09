a62a687bdd69437746dcaea645c4e067557cf16b
Kim Tinh <onlyesterday.16@gmail.com>
2019-09-09 11:05:08 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: LXQt/LXQt Config
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config/vi/

--- src/translations/lxqt-config_vi.ts
+++ src/translations/lxqt-config_vi.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>Configuration Center</source>
-        <translation type="unfinished"></translation>
+        <translation>Trung tâm cấu hình</translation>
     </message>
 </context>
 </TS>
