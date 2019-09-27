57b525687100a9b3a42180d5c080efc4375373e6
Masamichi Ito <ito32bit+github@gmail.com>
2019-09-27 05:36:29 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/ja/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_ja.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_ja.ts
@@ -21,7 +21,7 @@
     <message>
         <location filename="../selectwnd.ui" line="72"/>
         <source>LXQt session needs restart to view this change.</source>
-        <translation type="unfinished"></translation>
+        <translation>この変更を表示するには LXQt セッションを再起動して下さい。</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="91"/>
@@ -31,7 +31,7 @@
     <message>
         <location filename="../selectwnd.ui" line="111"/>
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation>サイズ</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="233"/>
