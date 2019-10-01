aeeb4f31302b5bd8d3a07f356c43edb565071aa2
Dimitrios Glentadakis <dglent@free.fr>
2019-10-01 04:37:26 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/el/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_el.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_el.ts
@@ -21,7 +21,7 @@
     <message>
         <location filename="../selectwnd.ui" line="72"/>
         <source>LXQt session needs restart to view this change.</source>
-        <translation type="unfinished"></translation>
+        <translation>Απαιτείται επανεκκίνηση της συνεδρίας του LXQt ούτως ώστε να ληφθεί υπόψιν η αλλαγή.</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="91"/>
@@ -31,7 +31,7 @@
     <message>
         <location filename="../selectwnd.ui" line="111"/>
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Μέγεθος</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="233"/>
