970c5ceb86769e911ff515a64739f716e4242e23
Luís Pereira <luis.artur.pereira@gmail.com>
2019-11-22 20:21:35 +0000

    brightness: Refactoring command line code
    Changes include:
        * Better handling of the command line arguments.
        * Thorough error handling.
        * Stricter type (float / double) use.

--- lxqt-config-brightness/main.cpp
+++ lxqt-config-brightness/main.cpp
@@ -22,74 +22,178 @@
 #include <QCommandLineParser>
 #include "brightnesssettings.h"
 
+#include <iostream>
+
+enum CommandLineParseResult {
+    CommandLineOk,
+    CommandLineError,
+    CommandLineVersionRequested,
+    CommandLineHelpRequested
+};
+
+enum UiMode {
+    GUI,
+    TUI
+};
+
+struct BrightnessConfigData {
+    BrightnessConfigData();
+
+    UiMode mode;
+    bool increaseBrightness;
+    bool decreaseBrightness;
+    bool setBrightness;
+    float brightnessValue;
+    bool resetGamma;
+};
+
+BrightnessConfigData::BrightnessConfigData()
+    : mode(UiMode::GUI),
+      increaseBrightness(false),
+      decreaseBrightness(false),
+      setBrightness(false),
+      brightnessValue(0.0f),
+      resetGamma(false)
+{
+}
+
+static CommandLineParseResult parseCommandLine(QCommandLineParser *parser, BrightnessConfigData *config, QString *errorMessage)
+{
+    parser->setApplicationDescription(QStringLiteral("LXQt Config Brightness"));
+    QCommandLineOption increaseOption(QStringList() << QStringLiteral("i") << QStringLiteral("increase"),
+            QObject::tr("Increase brightness."));
+    QCommandLineOption decreaseOption(QStringList() << QStringLiteral("d") << QStringLiteral("decrease"),
+            QObject::tr("Decrease brightness."));
+    QCommandLineOption setOption(QStringList() << QStringLiteral("s") << QStringLiteral("set"),
+            QObject::tr("Set brightness from 1 to 100."), QStringLiteral("brightness"));
+    QCommandLineOption resetGammaOption(QStringList() << QStringLiteral("r") << QStringLiteral("reset"),
+            QObject::tr("Reset gamma to default value."));
+    QCommandLineOption helpOption = parser->addHelpOption();
+    QCommandLineOption versionOption = parser->addVersionOption();
+    parser->addOption(increaseOption);
+    parser->addOption(decreaseOption);
+    parser->addOption(setOption);
+    parser->addOption(resetGammaOption);
+
+    const QStringList args = QCoreApplication::arguments();
+    if (args.size() <= 1) { // no arguments given. GUI mode
+        config->mode = UiMode::GUI;
+        return CommandLineOk;
+    } else {
+        config->mode = UiMode::TUI;
+    }
+
+    if (!parser->parse(QCoreApplication::arguments())) {
+        *errorMessage = parser->errorText();
+        return CommandLineError;
+    }
+
+    if (parser->isSet(versionOption))
+        return CommandLineVersionRequested;
+
+    if (parser->isSet(helpOption))
+        return CommandLineHelpRequested;
+
+    const bool isIncreaseSet = parser->isSet(increaseOption);
+    const bool isDecreaseSet = parser->isSet(decreaseOption);
+    const QString increase_value = parser->value(increaseOption);
+
+    const bool isSetBrightnessSet = parser->isSet(setOption);
+    float brightnessValue = 0.0f;
+    if (isSetBrightnessSet)
+        brightnessValue = parser->value(setOption).toFloat();
+
+    const bool isResetGammaSet = parser->isSet(resetGammaOption);
+
+    if ((isIncreaseSet || isDecreaseSet) && isSetBrightnessSet) {
+        *errorMessage = QObject::tr("%1: Can't use increase/decrease and set in conjunction").arg(QCoreApplication::applicationName());
+        return CommandLineError;
+    }
+
+    if (isIncreaseSet && isDecreaseSet) {
+        *errorMessage = QObject::tr("%1: Can't use increase and decrease options in conjunction").arg(QCoreApplication::applicationName());
+        return CommandLineError;
+    }
+
+    config->increaseBrightness = isIncreaseSet;
+    config->decreaseBrightness = isDecreaseSet;
+    config->setBrightness = isSetBrightnessSet;
+    config->brightnessValue = brightnessValue;
+    config->resetGamma = isResetGammaSet;
+
+    return CommandLineOk;
+}
+
 int main(int argn, char* argv[])
 {
     LXQt::SingleApplication app(argn, argv);
     app.setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 
-    // Command line options
-    QCommandLineParser parser;
-    parser.setApplicationDescription(QStringLiteral("LXQt Config Brightness"));
     const QString VERINFO = QStringLiteral(LXQT_CONFIG_VERSION
                                            "\nliblxqt   " LXQT_VERSION
                                            "\nQt        " QT_VERSION_STR);
     app.setApplicationVersion(VERINFO);
-    QCommandLineOption increaseOption(QStringList() << QStringLiteral("i") << QStringLiteral("increase"),
-            app.tr("Increase brightness."));
-    QCommandLineOption decreaseOption(QStringList() << QStringLiteral("d") << QStringLiteral("decrease"),
-            app.tr("Decrease brightness."));
-    QCommandLineOption setOption(QStringList() << QStringLiteral("s") << QStringLiteral("set"),
-            app.tr("Set brightness from 1 to 100."), QStringLiteral("brightness"));
-    QCommandLineOption resetGammaOption(QStringList() << QStringLiteral("r") << QStringLiteral("reset"),
-            app.tr("Reset gamma to default value."));
-    QCommandLineOption helpOption = parser.addHelpOption();
-    parser.addOption(increaseOption);
-    parser.addOption(decreaseOption);
-    parser.addOption(setOption);
-    parser.addOption(resetGammaOption);
-    parser.addOption(helpOption);
-    parser.addVersionOption();
 
-    parser.process(app);
-    bool runCmdLine = false;
+    // Command line options
+    QCommandLineParser parser;
 
-    runCmdLine = parser.isSet(increaseOption) || parser.isSet(decreaseOption) || parser.isSet(setOption) || parser.isSet(resetGammaOption);
+    BrightnessConfigData config;
+    QString errorMessage;
 
-    if (!runCmdLine) { //  GUI
+    switch(parseCommandLine(&parser, &config, &errorMessage)) {
+    case CommandLineOk:
+        break;
+    case CommandLineError:
+        std::cerr << qPrintable(errorMessage);
+        std::cerr << "\n\n";
+        std::cerr << qPrintable(parser.helpText());
+        return EXIT_FAILURE;
+    case CommandLineVersionRequested:
+        parser.showVersion();
+        Q_UNREACHABLE();
+    case CommandLineHelpRequested:
+        parser.showHelp();
+        Q_UNREACHABLE();
+    }
+
+    if (config.mode == UiMode::GUI) {
         BrightnessSettings *brightnessSettings = new BrightnessSettings();
         brightnessSettings->setWindowIcon(QIcon(QLatin1String(ICON_DIR) + QStringLiteral("/brightnesssettings.svg")));
         brightnessSettings->show();
         return app.exec();
     }
 
-    float sign = parser.isSet(decreaseOption)?-1.0:1.0;
-    double brightness_value = parser.value(setOption).toFloat();
-    brightness_value = qMin( qMax(brightness_value, 0.0), 100.0 ) / 100.0;
-    if(!parser.value(setOption).isEmpty())
-        sign = 0.0;
+    // TUi mode
+    float sign = (config.decreaseBrightness) ? -1.0f : 1.0f;
+    float brightnessValue = qMin( qMax(config.brightnessValue, 0.0f), 100.0f ) / 100.0f;
+
     // Checks if backlight driver is available
-    LXQt::Backlight *mBacklight = new LXQt::Backlight();
-    if(mBacklight->isBacklightAvailable() && (parser.isSet(increaseOption) || parser.isSet(decreaseOption) || parser.isSet(setOption)) ) {
-        // Use backlight driver
-        long backlight = ( mBacklight->getBacklight() + sign*(mBacklight->getMaxBacklight()/50 + 1) )*qAbs(sign) + brightness_value*mBacklight->getMaxBacklight();
+    LXQt::Backlight *mBacklight = new LXQt::Backlight(&app);
+    if (mBacklight->isBacklightAvailable() && !config.resetGamma) { // Use backlight driver
+        if (config.setBrightness)
+            sign = 0.0f;
+
+        const int currentBacklight = mBacklight->getBacklight();
+        const int maxBacklight = mBacklight->getMaxBacklight();
+        int backlight = ( currentBacklight + sign*(maxBacklight/50 + 1) )*qAbs(sign) + brightnessValue*maxBacklight;
+
         mBacklight->setBacklight(backlight);
-    } else {
-        // Use XRandr driver
+    } else { // Use XRandr driver
         XRandrBrightness *brightness = new XRandrBrightness();
         const QList<MonitorInfo> monitors = brightness->getMonitorsInfo();
         QList<MonitorInfo> monitorsChanged;
         for(MonitorInfo monitor : monitors)
         {
-            if(parser.isSet(resetGammaOption))
+            if(config.resetGamma)
             {
-                monitor.setBrightness(1.0);
+                monitor.setBrightness(1.0f);
                 monitorsChanged.append(monitor);
                 continue;
             }
 
             if(monitor.isBacklightSupported() )
             {
-                long backlight = ( monitor.backlight() + sign*(monitor.backlightMax()/50 + 1) )*qAbs(sign) + brightness_value*monitor.backlightMax();
+                long backlight = ( monitor.backlight() + sign*(monitor.backlightMax()/50 + 1) )*qAbs(sign) + brightnessValue*monitor.backlightMax();
                 if(backlight<monitor.backlightMax() && backlight>0)
                 {
                     monitor.setBacklight(backlight);
@@ -98,8 +202,8 @@ int main(int argn, char* argv[])
             }
             else
             {
-                float brightness = (monitor.brightness() + 0.1*sign)*qAbs(sign) + brightness_value*2.0;
-                if(brightness<2.0 && brightness>0.0)
+                float brightness = (monitor.brightness() + 0.1f *sign)*qAbs(sign) + brightnessValue * 2.0f;
+                if(brightness < 2.0f && brightness > 0.0f)
                 {
                     monitor.setBrightness(brightness);
                     monitorsChanged.append(monitor);
@@ -107,7 +211,6 @@ int main(int argn, char* argv[])
             }
         }
         brightness->setMonitorsSettings(monitorsChanged);
+        return 0;
     }
-    delete mBacklight;
-    return 0;
 }
