05270858bc0850b8a955309e2336222b0da97345
Golubev Alexander <fatzer2@gmail.com>
2020-01-24 10:36:25 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/ru/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_ru.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_ru.ts
@@ -21,7 +21,7 @@
     <message>
         <location filename="../selectwnd.ui" line="72"/>
         <source>LXQt session needs restart to view this change.</source>
-        <translation type="unfinished"></translation>
+        <translation>Изменение этого параметра вступит в силу после перезапуска сессии LXQT.</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="91"/>
@@ -31,7 +31,7 @@
     <message>
         <location filename="../selectwnd.ui" line="111"/>
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Размер</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="233"/>
