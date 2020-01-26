d3da01341c2d4f55dcc112d7ba82a374e724eba7
Nejc Bertoncelj <nbert@siol.net>
2020-01-26 16:44:23 +0000

    Translated using Weblate (Slovenian)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/sl/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_sl.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_sl.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../selectwnd.ui" line="14"/>
         <source>LXQt Mouse Theme Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavitev miškine teme</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="46"/>
         <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
-        <translation type="unfinished"></translation>
+        <translation>Izberite miškino temo (premik miške nad ikono prikaže animacijo). &lt;b&gt;Za uveljavitev sprememb je treba sejo LXQt ponovno zagnati.&lt;/b&gt;:</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="23"/>
         <source>&amp;Install New Theme...</source>
-        <translation type="unfinished"></translation>
+        <translation>Namest&amp;i novo temo...</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="72"/>
         <source>LXQt session needs restart to view this change.</source>
-        <translation type="unfinished"></translation>
+        <translation>Za prikaz sprememb je treba sejo LXQt ponovno zagnati.</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="91"/>
         <source>&amp;Remove Theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Odst&amp;rani temo</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="111"/>
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Velikost</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="233"/>
         <source>XCurTheme error</source>
-        <translation type="unfinished"></translation>
+        <translation>Napaka XCurTheme</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="234"/>
         <source>You can&apos;t remove active theme!</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktivne teme se ne da odstraniti!</translation>
     </message>
 </context>
 <context>
@@ -49,17 +49,17 @@
     <message>
         <location filename="../warninglabel.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Obrazec</translation>
     </message>
     <message>
         <location filename="../warninglabel.ui" line="44"/>
         <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt ni uspel najti nobene teme za miško. Uporablja se privzeta tema X11. Pregledane mape so bile:</translation>
     </message>
     <message>
         <location filename="../warninglabel.ui" line="54"/>
         <source>Show...</source>
-        <translation type="unfinished"></translation>
+        <translation>Prikaži...</translation>
     </message>
 </context>
 <context>
@@ -67,12 +67,12 @@
     <message>
         <location filename="../thememodel.cpp" line="61"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Ime</translation>
     </message>
     <message>
         <location filename="../thememodel.cpp" line="62"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Opis</translation>
     </message>
 </context>
 </TS>
