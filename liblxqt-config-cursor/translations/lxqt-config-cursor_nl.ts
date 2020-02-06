9ff4302308265e95a983b29df3f7abe526188a1e
Pander <pander@users.sourceforge.net>
2020-02-06 09:16:12 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: LXQt/LXQt Config (Cursor)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-cursor/nl/

--- liblxqt-config-cursor/translations/lxqt-config-cursor_nl.ts
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_nl.ts
@@ -21,7 +21,7 @@
     <message>
         <location filename="../selectwnd.ui" line="72"/>
         <source>LXQt session needs restart to view this change.</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt-session moet worden herstart om deze wijziging te zien.</translation>
     </message>
     <message>
         <location filename="../selectwnd.ui" line="91"/>
@@ -31,7 +31,7 @@
     <message>
         <location filename="../selectwnd.ui" line="111"/>
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Grootte</translation>
     </message>
     <message>
         <location filename="../selectwnd.cpp" line="233"/>
