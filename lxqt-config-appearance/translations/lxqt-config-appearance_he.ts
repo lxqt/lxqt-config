1d83cd89d83f167e7537298284e3ea7611defa55
Yaron Shahrabani <sh.yaron@gmail.com>
2020-02-24 08:29:21 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (51 of 51 strings)
    
    Translation: LXQt/LXQt Config (Appearance)
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-config-appearance/he/

--- lxqt-config-appearance/translations/lxqt-config-appearance_he.ts
+++ lxqt-config-appearance/translations/lxqt-config-appearance_he.ts
@@ -234,7 +234,9 @@
         <source>To attempt uniform theming, either select similar style/theme (if available) across all lists, or select &apos;gtk2&apos; Qt style (if available) to mimic GTK themes.
 
 Make sure &apos;xsettingsd&apos; is installed to help GTK applications apply themes on the fly.</source>
-        <translation type="unfinished"></translation>
+        <translation>כדי לנסות ערכת עיצוב אחידה, כדאי לבחור או בסגנון/ערכת עיצוב דומה (אם יש) עבור כל הרשימות, או לבחור בסגנון ה־Qt בשם ‚gtk2’ (אם הוא זמין) כדי לחקות ערכות עיצוב של GTK.
+
+נא לוודא כי ‚xsettingsd’ מותקן כדי לסייע ליישומי GTK להחיל ערכות עיצוב באופן חלק.</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="99"/>
