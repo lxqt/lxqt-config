30cf8267ce4af08f9953b169f9d8109fb9437f7d
Tsu Jan <tsujan2000@gmail.com>
2020-06-06 14:15:53 +0430

    A small improvement

--- lxqt-config-appearance/colorLabel.cpp
+++ lxqt-config-appearance/colorLabel.cpp
@@ -46,7 +46,7 @@ void ColorLabel::setColor(const QColor& color)
     QString borderColor = qGray(stylesheetColor_.rgb()) < 255 / 2
                             ? QStringLiteral("white") : QStringLiteral("black");
     setStyleSheet(QStringLiteral("QLabel{background-color: rgb(%1, %2, %3); border: 1px solid %4;}")
-                  .arg(color.red()).arg(color.green()).arg(color.blue()).arg(borderColor));
+                  .arg(QString::number(color.red()), QString::number(color.green()), QString::number(color.blue()), borderColor));
 }
 
 QColor ColorLabel::getColor() const
