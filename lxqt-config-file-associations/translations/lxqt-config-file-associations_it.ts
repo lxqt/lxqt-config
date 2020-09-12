68ce4be74f5201ae341bc41f59c6ceb12017bfa3
Standreas <standreas@riseup.net>
2020-09-12 22:05:51 +0200

    Weblate commits
    Co-authored-by: Standreas <standreas@riseup.net>
    Translate-URL: http://135.181.85.100/projects/lxqt-configuration/lxqt-config-file-associations/it/
    Translation: LXQt Configuration/lxqt-config-file-associations

--- lxqt-config-file-associations/translations/lxqt-config-file-associations_it.ts
+++ lxqt-config-file-associations/translations/lxqt-config-file-associations_it.ts
@@ -102,7 +102,7 @@
     <message>
         <location filename="../mimetypeviewer.ui" line="156"/>
         <source>*.txt *.xml</source>
-        <translation>*.txxt *.xml</translation>
+        <translation>*.txt *.xml</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="173"/>
