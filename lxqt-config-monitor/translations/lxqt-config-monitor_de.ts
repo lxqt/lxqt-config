d66e4d9efcfca16c55f11a577ae416d7fcbd7bc2
Ettore Atalan <atalanttore@googlemail.com>
2020-10-26 19:06:10 +0100

    Weblate commits
    Co-authored-by: Ettore Atalan <atalanttore@googlemail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/de/
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-file-associations
    Translation: LXQt Configuration/lxqt-config-input
    Translation: LXQt Configuration/lxqt-config-monitor

--- lxqt-config-monitor/translations/lxqt-config-monitor_de.ts
+++ lxqt-config-monitor/translations/lxqt-config-monitor_de.ts
@@ -34,7 +34,7 @@
     <message>
         <location filename="../loadsettings.cpp" line="97"/>
         <source>Default monitor settings has been applied. If you want change monitors settings, please, use lxqt-config-monitor.</source>
-        <translation type="unfinished"></translation>
+        <translation>Standardbildschirmeinstellungen wurden angewendet. Wenn Sie die Bildschirmeinstellungen ändern möchten, verwenden Sie bitte lxqt-config-monitor.</translation>
     </message>
 </context>
 <context>
