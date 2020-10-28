71eec1ba70608f78bedc1313eb99b585537210f5
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2020-10-29 01:40:07 +0800

    Weblate commits (#664)
    Co-authored-by: Baptiste Huchon <baptistehuchon17@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/fr/
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-file-associations
    Translation: LXQt Configuration/lxqt-config-input
    Translation: LXQt Configuration/lxqt-config-monitor
    
    Co-authored-by: Baptiste Huchon <baptistehuchon17@gmail.com>

--- lxqt-config-file-associations/translations/lxqt-config-file-associations_fr.ts
+++ lxqt-config-file-associations/translations/lxqt-config-file-associations_fr.ts
@@ -41,23 +41,23 @@
     <message>
         <location filename="../applicationchooser.cpp" line="147"/>
         <source>Web browsers</source>
-        <translation type="unfinished"></translation>
+        <translation>Navigateurs internet</translation>
     </message>
     <message>
         <location filename="../applicationchooser.cpp" line="152"/>
         <source>Email clients</source>
-        <translation type="unfinished"></translation>
+        <translation>Clients de messagerie</translation>
     </message>
     <message>
         <location filename="../applicationchooser.cpp" line="157"/>
         <source>File managers</source>
-        <translation type="unfinished"></translation>
+        <translation>Gestionnaires de fichiers</translation>
     </message>
     <message>
         <location filename="../applicationchooser.cpp" line="193"/>
         <location filename="../applicationchooser.cpp" line="219"/>
         <source>Other applications that handle %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Autres applications de gestion %1</translation>
     </message>
     <message>
         <location filename="../applicationchooser.cpp" line="194"/>
@@ -73,7 +73,7 @@
     <message>
         <location filename="../applicationchooser.cpp" line="310"/>
         <source>No more applications found</source>
-        <translation type="unfinished"></translation>
+        <translation>Aucune autre application trouvée</translation>
     </message>
 </context>
 <context>
@@ -83,7 +83,7 @@
         <location filename="../mimetypeviewer.cpp" line="290"/>
         <location filename="../mimetypeviewer.cpp" line="309"/>
         <source>Change...</source>
-        <translation type="unfinished"></translation>
+        <translation>Changer...</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.cpp" line="275"/>
@@ -99,7 +99,7 @@
         <location filename="../mimetypeviewer.cpp" line="295"/>
         <location filename="../mimetypeviewer.cpp" line="314"/>
         <source>Choose...</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisir...</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.cpp" line="384"/>
@@ -122,7 +122,7 @@
     <message>
         <location filename="../mimetypeviewer.ui" line="30"/>
         <source>Associations</source>
-        <translation type="unfinished"></translation>
+        <translation>Associations</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="206"/>
@@ -137,29 +137,29 @@
     <message>
         <location filename="../mimetypeviewer.ui" line="305"/>
         <source>Default Applications</source>
-        <translation type="unfinished"></translation>
+        <translation>Applications par défaut</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="347"/>
         <source>Web Browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Navigateur Internet</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="412"/>
         <location filename="../mimetypeviewer.ui" line="487"/>
         <location filename="../mimetypeviewer.ui" line="562"/>
         <source>Choose...</source>
-        <translation type="unfinished"></translation>
+        <translation>Choisir...</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="422"/>
         <source>Email Client</source>
-        <translation type="unfinished"></translation>
+        <translation>Client de messagerie</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="497"/>
         <source>File Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Gestionnaire de Fichier</translation>
     </message>
     <message>
         <location filename="../mimetypeviewer.ui" line="255"/>
