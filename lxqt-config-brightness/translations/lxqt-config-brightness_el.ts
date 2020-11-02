467db125032cc6d25a8b20b7779ac86771ab304e
Dimitrios Glentadakis <dglent@free.fr>
2020-11-02 10:06:17 +0100

    Weblate commits
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/el/
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-file-associations
    Translation: LXQt Configuration/lxqt-config-input
    Translation: LXQt Configuration/lxqt-config-monitor

--- lxqt-config-brightness/translations/lxqt-config-brightness_el.ts
+++ lxqt-config-brightness/translations/lxqt-config-brightness_el.ts
@@ -12,27 +12,27 @@
         <location filename="../brightnesssettings.ui" line="35"/>
         <location filename="../brightnesssettings.ui" line="74"/>
         <source>☀</source>
-        <translation type="unfinished"></translation>
+        <translation>☀</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="42"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Ρυθμίσεις οπισθοφωτός και φωτεινότητας:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="51"/>
         <source>Backlight</source>
-        <translation type="unfinished"></translation>
+        <translation>Οπισθόφως</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="57"/>
         <source>☼</source>
-        <translation type="unfinished"></translation>
+        <translation>☼</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="87"/>
         <source>Brightness</source>
-        <translation type="unfinished"></translation>
+        <translation>Φωτεινότητα</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.ui" line="105"/>
@@ -60,12 +60,12 @@
     <message>
         <location filename="../outputwidget.ui" line="48"/>
         <source>☼</source>
-        <translation type="unfinished"></translation>
+        <translation>☼</translation>
     </message>
     <message>
         <location filename="../outputwidget.ui" line="80"/>
         <source>☀</source>
-        <translation type="unfinished"></translation>
+        <translation>☀</translation>
     </message>
 </context>
 <context>
@@ -73,32 +73,32 @@
     <message>
         <location filename="../main.cpp" line="68"/>
         <source>Increase brightness.</source>
-        <translation type="unfinished"></translation>
+        <translation>Αύξηση φωτεινότητας.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="70"/>
         <source>Decrease brightness.</source>
-        <translation type="unfinished"></translation>
+        <translation>Μείωση φωτεινότητας.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="72"/>
         <source>Set brightness from 1 to 100.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ορισμός φωτεινότητας από 1 ως 100.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="74"/>
         <source>Reset gamma to default value.</source>
-        <translation type="unfinished"></translation>
+        <translation>Επαναφορά του γάμμα στην προκαθορισμένη τιμή.</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="112"/>
         <source>%1: Can&apos;t use increase/decrease and set in conjunction</source>
-        <translation type="unfinished"></translation>
+        <translation>%1: Αδύνατη η χρήση αύξησης/μείωσης και ο ορισμός τους ταυτόχρονα</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="117"/>
         <source>%1: Can&apos;t use increase and decrease options in conjunction</source>
-        <translation type="unfinished"></translation>
+        <translation>%1: Αδύνατη η χρήση των επιλογών αύξησης και μείωσης ταυτόχρονα</translation>
     </message>
 </context>
 </TS>
--- lxqt-config-input/translations/lxqt-config-input_el.ts
+++ lxqt-config-input/translations/lxqt-config-input_el.ts
@@ -279,7 +279,7 @@
     <message>
         <location filename="../touchpadconfig.ui" line="145"/>
         <source>DeviceInfoLabel</source>
-        <translation type="unfinished"></translation>
+        <translation>Ετικέτα πληροφοριών της συσκευής</translation>
     </message>
     <message>
         <location filename="../touchpadconfig.cpp" line="85"/>
@@ -289,7 +289,12 @@
 If this is intended, please configure xinput manually.
 Otherwise you can get rid of this message by changing xinput driver to &quot;libinput&quot;.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Το LXQt υποστηρίζει μόνο &quot;libinput&quot; ως οδηγό xinput.
+(τρέχουσα τιμή: %1)
+
+Αν είναι σκόπιμο, παρακαλώ διαμορφώστε το xinput χειροκίνητα.
+Διαφορετικά μπορείτε να απαλλαγείτε από αυτό το μήνυμα αλλάζοντας τον οδηγό xinput σε &quot;libinput&quot;.
+</translation>
     </message>
 </context>
 </TS>
