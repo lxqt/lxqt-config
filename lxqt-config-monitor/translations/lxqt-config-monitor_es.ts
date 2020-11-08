68de17898198beef656b208c82c11d60cf8b48d5
Noumeno <lumintnian@outlook.com>
2020-11-08 09:06:16 +0100

    Weblate commits
    Co-authored-by: Noumeno <lumintnian@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/es/
    Translation: LXQt Configuration/lxqt-config-monitor

--- lxqt-config-monitor/translations/lxqt-config-monitor_es.ts
+++ lxqt-config-monitor/translations/lxqt-config-monitor_es.ts
@@ -34,7 +34,7 @@
     <message>
         <location filename="../loadsettings.cpp" line="97"/>
         <source>Default monitor settings has been applied. If you want change monitors settings, please, use lxqt-config-monitor.</source>
-        <translation type="unfinished"></translation>
+        <translation>Se ha aplicado la configuración predeterminada del monitor. Si desea cambiar la configuración de los monitores, utilice lxqt-config-monitor.</translation>
     </message>
 </context>
 <context>
