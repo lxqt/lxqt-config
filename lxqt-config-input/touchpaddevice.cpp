lxqt/lxqt-config

commit 340ff342c0d6ac684f23afb806f84c2478c85f87

add xinput driver detection for touchpad devices (#645)
* add xinput driver detection to touchpad device

As the driver name cannot be queried directly from xinput api
we use a "known" property instead.

* add TouchpadDevice::xinputDriverSupported

* minor fix for the acceleration float value

QVariant always stores a double.

* add DeviceInfoLabel

* minor ui changes

* adjust info text for xinput/libinput

lxqt-config-input/touchpaddevice.cpp
@@ -133,7 +133,7 @@ static bool xi2_set_device_property(int deviceid, const char* prop, QList<QVaria
             }
         }
         break;
-    case QMetaType::Float:
+    case QVariant::Double:
         Q_ASSERT(act_type == XInternAtom(dpy, "FLOAT", False));
         Q_ASSERT(act_format == 32);
         float_data = new float[values.size()];
@@ -201,6 +201,7 @@ QList<TouchpadDevice> TouchpadDevice::enumerate_from_udev()
             if(dev.find_xi2_device())
             {
                 qDebug() << "Detected" << dev.m_name << "on" << dev.devnode;
+                qDebug() << "xinput driver:" << dev.xinputDriver();
                 dev.m_oldTappingEnabled = dev.tappingEnabled();
                 dev.m_oldNaturalScrollingEnabled = dev.naturalScrollingEnabled();
                 dev.m_oldTapToDragEnabled = dev.tapToDragEnabled();
@@ -234,6 +235,17 @@ bool TouchpadDevice::find_xi2_device()
         {
             m_name = QString::fromUtf8(info[i].name);
             deviceid = info[i].deviceid;
+
+            // init xinput driver name
+            auto xi_driver_prop = xi2_get_device_property(deviceid, "Synaptics Capabilities");
+            if (xi_driver_prop.size()) {
+              // lxqt does not support xf86-input-synaptics properties
+              m_xinputDriver = QStringLiteral("synaptics");
+            } else {
+              // otherweise we assumge libinput and don't test on libinput properties
+              m_xinputDriver = QStringLiteral("libinput");
+            }
+
             found = true;
             break;
         }

lxqt-config-input/touchpadconfig.cpp
@@ -79,6 +79,21 @@ void TouchpadConfig::initControls()
     initFeatureControl(ui.naturalScrollingEnabledCheckBox, device.naturalScrollingEnabled());
     initFeatureControl(ui.tapToDragEnabledCheckBox, device.tapToDragEnabled());
 
+    auto ok = device.xinputDriverSupported();
+    if (!ok) {
+      ui.deviceInfoLabel->setText(
+            tr(
+              "LXQt only supports \"libinput\" as xinput driver.\n"
+              "(current value: %1)\n"
+              "\n"
+              "If this is intended, please configure xinput manually.\n"
+              "Otherwise you can get rid of this message by changing xinput driver to \"libinput\".\n")
+            .arg(device.xinputDriver()));
+      ui.deviceInfoLabel->setStyleSheet(QStringLiteral("background-color: #f88; border-color: #f33; border-size: 1px"));
+    };
+    ui.deviceInfoLabel->setVisible(!ok);
+    ui.deviceInfoLabel->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+
     float accelSpeed = device.accelSpeed();
     if (!std::isnan(accelSpeed)) {
         ui.accelSpeedDoubleSpinBox->setEnabled(true);

lxqt-config-input/touchpadconfig.ui
@@ -139,6 +139,13 @@
      </item>
     </layout>
    </item>
+   <item row="1" column="1">
+    <widget class="QLabel" name="deviceInfoLabel">
+     <property name="text">
+      <string>DeviceInfoLabel</string>
+     </property>
+    </widget>
+   </item>
   </layout>
  </widget>
  <resources/>

lxqt-config-input/touchpaddevice.h
@@ -49,6 +49,8 @@ public:
 
     const QString& name() const { return m_name; }
     QString escapedName() const;
+    auto xinputDriver() const { return m_xinputDriver; }
+    auto xinputDriverSupported() const { return m_xinputDriver == QStringLiteral("libinput"); }
 
     int tappingEnabled() const;
     int naturalScrollingEnabled() const;
@@ -72,6 +74,7 @@ public:
     void saveSettings(LXQt::Settings* settings) const;
 private:
     QString m_name;
+    QString m_xinputDriver;
     QString devnode;
     int deviceid;
 

commit 92d5173057c75d136d10ea61e4b5682ecdd5fcbb

Fixed regression in setting acceleration
The regression was introduced in https://github.com/lxqt/lxqt-config/pull/645, probably because `Float` wasn't a member of `QVariant`. (Dear contributors, please don't include unrelated changes into a single PR!)

The patch fixes the problem by using`QMetaType::Type`. As Qt doc says, the return value of `QVariant::Type QVariant::type()` should be interpreted as `QMetaType::Type` (see https://doc.qt.io/qt-5/qvariant.html#type).

Fixes https://github.com/lxqt/lxqt-config/issues/676

lxqt-config-input/touchpaddevice.cpp
@@ -108,10 +108,10 @@ static bool xi2_set_device_property(int deviceid, const char* prop, QList<QVaria
 
     XFree(data);
 
-    auto dataType = values[0].type();
+    auto dataType = static_cast<QMetaType::Type>(values[0].type());
     switch (dataType)
     {
-    case QVariant::Int:
+    case QMetaType::Int:
         Q_ASSERT(act_type == XA_INTEGER);
 
         data = new unsigned char[values.size() * act_format / 8];
@@ -133,7 +133,7 @@ static bool xi2_set_device_property(int deviceid, const char* prop, QList<QVaria
             }
         }
         break;
-    case QVariant::Double:
+    case QMetaType::Float:
         Q_ASSERT(act_type == XInternAtom(dpy, "FLOAT", False));
         Q_ASSERT(act_format == 32);
         float_data = new float[values.size()];