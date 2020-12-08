6efe54f74d106c3822cebb8b2670dd36ac60a44b
Mincho Kondarev <mkondarev@yahoo.de>
2020-12-08 22:30:55 +0100

    Weblate commits
    Co-authored-by: Mincho Kondarev <mkondarev@yahoo.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/liblxqt-config-cursor/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locate-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-configuration-center-menu-entry/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-settings-menu-category-name/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/system-settings-in-lxqt-configuration-center/bg/
    Translation: LXQt Configuration/LXQt Configuration Center (Menu Entry)
    Translation: LXQt Configuration/LXQt Settings (Menu Category Name)
    Translation: LXQt Configuration/System Settings (in LXQt Configuration Center)
    Translation: LXQt Configuration/liblxqt-config-cursor
    Translation: LXQt Configuration/lxqt-config
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-appearance Menu Entry
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-brightness Menu Entry
    Translation: LXQt Configuration/lxqt-config-file-associations
    Translation: LXQt Configuration/lxqt-config-file-associations Menu Entry
    Translation: LXQt Configuration/lxqt-config-input Menu Entry
    Translation: LXQt Configuration/lxqt-config-locale
    Translation: LXQt Configuration/lxqt-config-locale Menu Entry
    Translation: LXQt Configuration/lxqt-config-monitor
    Translation: LXQt Configuration/lxqt-config-monitor Menu Entry

---
+++ lxqt-config-monitor/translations/lxqt-config-monitor_bg.ts
@@ -0,0 +1,318 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>FastMenu</name>
+    <message>
+        <location filename="../fastmenu.ui" line="20"/>
+        <source>Fast options</source>
+        <translation>Кратки опции</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="40"/>
+        <source> ❶ ❷ Extended view</source>
+        <translation> ❶ ❷ Разширен изглед</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="45"/>
+        <source> ❶ ❶ Unified view</source>
+        <translation> ❶ ❶ Унифициран изглед</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="50"/>
+        <source> ❶     Only first</source>
+        <translation> ❶     Само първия</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="55"/>
+        <source>     ❷ Only second</source>
+        <translation>     ❷ Само втория</translation>
+    </message>
+</context>
+<context>
+    <name>LoadSettings</name>
+    <message>
+        <location filename="../loadsettings.cpp" line="97"/>
+        <source>Default monitor settings has been applied. If you want change monitors settings, please, use lxqt-config-monitor.</source>
+        <translation>Приложени са настройки по подразбиране на монитора. Ако искате да промените настройките на мониторите, използвайте lxqt-config-monitor.</translation>
+    </message>
+</context>
+<context>
+    <name>ManageSavedSettings</name>
+    <message>
+        <location filename="../managesavedsettings.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>Настройки на монитора</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="27"/>
+        <source>Saved settings</source>
+        <translation>Запазени настройки</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="38"/>
+        <source>Rename</source>
+        <translation>Преименуване</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="45"/>
+        <source>Delete</source>
+        <translation>Изтриване</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="52"/>
+        <source>Apply</source>
+        <translation>Прилагане</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="62"/>
+        <source>None</source>
+        <translation>Без</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="65"/>
+        <source>Left</source>
+        <translation>Наляво</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="68"/>
+        <source>Inverted</source>
+        <translation>Наобратно</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="71"/>
+        <source>Right</source>
+        <translation>Надясно</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="75"/>
+        <location filename="../managesavedsettings.cpp" line="76"/>
+        <source>True</source>
+        <translation>Правилно</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="75"/>
+        <location filename="../managesavedsettings.cpp" line="76"/>
+        <source>False</source>
+        <translation>Неправилно</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="136"/>
+        <source>Name</source>
+        <translation>Име</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="136"/>
+        <source>Name:</source>
+        <translation>Име:</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorPictureDialog</name>
+    <message>
+        <location filename="../monitorpicture.ui" line="27"/>
+        <source>Keep monitors attached</source>
+        <translation>Запазване мониторите свързани</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorSettingsDialog</name>
+    <message>
+        <location filename="../monitorsettingsdialog.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>Настройки на монитора</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.ui" line="70"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="105"/>
+        <source>Fast Menu</source>
+        <translation>Кратко меню</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="109"/>
+        <source>Set position</source>
+        <translation>Задаване на позиция</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="228"/>
+        <source>Advanced settings</source>
+        <translation>Разширени настройки</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorWidget</name>
+    <message>
+        <location filename="../monitorwidget.ui" line="21"/>
+        <source>Setup</source>
+        <translation>Настройване</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="29"/>
+        <source> (x)</source>
+        <translation> (x)</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="45"/>
+        <source> (y)</source>
+        <translation> (y)</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="78"/>
+        <source>Resolution:</source>
+        <translation>Резолюция:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="90"/>
+        <source>Enable this display</source>
+        <translation>Активиране на този монитор</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="98"/>
+        <source>This is my primary display</source>
+        <translation>Това е моят главен монитор</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="103"/>
+        <source>This screen extends another display</source>
+        <translation>Екранът се разширява на другия монитор</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="125"/>
+        <source>Advanced</source>
+        <translation>Разширени</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="146"/>
+        <source>Refresh rate:</source>
+        <translation>Честота на опресняване:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="153"/>
+        <source>Rotation:</source>
+        <translation>Завъртване:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="176"/>
+        <source>Info</source>
+        <translation>Инфо</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="182"/>
+        <source>Display information</source>
+        <translation>Информация на дисплея</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="128"/>
+        <source>Name: %1
+</source>
+        <translation>Име: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="129"/>
+        <source>Vendor: %1
+</source>
+        <translation>Vendor: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="130"/>
+        <source>Serial: %1
+</source>
+        <translation>Серия: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="131"/>
+        <source>Display size: %1cm x %2cm
+</source>
+        <translation>Размер на екрана: %1cm x %2cm
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="132"/>
+        <source>Serial number: %1
+</source>
+        <translation>Сериен номер: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="133"/>
+        <source>EISA device ID: %1
+</source>
+        <translation>EISA device ID: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="153"/>
+        <source>None</source>
+        <translation>Без</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="154"/>
+        <source>Left</source>
+        <translation>Наляво</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="155"/>
+        <source>Right</source>
+        <translation>Надясно</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="156"/>
+        <source>Inverted</source>
+        <translation>Наобратно</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="244"/>
+        <source>%1 Hz</source>
+        <translation>%1 Hz</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../settingsdialog.cpp" line="33"/>
+        <source>Manage Saved Settings</source>
+        <translation>Управление на запазените настройки</translation>
+    </message>
+</context>
+<context>
+    <name>SaveSettings</name>
+    <message>
+        <location filename="../savesettings.cpp" line="87"/>
+        <source>Name</source>
+        <translation>Име</translation>
+    </message>
+    <message>
+        <location filename="../savesettings.cpp" line="87"/>
+        <source>Name:</source>
+        <translation>Име:</translation>
+    </message>
+</context>
+<context>
+    <name>TimeoutDialog</name>
+    <message>
+        <location filename="../timeoutdialog.ui" line="14"/>
+        <source>Settings are changed</source>
+        <translation>Настройките са променени</translation>
+    </message>
+    <message>
+        <location filename="../timeoutdialog.ui" line="46"/>
+        <source>Are the current settings OK for you?</source>
+        <translation>Правилни ли са настоящите настройки?</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../timeoutdialog.cpp" line="63"/>
+        <source>%n second(s) remaining</source>
+        <translation>
+            <numerusform>%n оставащи секунди</numerusform>
+            <numerusform>%n оставащи секунди</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
