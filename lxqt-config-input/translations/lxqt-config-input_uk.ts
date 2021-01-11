5f016f21487384cfe9d5d9c4e24649cf5ad0e5a8
Ihor Hordiichuk <igor_ck@outlook.com>
2021-01-11 03:06:36 +0100

    Weblate commits
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/uk/
    Translation: LXQt Configuration/lxqt-config-input

--- lxqt-config-input/translations/lxqt-config-input_uk.ts
+++ lxqt-config-input/translations/lxqt-config-input_uk.ts
@@ -69,7 +69,7 @@
     <message>
         <location filename="../keyboardconfig.ui" line="167"/>
         <source>Turn on NumLock after login</source>
-        <translation>Включити NumLock після входу</translation>
+        <translation>Вмикати NumLock після входу</translation>
     </message>
 </context>
 <context>
