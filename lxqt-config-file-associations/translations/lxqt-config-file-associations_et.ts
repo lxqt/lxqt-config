0909c11b3ec836909e0d8d713ac6bc8d84872d32
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-04-12 12:54:22 +0200

    Translations update from Weblate (#730)
    * Weblate commits
    
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-configuration-center-menu-entry/et/
    Translation: LXQt Configuration/LXQt Configuration Center (Menu Entry)
    Translation: LXQt Configuration/lxqt-config-file-associations
    
    * Weblate commits
    
    Co-authored-by: Heimen Stoffels <vistausss@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/liblxqt-config-cursor/nl/
    Translation: LXQt Configuration/liblxqt-config-cursor
    
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Co-authored-by: Heimen Stoffels <vistausss@outlook.com>

---
+++ lxqt-config-file-associations/translations/lxqt-config-file-associations_et.ts
@@ -0,0 +1,202 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>ApplicationChooser</name>
+    <message>
+        <location filename="../applicationchooser.ui" line="14"/>
+        <source>ApplicationChooser</source>
+        <translation>Rakenduse valija</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="46"/>
+        <source>Pick an application for:</source>
+        <translation>Vali rakendus järgmise failitüübi jaoks:</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="110"/>
+        <source>1</source>
+        <translation>1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="115"/>
+        <source>Applications that handle JPEG</source>
+        <translation>Rakendused, mis oskavad avada JPEG-faile</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="124"/>
+        <location filename="../applicationchooser.ui" line="129"/>
+        <location filename="../applicationchooser.ui" line="139"/>
+        <location filename="../applicationchooser.ui" line="144"/>
+        <source>New Subitem</source>
+        <translation>Uus alamkirje</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="135"/>
+        <location filename="../applicationchooser.cpp" line="191"/>
+        <location filename="../applicationchooser.cpp" line="217"/>
+        <source>Other applications</source>
+        <translation>Muud rakendused</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="147"/>
+        <source>Web browsers</source>
+        <translation>Veebibrauserid</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="152"/>
+        <source>Email clients</source>
+        <translation>E-posti kliendid</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="157"/>
+        <source>File managers</source>
+        <translation>Failihaldurid</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="193"/>
+        <location filename="../applicationchooser.cpp" line="219"/>
+        <source>Other applications that handle %1</source>
+        <translation>Muud rakendused, mis oskavad avada failitüüpi %1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="194"/>
+        <location filename="../applicationchooser.cpp" line="220"/>
+        <source>Applications that handle %1</source>
+        <translation>Rakendused, mis oskavad avada failitüüpi %1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="305"/>
+        <source>No applications found</source>
+        <translation>Rakendusi ei leidunud</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="310"/>
+        <source>No more applications found</source>
+        <translation>Rohkem rakendusi ei leidu</translation>
+    </message>
+</context>
+<context>
+    <name>MimetypeViewer</name>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="271"/>
+        <location filename="../mimetypeviewer.cpp" line="290"/>
+        <location filename="../mimetypeviewer.cpp" line="309"/>
+        <source>Change...</source>
+        <translation>Muuda...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="275"/>
+        <location filename="../mimetypeviewer.cpp" line="294"/>
+        <location filename="../mimetypeviewer.cpp" line="313"/>
+        <location filename="../mimetypeviewer.cpp" line="327"/>
+        <location filename="../mimetypeviewer.cpp" line="388"/>
+        <source>None</source>
+        <translation>Puudub</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="276"/>
+        <location filename="../mimetypeviewer.cpp" line="295"/>
+        <location filename="../mimetypeviewer.cpp" line="314"/>
+        <source>Choose...</source>
+        <translation>Vali...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="384"/>
+        <source>&amp;Change...</source>
+        <translation>&amp;Muuda...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="389"/>
+        <source>&amp;Choose...</source>
+        <translation>&amp;Vali...</translation>
+    </message>
+</context>
+<context>
+    <name>mimetypeviewer</name>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="20"/>
+        <source>File Associations</source>
+        <translation>Failide seosed</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="30"/>
+        <source>Associations</source>
+        <translation>Failitüüpide ja rakenduste seosed</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="206"/>
+        <source>Default application</source>
+        <translation>Vaikimisi rakendus</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="271"/>
+        <source>&amp;Choose...</source>
+        <translation>&amp;Vali...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="305"/>
+        <source>Default Applications</source>
+        <translation>Vaikimisi kasutatavad rakendused</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="347"/>
+        <source>Web Browser</source>
+        <translation>Veebibrauser</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="412"/>
+        <location filename="../mimetypeviewer.ui" line="487"/>
+        <location filename="../mimetypeviewer.ui" line="562"/>
+        <source>Choose...</source>
+        <translation>Vali...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="422"/>
+        <source>Email Client</source>
+        <translation>E-posti klient</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="497"/>
+        <source>File Manager</source>
+        <translation>Failihaldur</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="255"/>
+        <location filename="../mimetypeviewer.ui" line="396"/>
+        <location filename="../mimetypeviewer.ui" line="471"/>
+        <location filename="../mimetypeviewer.ui" line="546"/>
+        <source>None</source>
+        <translation>Puudub</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="187"/>
+        <source>Patterns</source>
+        <translation>Failimuster</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="193"/>
+        <source>*.txt *.xml</source>
+        <translation>*.txt *.xml</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="146"/>
+        <source>MIME type</source>
+        <translation>MIME-tüüp</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="164"/>
+        <location filename="../mimetypeviewer.ui" line="248"/>
+        <location filename="../mimetypeviewer.ui" line="389"/>
+        <location filename="../mimetypeviewer.ui" line="464"/>
+        <location filename="../mimetypeviewer.ui" line="539"/>
+        <source>Icon</source>
+        <translation>Ikoon</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="171"/>
+        <source>Description</source>
+        <translation>Kirjeldus</translation>
+    </message>
+</context>
+</TS>
