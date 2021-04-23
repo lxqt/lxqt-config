8bb1bfca5a4a9f3de9b62300415e324e4df1c3d0
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-23 10:07:16 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/et/
    Translation: LXQt Configuration/lxqt-config-monitor
    Translation: LXQt Configuration/lxqt-config-monitor Menu Entry

---
+++ lxqt-config-monitor/translations/lxqt-config-monitor_et.ts
@@ -0,0 +1,318 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>FastMenu</name>
+    <message>
+        <location filename="../fastmenu.ui" line="20"/>
+        <source>Fast options</source>
+        <translation>Kiirvalikud</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="40"/>
+        <source> ❶ ❷ Extended view</source>
+        <translation> ❶ ❷ Laiendatud vaade</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="45"/>
+        <source> ❶ ❶ Unified view</source>
+        <translation> ❶ ❶ Ühendatud vaade</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="50"/>
+        <source> ❶     Only first</source>
+        <translation> ❶     Ainult esimene</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="55"/>
+        <source>     ❷ Only second</source>
+        <translation>     ❷ Ainult teine</translation>
+    </message>
+</context>
+<context>
+    <name>LoadSettings</name>
+    <message>
+        <location filename="../loadsettings.cpp" line="97"/>
+        <source>Default monitor settings has been applied. If you want change monitors settings, please, use lxqt-config-monitor.</source>
+        <translation>Ekraanide vaikimisi seadistused on kasutusel. Kui sa soovid neid muuta, siis palun kasuta seadistajat lxqt-config-monitor.</translation>
+    </message>
+</context>
+<context>
+    <name>ManageSavedSettings</name>
+    <message>
+        <location filename="../managesavedsettings.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>Ekraani seadistused</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="27"/>
+        <source>Saved settings</source>
+        <translation>Salvestatud seadistused</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="38"/>
+        <source>Rename</source>
+        <translation>Anna uus nimi</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="45"/>
+        <source>Delete</source>
+        <translation>Kustuta</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="52"/>
+        <source>Apply</source>
+        <translation>Rakenda</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="62"/>
+        <source>None</source>
+        <translation>Puudub</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="65"/>
+        <source>Left</source>
+        <translation>Vasakul</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="68"/>
+        <source>Inverted</source>
+        <translation>Pööratud</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="71"/>
+        <source>Right</source>
+        <translation>Paremal</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="75"/>
+        <location filename="../managesavedsettings.cpp" line="76"/>
+        <source>True</source>
+        <translation>Jah</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="75"/>
+        <location filename="../managesavedsettings.cpp" line="76"/>
+        <source>False</source>
+        <translation>Ei</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="136"/>
+        <source>Name</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="136"/>
+        <source>Name:</source>
+        <translation>Nimi:</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorPictureDialog</name>
+    <message>
+        <location filename="../monitorpicture.ui" line="27"/>
+        <source>Keep monitors attached</source>
+        <translation>Jäta ekraanid ühendatuks</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorSettingsDialog</name>
+    <message>
+        <location filename="../monitorsettingsdialog.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>Ekraanide seadistused</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.ui" line="70"/>
+        <source>Settings</source>
+        <translation>Seadistused</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="105"/>
+        <source>Fast Menu</source>
+        <translation>Kiirmenüü</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="109"/>
+        <source>Set position</source>
+        <translation>Seadista asukohta</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="228"/>
+        <source>Advanced settings</source>
+        <translation>Lisaseadistused</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorWidget</name>
+    <message>
+        <location filename="../monitorwidget.ui" line="21"/>
+        <source>Setup</source>
+        <translation>Seadista</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="29"/>
+        <source> (x)</source>
+        <translation> (x)</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="45"/>
+        <source> (y)</source>
+        <translation> (y)</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="78"/>
+        <source>Resolution:</source>
+        <translation>Lahutusvõime:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="90"/>
+        <source>Enable this display</source>
+        <translation>Kasuta seda ekraani</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="98"/>
+        <source>This is my primary display</source>
+        <translation>See on minu esmane ekraan</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="103"/>
+        <source>This screen extends another display</source>
+        <translation>See ekraan laieneb teisele monitorile</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="125"/>
+        <source>Advanced</source>
+        <translation>Lisaseadistused</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="146"/>
+        <source>Refresh rate:</source>
+        <translation>Värskendamissagedus:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="153"/>
+        <source>Rotation:</source>
+        <translation>Suund:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="176"/>
+        <source>Info</source>
+        <translation>Seadme teave</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="182"/>
+        <source>Display information</source>
+        <translation>Ekraani teave</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="128"/>
+        <source>Name: %1
+</source>
+        <translation>Nimi: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="129"/>
+        <source>Vendor: %1
+</source>
+        <translation>Tootja: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="130"/>
+        <source>Serial: %1
+</source>
+        <translation>Seerianumber: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="131"/>
+        <source>Display size: %1cm x %2cm
+</source>
+        <translation>Ekraani mõõdud: %1cm x %2cm
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="132"/>
+        <source>Serial number: %1
+</source>
+        <translation>Seerianumber: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="133"/>
+        <source>EISA device ID: %1
+</source>
+        <translation>Seadme EISA ID: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="153"/>
+        <source>None</source>
+        <translation>Puudub</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="154"/>
+        <source>Left</source>
+        <translation>Vasakul</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="155"/>
+        <source>Right</source>
+        <translation>Paremal</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="156"/>
+        <source>Inverted</source>
+        <translation>Pööratud</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="244"/>
+        <source>%1 Hz</source>
+        <translation>%1 Hz</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../settingsdialog.cpp" line="33"/>
+        <source>Manage Saved Settings</source>
+        <translation>Halda salvestatud seadistusi</translation>
+    </message>
+</context>
+<context>
+    <name>SaveSettings</name>
+    <message>
+        <location filename="../savesettings.cpp" line="87"/>
+        <source>Name</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <location filename="../savesettings.cpp" line="87"/>
+        <source>Name:</source>
+        <translation>Nimi:</translation>
+    </message>
+</context>
+<context>
+    <name>TimeoutDialog</name>
+    <message>
+        <location filename="../timeoutdialog.ui" line="14"/>
+        <source>Settings are changed</source>
+        <translation>Seadistused on nüüd muudetud</translation>
+    </message>
+    <message>
+        <location filename="../timeoutdialog.ui" line="46"/>
+        <source>Are the current settings OK for you?</source>
+        <translation>Kas praegused seadistused sobivad Sulle?</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../timeoutdialog.cpp" line="63"/>
+        <source>%n second(s) remaining</source>
+        <translation>
+            <numerusform>jäänud on veel %n sekund</numerusform>
+            <numerusform>jäänud on veel %n sekundit</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
