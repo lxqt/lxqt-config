b54233866534d1c09b39982659488ee41f09c5ad
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-27 23:07:19 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locate-menu-entry/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-settings-menu-category-name/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/system-settings-in-lxqt-configuration-center/et/
    Translation: LXQt Configuration/LXQt Settings (Menu Category Name)
    Translation: LXQt Configuration/System Settings (in LXQt Configuration Center)
    Translation: LXQt Configuration/lxqt-config
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-appearance Menu Entry
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-brightness Menu Entry
    Translation: LXQt Configuration/lxqt-config-file-associations Menu Entry
    Translation: LXQt Configuration/lxqt-config-input
    Translation: LXQt Configuration/lxqt-config-input Menu Entry
    Translation: LXQt Configuration/lxqt-config-locale
    Translation: LXQt Configuration/lxqt-config-locale Menu Entry

---
+++ lxqt-config-brightness/translations/lxqt-config-brightness_et.ts
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>BrightnessSettings</name>
+    <message>
+        <location filename="../brightnesssettings.ui" line="14"/>
+        <source>Brightness Settings</source>
+        <translation>Heleduse seadistused</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="35"/>
+        <location filename="../brightnesssettings.ui" line="74"/>
+        <source>☀</source>
+        <translation>☀</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="42"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Taustavalguse ja ekraani heleduse seadistused:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="51"/>
+        <source>Backlight</source>
+        <translation>Taustavalgus</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="57"/>
+        <source>☼</source>
+        <translation>☼</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="87"/>
+        <source>Brightness</source>
+        <translation>Heledus (gamma)</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="105"/>
+        <source>Require confirmation after settings change</source>
+        <translation>Peale muutmist küsi kinnitust</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.cpp" line="112"/>
+        <source>Brightness settings changed</source>
+        <translation>Gamma-heleduse seadistused on muutunud</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.cpp" line="113"/>
+        <source>Confirmation required. Are the settings correct?</source>
+        <translation>Palun kinnita uuesti üle. Kas seadistused on õiged?</translation>
+    </message>
+</context>
+<context>
+    <name>OutputWidget</name>
+    <message>
+        <location filename="../outputwidget.ui" line="38"/>
+        <source>&lt;b&gt;Output:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Tulemus:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../outputwidget.ui" line="48"/>
+        <source>☼</source>
+        <translation>☼</translation>
+    </message>
+    <message>
+        <location filename="../outputwidget.ui" line="80"/>
+        <source>☀</source>
+        <translation>☀</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="68"/>
+        <source>Increase brightness.</source>
+        <translation>Lisa heledust.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="70"/>
+        <source>Decrease brightness.</source>
+        <translation>Vähenda heledust.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="72"/>
+        <source>Set brightness from 1 to 100.</source>
+        <translation>Seadista heledust skaalal 1 kuni 100.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="74"/>
+        <source>Reset gamma to default value.</source>
+        <translation>Lähtesta gamma-heleduse väärtused.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="112"/>
+        <source>%1: Can&apos;t use increase/decrease and set in conjunction</source>
+        <translation>%1: Suurendamise, vähendamise ja seadistamise käske ei saa samaaegselt kasutada</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="117"/>
+        <source>%1: Can&apos;t use increase and decrease options in conjunction</source>
+        <translation>%1: Suurendamise ja vähendamise käske ei saa samaaegselt kasutada</translation>
+    </message>
+</context>
+</TS>
---
+++ lxqt-config-locale/translations/lxqt-config-locale_et.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>LocaleConfig</name>
+    <message>
+        <location filename="../localeconfig.ui" line="47"/>
+        <source>Re&amp;gion:</source>
+        <translation>&amp;Piirkond:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="76"/>
+        <source>De&amp;tailed Settings</source>
+        <translation>Ü&amp;ksikasjalikud seadistused</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="83"/>
+        <source>&amp;Numbers:</source>
+        <translation>A&amp;rvud:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="112"/>
+        <source>&amp;Time:</source>
+        <translation>&amp;Aeg:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="141"/>
+        <source>Currenc&amp;y:</source>
+        <translation>V&amp;ääring:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="170"/>
+        <source>Measurement &amp;Units:</source>
+        <translation>M&amp;õõtühikud:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="199"/>
+        <source>Co&amp;llation and Sorting:</source>
+        <translation>Järjestamine ja &amp;sortimine:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="238"/>
+        <source>&lt;b&gt;Examples&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Näited&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="248"/>
+        <source>Numbers:</source>
+        <translation>Arvud:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="268"/>
+        <source>Time:</source>
+        <translation>Aeg:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="288"/>
+        <source>Currency:</source>
+        <translation>Vääring:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="308"/>
+        <source>Measurement Units:</source>
+        <translation>Mõõtühikud:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="127"/>
+        <source>No change</source>
+        <translation>Muutmata</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="316"/>
+        <source>Format Settings Changed</source>
+        <translation>Salvestasin vormingu muudatused</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="317"/>
+        <source>Do you want to save your changes? They will take effect the next time you log in.</source>
+        <translation>Kas sa soovid muudatused salvestada? Nad jõustuvad järgmisel sisselogimisel.</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="438"/>
+        <source>Imperial UK</source>
+        <translation>Briti mõõdustik</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="442"/>
+        <source>Imperial US</source>
+        <translation>USA mõõdustik</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="446"/>
+        <source>Metric</source>
+        <translation>Meetermõõdustik</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="50"/>
+        <source>LXQt Locale Configuration</source>
+        <translation>LXQt lokaadi seadistused</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="55"/>
+        <source>Locale Settings</source>
+        <translation>Lokaadi seadistused</translation>
+    </message>
+</context>
+</TS>
