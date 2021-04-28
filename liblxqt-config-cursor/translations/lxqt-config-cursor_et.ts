264ec45797d8a5c3a599a625ba8f1a9e09aa580f
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-29 00:07:18 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/liblxqt-config-cursor/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/et/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/et/
    Translation: LXQt Configuration/liblxqt-config-cursor
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-input

---
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_et.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>SelectWnd</name>
+    <message>
+        <location filename="../selectwnd.ui" line="14"/>
+        <source>LXQt Mouse Theme Configuration</source>
+        <translation>LXQt hiireteema seadistused</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="46"/>
+        <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
+        <translation>Vali kursoriteema, mida soovid kasutada (kursori testimiseks vii ta eelvaate kohale). &lt;b&gt;Peale seda muudatust käivita LXQt sessioon uuesti&lt;/b&gt;:</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="23"/>
+        <source>&amp;Install New Theme...</source>
+        <translation>&amp;Paigalda uus teema...</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="72"/>
+        <source>LXQt session needs restart to view this change.</source>
+        <translation>Selle muudatuse nägemiseks käivita LXQt sessioon uuesti.</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="91"/>
+        <source>&amp;Remove Theme</source>
+        <translation>&amp;Eemalda teema</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="111"/>
+        <source>Size</source>
+        <translation>Suurus</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.cpp" line="233"/>
+        <source>XCurTheme error</source>
+        <translation>XCurTheme viga</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.cpp" line="234"/>
+        <source>You can&apos;t remove active theme!</source>
+        <translation>Kasutusel olevat teemat ei saa kustutada!</translation>
+    </message>
+</context>
+<context>
+    <name>WarningLabel</name>
+    <message>
+        <location filename="../warninglabel.ui" line="14"/>
+        <source>Form</source>
+        <translation>Vorm</translation>
+    </message>
+    <message>
+        <location filename="../warninglabel.ui" line="44"/>
+        <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
+        <translation>LXQt ei leidnud ühtegi kursoriteemat. Selle asemel kasutame X11 vaikimisi kursoriteemat. LXQt üritab neid leida järgmistest kasutadest:</translation>
+    </message>
+    <message>
+        <location filename="../warninglabel.ui" line="54"/>
+        <source>Show...</source>
+        <translation>Näita...</translation>
+    </message>
+</context>
+<context>
+    <name>XCursorThemeModel</name>
+    <message>
+        <location filename="../thememodel.cpp" line="61"/>
+        <source>Name</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <location filename="../thememodel.cpp" line="62"/>
+        <source>Description</source>
+        <translation>Kirjeldus</translation>
+    </message>
+</context>
+</TS>
