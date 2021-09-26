9aa6e063c121dab06cc6486bf090e4d757fcb41e
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-09-26 21:18:34 +0200

    Translations update from Weblate (#768)
    * Weblate commits
    
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor-menu-entry/uk/
    Translation: LXQt Configuration/lxqt-config-monitor Menu Entry
    
    * Weblate commits
    
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/liblxqt-config-cursor/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance-menu-entry/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness-menu-entry/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations-menu-entry/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input-menu-entry/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locate-menu-entry/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor-menu-entry/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-configuration-center-menu-entry/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-settings-menu-category-name/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/system-settings-in-lxqt-configuration-center/ko/
    Translation: LXQt Configuration/LXQt Configuration Center (Menu Entry)
    Translation: LXQt Configuration/LXQt Settings (Menu Category Name)
    Translation: LXQt Configuration/System Settings (in LXQt Configuration Center)
    Translation: LXQt Configuration/liblxqt-config-cursor
    Translation: LXQt Configuration/lxqt-config
    Translation: LXQt Configuration/lxqt-config-appearance Menu Entry
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-brightness Menu Entry
    Translation: LXQt Configuration/lxqt-config-file-associations
    Translation: LXQt Configuration/lxqt-config-file-associations Menu Entry
    Translation: LXQt Configuration/lxqt-config-input
    Translation: LXQt Configuration/lxqt-config-input Menu Entry
    Translation: LXQt Configuration/lxqt-config-locale
    Translation: LXQt Configuration/lxqt-config-locale Menu Entry
    Translation: LXQt Configuration/lxqt-config-monitor
    Translation: LXQt Configuration/lxqt-config-monitor Menu Entry
    
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: 이정희 <daemul72@gmail.com>

---
+++ lxqt-config-brightness/translations/lxqt-config-brightness_ko.ts
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>BrightnessSettings</name>
+    <message>
+        <location filename="../brightnesssettings.ui" line="14"/>
+        <source>Brightness Settings</source>
+        <translation>밝기 설정</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="35"/>
+        <location filename="../brightnesssettings.ui" line="74"/>
+        <source>☀</source>
+        <translation>☀</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="42"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Backlight and brightness settings:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;백라이트 및 밝기 설정:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="51"/>
+        <source>Backlight</source>
+        <translation>백라이트</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="57"/>
+        <source>☼</source>
+        <translation>☼</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="87"/>
+        <source>Brightness</source>
+        <translation>밝기</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.ui" line="105"/>
+        <source>Require confirmation after settings change</source>
+        <translation>설정 변경 후 확인 필요</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.cpp" line="112"/>
+        <source>Brightness settings changed</source>
+        <translation>밝기 설정이 변경됨</translation>
+    </message>
+    <message>
+        <location filename="../brightnesssettings.cpp" line="113"/>
+        <source>Confirmation required. Are the settings correct?</source>
+        <translation>확인이 필요합니다. 설정이 올바릅니까?</translation>
+    </message>
+</context>
+<context>
+    <name>OutputWidget</name>
+    <message>
+        <location filename="../outputwidget.ui" line="38"/>
+        <source>&lt;b&gt;Output:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;출력:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../outputwidget.ui" line="48"/>
+        <source>☼</source>
+        <translation>☼</translation>
+    </message>
+    <message>
+        <location filename="../outputwidget.ui" line="80"/>
+        <source>☀</source>
+        <translation>☀</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="68"/>
+        <source>Increase brightness.</source>
+        <translation>밝기를 높입니다.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="70"/>
+        <source>Decrease brightness.</source>
+        <translation>밝기를 줄입니다.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="72"/>
+        <source>Set brightness from 1 to 100.</source>
+        <translation>밝기를 1에서 100까지 설정합니다.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="74"/>
+        <source>Reset gamma to default value.</source>
+        <translation>감마를 기본값으로 재설정합니다.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="112"/>
+        <source>%1: Can&apos;t use increase/decrease and set in conjunction</source>
+        <translation>%1: 증가/감소 및 설정을 함께 사용할 수 없습니다</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="117"/>
+        <source>%1: Can&apos;t use increase and decrease options in conjunction</source>
+        <translation>%1: 증가 및 감소 옵션을 함께 사용할 수 없습니다</translation>
+    </message>
+</context>
+</TS>
---
+++ lxqt-config-monitor/translations/lxqt-config-monitor_ko.ts
@@ -0,0 +1,317 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>FastMenu</name>
+    <message>
+        <location filename="../fastmenu.ui" line="20"/>
+        <source>Fast options</source>
+        <translation>빠른 옵션</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="40"/>
+        <source> ❶ ❷ Extended view</source>
+        <translation> ❶ ❷ 확장 보기</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="45"/>
+        <source> ❶ ❶ Unified view</source>
+        <translation> ❶ ❶ 통합 보기</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="50"/>
+        <source> ❶     Only first</source>
+        <translation> ❶     첫 번째만</translation>
+    </message>
+    <message>
+        <location filename="../fastmenu.ui" line="55"/>
+        <source>     ❷ Only second</source>
+        <translation>     ❷ 두 번째만</translation>
+    </message>
+</context>
+<context>
+    <name>LoadSettings</name>
+    <message>
+        <location filename="../loadsettings.cpp" line="97"/>
+        <source>Default monitor settings has been applied. If you want change monitors settings, please, use lxqt-config-monitor.</source>
+        <translation>기본 모니터 설정이 적용되었습니다. 모니터 설정을 변경하려면 lxqt-config-monitor를 사용하십시오.</translation>
+    </message>
+</context>
+<context>
+    <name>ManageSavedSettings</name>
+    <message>
+        <location filename="../managesavedsettings.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>모니터 설정</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="27"/>
+        <source>Saved settings</source>
+        <translation>설정 저장됨</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="38"/>
+        <source>Rename</source>
+        <translation>이름변경</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="45"/>
+        <source>Delete</source>
+        <translation>삭제</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.ui" line="52"/>
+        <source>Apply</source>
+        <translation>적용</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="62"/>
+        <source>None</source>
+        <translation>없음</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="65"/>
+        <source>Left</source>
+        <translation>왼쪽</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="68"/>
+        <source>Inverted</source>
+        <translation>반전됨</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="71"/>
+        <source>Right</source>
+        <translation>오른쪽</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="75"/>
+        <location filename="../managesavedsettings.cpp" line="76"/>
+        <source>True</source>
+        <translation>참</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="75"/>
+        <location filename="../managesavedsettings.cpp" line="76"/>
+        <source>False</source>
+        <translation>거짓</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="136"/>
+        <source>Name</source>
+        <translation>이름</translation>
+    </message>
+    <message>
+        <location filename="../managesavedsettings.cpp" line="136"/>
+        <source>Name:</source>
+        <translation>이름:</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorPictureDialog</name>
+    <message>
+        <location filename="../monitorpicture.ui" line="27"/>
+        <source>Keep monitors attached</source>
+        <translation>모니터를 연결된 상태로 유지</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorSettingsDialog</name>
+    <message>
+        <location filename="../monitorsettingsdialog.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>모니터 설정</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.ui" line="70"/>
+        <source>Settings</source>
+        <translation>설정</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="105"/>
+        <source>Fast Menu</source>
+        <translation>빠른 메뉴</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="109"/>
+        <source>Set position</source>
+        <translation>위치 지정</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="228"/>
+        <source>Advanced settings</source>
+        <translation>고급 설정</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorWidget</name>
+    <message>
+        <location filename="../monitorwidget.ui" line="21"/>
+        <source>Setup</source>
+        <translation>설정</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="29"/>
+        <source> (x)</source>
+        <translation> (x)</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="45"/>
+        <source> (y)</source>
+        <translation> (y)</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="78"/>
+        <source>Resolution:</source>
+        <translation>해상도:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="90"/>
+        <source>Enable this display</source>
+        <translation>이 디스플레이 활성화</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="98"/>
+        <source>This is my primary display</source>
+        <translation>이것이 내 기본 디스플레이입니다</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="103"/>
+        <source>This screen extends another display</source>
+        <translation>이 화면은 다른 디스플레이를 확장합니다</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="125"/>
+        <source>Advanced</source>
+        <translation>고급</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="146"/>
+        <source>Refresh rate:</source>
+        <translation>재생 빈도:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="153"/>
+        <source>Rotation:</source>
+        <translation>회전:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="176"/>
+        <source>Info</source>
+        <translation>정보</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="182"/>
+        <source>Display information</source>
+        <translation>디스플레이 정보</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="128"/>
+        <source>Name: %1
+</source>
+        <translation>이름: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="129"/>
+        <source>Vendor: %1
+</source>
+        <translation>제조사: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="130"/>
+        <source>Serial: %1
+</source>
+        <translation>시리얼: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="131"/>
+        <source>Display size: %1cm x %2cm
+</source>
+        <translation>디스플레이 크기: %1cm x %2cm
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="132"/>
+        <source>Serial number: %1
+</source>
+        <translation>일련 번호: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="133"/>
+        <source>EISA device ID: %1
+</source>
+        <translation>EISA 장치 ID: %1
+</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="153"/>
+        <source>None</source>
+        <translation>없음</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="154"/>
+        <source>Left</source>
+        <translation>왼쪽</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="155"/>
+        <source>Right</source>
+        <translation>오른쪽</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="156"/>
+        <source>Inverted</source>
+        <translation>반전됨</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="244"/>
+        <source>%1 Hz</source>
+        <translation>%1 Hz</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../settingsdialog.cpp" line="33"/>
+        <source>Manage Saved Settings</source>
+        <translation>저장된 설정 관리</translation>
+    </message>
+</context>
+<context>
+    <name>SaveSettings</name>
+    <message>
+        <location filename="../savesettings.cpp" line="87"/>
+        <source>Name</source>
+        <translation>이름</translation>
+    </message>
+    <message>
+        <location filename="../savesettings.cpp" line="87"/>
+        <source>Name:</source>
+        <translation>이름:</translation>
+    </message>
+</context>
+<context>
+    <name>TimeoutDialog</name>
+    <message>
+        <location filename="../timeoutdialog.ui" line="14"/>
+        <source>Settings are changed</source>
+        <translation>설정이 변경됨</translation>
+    </message>
+    <message>
+        <location filename="../timeoutdialog.ui" line="46"/>
+        <source>Are the current settings OK for you?</source>
+        <translation>현재 설정에 문제가 없습니까?</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../timeoutdialog.cpp" line="63"/>
+        <source>%n second(s) remaining</source>
+        <translation>
+            <numerusform>%n초 남음</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
