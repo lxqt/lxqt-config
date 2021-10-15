ae7abcc30b5c163ec81cb4cddbf3dccdd7f7e516
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2021-10-15 09:02:34 +0200

    Weblate commits (#776)
    Co-authored-by: Dwi Kristianto <steamboatid@gmail.com>
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Paulo Santiago <falecom.pw@gmail.com>
    Co-authored-by: Zhang Wei <zarchwegiant@126.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/id/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/pt_BR/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/zh_CN/
    Translation: LXQt Configuration/lxqt-config-appearance

--- lxqt-config-appearance/translations/lxqt-config-appearance_pt.ts
+++ lxqt-config-appearance/translations/lxqt-config-appearance_pt.ts
@@ -29,12 +29,12 @@
     <message>
         <location filename="../configothertoolkits.cpp" line="235"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro</translation>
     </message>
     <message>
         <location filename="../configothertoolkits.cpp" line="235"/>
         <source>Error: gsettings cannot be run</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro: não é possível executar o gsettings</translation>
     </message>
 </context>
 <context>
@@ -203,17 +203,17 @@
         <location filename="../palettes.ui" line="14"/>
         <location filename="../palettes.ui" line="20"/>
         <source>Palettes</source>
-        <translation type="unfinished"></translation>
+        <translation>Paletas</translation>
     </message>
     <message>
         <location filename="../palettes.ui" line="33"/>
         <source>Filter...</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtrar...</translation>
     </message>
     <message>
         <location filename="../palettes.ui" line="43"/>
         <source>&amp;Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Remover</translation>
     </message>
 </context>
 <context>
@@ -309,12 +309,12 @@
     <message>
         <location filename="../styleconfig.ui" line="214"/>
         <source>&amp;Save Palette</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Guardar paleta</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="221"/>
         <source>&amp;Load Palette</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Carregar paleta</translation>
     </message>
     <message>
         <location filename="../styleconfig.ui" line="241"/>
@@ -389,29 +389,31 @@ Certifique-se de que o &apos;xsettingsd&apos; está instalado para ajudar as apl
         <location filename="../styleconfig.cpp" line="294"/>
         <location filename="../styleconfig.cpp" line="304"/>
         <source>Save Palette</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar paleta</translation>
     </message>
     <message>
         <location filename="../styleconfig.cpp" line="294"/>
         <source>Palette name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nome da paleta:</translation>
     </message>
     <message>
         <location filename="../styleconfig.cpp" line="305"/>
         <source>A palette with the same name exists.
 Do you want to replace it?</source>
-        <translation type="unfinished"></translation>
+        <translation>Existe uma paleta com o mesmo nome.
+Quer substituí-la?</translation>
     </message>
     <message>
         <location filename="../styleconfig.cpp" line="374"/>
         <source>Remove Palettes</source>
-        <translation type="unfinished"></translation>
+        <translation>Remover paletas</translation>
     </message>
     <message>
         <location filename="../styleconfig.cpp" line="375"/>
         <source>Do you really want to remove selected palette(s)?
 Root palettes will remain intact if existing.</source>
-        <translation type="unfinished"></translation>
+        <translation>Quer realmente remover a(s) paleta(s) selecionada(s)?
+As paletas do root permanecerão intactas se existirem.</translation>
     </message>
 </context>
 </TS>
