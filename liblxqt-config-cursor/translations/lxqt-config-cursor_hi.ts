a639bbb6b7d23f3aa77e9911d1c4c6d9597bd360
Weblate <noreply@weblate.org>
2022-03-14 08:09:10 +0100

    Weblate commits
    Co-authored-by: Bashishtha Narayan Singh <bashishtha.singh@gmail.com>
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/liblxqt-config-cursor/hi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/hi/
    Translation: LXQt Configuration/liblxqt-config-cursor
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-locale

---
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_hi.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hi">
+<context>
+    <name>SelectWnd</name>
+    <message>
+        <location filename="../selectwnd.ui" line="14"/>
+        <source>LXQt Mouse Theme Configuration</source>
+        <translation>LXQt माउस थीम कॉन्फ़िगरेशन</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="46"/>
+        <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
+        <translation>उस कर्सर थीम का चयन करें जिसका आप उपयोग करना चाहते हैं (कर्सर का परीक्षण करने के लिए होवर पूर्वावलोकन करें).&lt;b&gt;इस परिवर्तन के बाद LXQt सत्र को पुनः आरंभ करने की आवश्यकता है&lt;/b&gt;:</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="23"/>
+        <source>&amp;Install New Theme...</source>
+        <translation>नई थीम &amp;स्थापित करें...</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="72"/>
+        <source>LXQt session needs restart to view this change.</source>
+        <translation>इस परिवर्तन को देखने के लिए LXQt सत्र को पुनः आरंभ करने की आवश्यकता है.</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="91"/>
+        <source>&amp;Remove Theme</source>
+        <translation>थीम &amp;निकालें</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="111"/>
+        <source>Size</source>
+        <translation>आकार</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.cpp" line="231"/>
+        <source>XCurTheme error</source>
+        <translation>XCurTheme त्रुटि</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.cpp" line="232"/>
+        <source>You can&apos;t remove active theme!</source>
+        <translation>आप सक्रिय थीम को नहीं हटा सकते!</translation>
+    </message>
+</context>
+<context>
+    <name>WarningLabel</name>
+    <message>
+        <location filename="../warninglabel.ui" line="14"/>
+        <source>Form</source>
+        <translation>फारम</translation>
+    </message>
+    <message>
+        <location filename="../warninglabel.ui" line="44"/>
+        <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
+        <translation>LXQt को कोई कर्सर थीम नहीं मिली. इसके बजाय आरंभिक X11 कर्सर थीम का उपयोग किया जाएगा. LXQt ने निम्नलिखित निर्देशिकाओं में देखा :</translation>
+    </message>
+    <message>
+        <location filename="../warninglabel.ui" line="54"/>
+        <source>Show...</source>
+        <translation>दिखाएं...</translation>
+    </message>
+</context>
+<context>
+    <name>XCursorThemeModel</name>
+    <message>
+        <location filename="../thememodel.cpp" line="61"/>
+        <source>Name</source>
+        <translation>नाम</translation>
+    </message>
+    <message>
+        <location filename="../thememodel.cpp" line="62"/>
+        <source>Description</source>
+        <translation>विवरण</translation>
+    </message>
+</context>
+</TS>
