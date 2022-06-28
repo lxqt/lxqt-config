7da1a7d6e0a73edc453b18d9c98447f68beeee01
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-06-28 10:24:05 +0200

    Weblate commits (#867)
    Co-authored-by: Dejweed <12davidkac@gmail.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: cakkynorris <cakkynorris@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/id/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/id/
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-locale

--- lxqt-config-locale/translations/lxqt-config-locale_id.ts
+++ lxqt-config-locale/translations/lxqt-config-locale_id.ts
@@ -41,7 +41,7 @@
     <message>
         <location filename="../localeconfig.ui" line="226"/>
         <source>Examples</source>
-        <translation type="unfinished"></translation>
+        <translation>Contoh</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="238"/>
