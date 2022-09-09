7ed90a67467c3e81c3fa465e434eec10c077008b
Weblate <noreply@weblate.org>
2022-09-09 05:10:14 +0200

    Weblate commits
    Co-authored-by: Daniel <d3vs4n@tutanota.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/ca/
    Translation: LXQt Configuration/lxqt-config-file-associations

--- lxqt-config-file-associations/translations/lxqt-config-file-associations_ca.ts
+++ lxqt-config-file-associations/translations/lxqt-config-file-associations_ca.ts
@@ -57,7 +57,7 @@
         <location filename="../applicationchooser.cpp" line="193"/>
         <location filename="../applicationchooser.cpp" line="219"/>
         <source>Other applications that handle %1</source>
-        <translation type="unfinished">Altres aplicacions de gestió de %1</translation>
+        <translation>Altres aplicacions que gestionen %1</translation>
     </message>
     <message>
         <location filename="../applicationchooser.cpp" line="194"/>
