e079574add501aa377502583cf2d0f84977e5ea0
Weblate <noreply@weblate.org>
2022-12-30 13:45:09 +0100

    Weblate commits
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Co-authored-by: Bashishtha Narayan Singh <bashishtha.singh@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/nb_NO/
    Translation: LXQt Configuration/lxqt-config-locale

--- lxqt-config-locale/translations/lxqt-config-locale_nb_NO.ts
+++ lxqt-config-locale/translations/lxqt-config-locale_nb_NO.ts
@@ -41,7 +41,7 @@
     <message>
         <location filename="../localeconfig.ui" line="226"/>
         <source>Examples</source>
-        <translation type="unfinished"></translation>
+        <translation>Eksempler</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="238"/>
