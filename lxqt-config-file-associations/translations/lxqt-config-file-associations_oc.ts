679c4e2c85a1b1e75322570ba79b8ab4a34a0a13
Weblate <noreply@weblate.org>
2023-01-24 19:11:05 +0100

    Weblate commits
    Co-authored-by: Quentin PAGÈS <quentinantonin@free.fr>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor-menu-entry/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-configuration-center-menu-entry/oc/
    Translation: LXQt Configuration/LXQt Configuration Center (Menu Entry)
    Translation: LXQt Configuration/lxqt-config-file-associations
    Translation: LXQt Configuration/lxqt-config-locale
    Translation: LXQt Configuration/lxqt-config-monitor
    Translation: LXQt Configuration/lxqt-config-monitor Menu Entry

---
+++ lxqt-config-file-associations/translations/lxqt-config-file-associations_oc.ts
@@ -0,0 +1,202 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="oc">
+<context>
+    <name>ApplicationChooser</name>
+    <message>
+        <location filename="../applicationchooser.ui" line="14"/>
+        <source>ApplicationChooser</source>
+        <translation>Selector d’aplicacions</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="46"/>
+        <source>Pick an application for:</source>
+        <translation>Causir una aplicacion per :</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="110"/>
+        <source>1</source>
+        <translation>1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="115"/>
+        <source>Applications that handle JPEG</source>
+        <translation>Las aplicacions que gerisson JPEG</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="124"/>
+        <location filename="../applicationchooser.ui" line="129"/>
+        <location filename="../applicationchooser.ui" line="139"/>
+        <location filename="../applicationchooser.ui" line="144"/>
+        <source>New Subitem</source>
+        <translation>Sostítol novèl</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="135"/>
+        <location filename="../applicationchooser.cpp" line="191"/>
+        <location filename="../applicationchooser.cpp" line="217"/>
+        <source>Other applications</source>
+        <translation>Autras aplicacions</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="147"/>
+        <source>Web browsers</source>
+        <translation>Navegadors web</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="152"/>
+        <source>Email clients</source>
+        <translation>Clients de messatjariá</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="157"/>
+        <source>File managers</source>
+        <translation>Gestionari de fichièrs</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="193"/>
+        <location filename="../applicationchooser.cpp" line="219"/>
+        <source>Other applications that handle %1</source>
+        <translation>Autras aplicacions de gestion de %1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="194"/>
+        <location filename="../applicationchooser.cpp" line="220"/>
+        <source>Applications that handle %1</source>
+        <translation>Aplicacions que gerisson %1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="305"/>
+        <source>No applications found</source>
+        <translation>Cap d’aplicacion pas trobada</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="310"/>
+        <source>No more applications found</source>
+        <translation>Pas mai d’aplicacions pas trobadas</translation>
+    </message>
+</context>
+<context>
+    <name>MimetypeViewer</name>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="271"/>
+        <location filename="../mimetypeviewer.cpp" line="290"/>
+        <location filename="../mimetypeviewer.cpp" line="309"/>
+        <source>Change...</source>
+        <translation>Cambiar...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="275"/>
+        <location filename="../mimetypeviewer.cpp" line="294"/>
+        <location filename="../mimetypeviewer.cpp" line="313"/>
+        <location filename="../mimetypeviewer.cpp" line="327"/>
+        <location filename="../mimetypeviewer.cpp" line="388"/>
+        <source>None</source>
+        <translation>Cap</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="276"/>
+        <location filename="../mimetypeviewer.cpp" line="295"/>
+        <location filename="../mimetypeviewer.cpp" line="314"/>
+        <source>Choose...</source>
+        <translation>Causir...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="384"/>
+        <source>&amp;Change...</source>
+        <translation>&amp;Cambiar...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="389"/>
+        <source>&amp;Choose...</source>
+        <translation>&amp;Causir...</translation>
+    </message>
+</context>
+<context>
+    <name>mimetypeviewer</name>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="20"/>
+        <source>File Associations</source>
+        <translation>Associacions de fichièrs</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="30"/>
+        <source>Associations</source>
+        <translation>Associacions</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="206"/>
+        <source>Default application</source>
+        <translation>Aplicacion per defaut</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="271"/>
+        <source>&amp;Choose...</source>
+        <translation>&amp;Causir...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="305"/>
+        <source>Default Applications</source>
+        <translation>Aplicacions per defaut</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="347"/>
+        <source>Web Browser</source>
+        <translation>Navegador web</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="412"/>
+        <location filename="../mimetypeviewer.ui" line="487"/>
+        <location filename="../mimetypeviewer.ui" line="562"/>
+        <source>Choose...</source>
+        <translation>Causir...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="422"/>
+        <source>Email Client</source>
+        <translation>Clients de messatjariá</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="497"/>
+        <source>File Manager</source>
+        <translation>Gestionari de fichièrs</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="255"/>
+        <location filename="../mimetypeviewer.ui" line="396"/>
+        <location filename="../mimetypeviewer.ui" line="471"/>
+        <location filename="../mimetypeviewer.ui" line="546"/>
+        <source>None</source>
+        <translation>Cap</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="187"/>
+        <source>Patterns</source>
+        <translation>Motius</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="193"/>
+        <source>*.txt *.xml</source>
+        <translation>*.txt *.xml</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="146"/>
+        <source>MIME type</source>
+        <translation>Tipe MIME</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="164"/>
+        <location filename="../mimetypeviewer.ui" line="248"/>
+        <location filename="../mimetypeviewer.ui" line="389"/>
+        <location filename="../mimetypeviewer.ui" line="464"/>
+        <location filename="../mimetypeviewer.ui" line="539"/>
+        <source>Icon</source>
+        <translation>Icòna</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="171"/>
+        <source>Description</source>
+        <translation>Descripcion</translation>
+    </message>
+</context>
+</TS>
