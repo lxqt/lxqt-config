fdc1a5111e04ccb755acdf56a937274c9ec8b820
Weblate <noreply@weblate.org>
2023-01-25 07:59:50 +0100

    Weblate commits
    Co-authored-by: Quentin PAGÈS <quentinantonin@free.fr>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/liblxqt-config-cursor/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance-menu-entry/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations-menu-entry/oc/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-settings-menu-category-name/oc/
    Translation: LXQt Configuration/LXQt Settings (Menu Category Name)
    Translation: LXQt Configuration/liblxqt-config-cursor
    Translation: LXQt Configuration/lxqt-config-appearance Menu Entry
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-file-associations Menu Entry

---
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_oc.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="oc">
+<context>
+    <name>SelectWnd</name>
+    <message>
+        <location filename="../selectwnd.ui" line="14"/>
+        <source>LXQt Mouse Theme Configuration</source>
+        <translation>Configuracion del tèma de la mirga de LXQt</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="46"/>
+        <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="23"/>
+        <source>&amp;Install New Theme...</source>
+        <translation>&amp;Installar un tèma novèl...</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="72"/>
+        <source>LXQt session needs restart to view this change.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="91"/>
+        <source>&amp;Remove Theme</source>
+        <translation>&amp;Suprimir lo tèma</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.ui" line="111"/>
+        <source>Size</source>
+        <translation>Talha</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.cpp" line="231"/>
+        <source>XCurTheme error</source>
+        <translation>Error XCurTheme</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.cpp" line="232"/>
+        <source>You can&apos;t remove active theme!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WarningLabel</name>
+    <message>
+        <location filename="../warninglabel.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formulari</translation>
+    </message>
+    <message>
+        <location filename="../warninglabel.ui" line="44"/>
+        <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../warninglabel.ui" line="54"/>
+        <source>Show...</source>
+        <translation>Afichar...</translation>
+    </message>
+</context>
+<context>
+    <name>XCursorThemeModel</name>
+    <message>
+        <location filename="../thememodel.cpp" line="61"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../thememodel.cpp" line="62"/>
+        <source>Description</source>
+        <translation>Descripcion</translation>
+    </message>
+</context>
+</TS>
