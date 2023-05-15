e8c7b0972dddb1150883538a13ea9440f2a76dc4
Weblate <noreply@weblate.org>
2023-05-15 02:11:53 +0200

    Weblate commits
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness-menu-entry/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations-menu-entry/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input-menu-entry/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locate-menu-entry/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor-menu-entry/fi/
    Translation: LXQt Configuration/lxqt-config-brightness Menu Entry
    Translation: LXQt Configuration/lxqt-config-file-associations Menu Entry
    Translation: LXQt Configuration/lxqt-config-input Menu Entry
    Translation: LXQt Configuration/lxqt-config-locale Menu Entry
    Translation: LXQt Configuration/lxqt-config-monitor Menu Entry

---
+++ lxqt-config-locale/translations/lxqt-config-locale_fi.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>LocaleConfig</name>
+    <message>
+        <location filename="../localeconfig.ui" line="31"/>
+        <source>Re&amp;gion:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="60"/>
+        <source>De&amp;tailed Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="78"/>
+        <source>&amp;Numbers:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="107"/>
+        <source>&amp;Time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="136"/>
+        <source>Currenc&amp;y:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="165"/>
+        <source>Measurement &amp;Units:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="194"/>
+        <source>Co&amp;llation and Sorting:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="226"/>
+        <source>Examples</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="238"/>
+        <source>Numbers:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="258"/>
+        <source>Time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="278"/>
+        <source>Currency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="298"/>
+        <source>Measurement Units:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="126"/>
+        <source>No change</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="313"/>
+        <source>Format Settings Changed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="314"/>
+        <source>Do you want to save your changes? They will take effect the next time you log in.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="417"/>
+        <source>Imperial UK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="421"/>
+        <source>Imperial US</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="425"/>
+        <source>Metric</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="50"/>
+        <source>LXQt Locale Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="55"/>
+        <source>Locale Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
