92bf847d21c9de00e84f0b4c432bea11f1201406
Weblate <noreply@weblate.org>
2023-05-21 05:11:45 +0200

    Weblate commits
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/fi/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/fi/
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-file-associations
    Translation: LXQt Configuration/lxqt-config-locale

--- lxqt-config-locale/translations/lxqt-config-locale_fi.ts
+++ lxqt-config-locale/translations/lxqt-config-locale_fi.ts
@@ -6,92 +6,92 @@
     <message>
         <location filename="../localeconfig.ui" line="31"/>
         <source>Re&amp;gion:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Alue:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="60"/>
         <source>De&amp;tailed Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Tarkat asetukset</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="78"/>
         <source>&amp;Numbers:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Numerot:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="107"/>
         <source>&amp;Time:</source>
-        <translation type="unfinished"></translation>
+        <translation>A&amp;ika:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="136"/>
         <source>Currenc&amp;y:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Valuutta:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="165"/>
         <source>Measurement &amp;Units:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Mittayksiköt:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="194"/>
         <source>Co&amp;llation and Sorting:</source>
-        <translation type="unfinished"></translation>
+        <translation>Aa&amp;kkostus ja lajittelu:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="226"/>
         <source>Examples</source>
-        <translation type="unfinished"></translation>
+        <translation>Esimerkit</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="238"/>
         <source>Numbers:</source>
-        <translation type="unfinished"></translation>
+        <translation>Numerot:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="258"/>
         <source>Time:</source>
-        <translation type="unfinished"></translation>
+        <translation>Aika:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="278"/>
         <source>Currency:</source>
-        <translation type="unfinished"></translation>
+        <translation>Valuutta:</translation>
     </message>
     <message>
         <location filename="../localeconfig.ui" line="298"/>
         <source>Measurement Units:</source>
-        <translation type="unfinished"></translation>
+        <translation>Mittayksiköt:</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="126"/>
         <source>No change</source>
-        <translation type="unfinished"></translation>
+        <translation>Ei muutosta</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="313"/>
         <source>Format Settings Changed</source>
-        <translation type="unfinished"></translation>
+        <translation>Muotoasetukset muutettu</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="314"/>
         <source>Do you want to save your changes? They will take effect the next time you log in.</source>
-        <translation type="unfinished"></translation>
+        <translation>Haluatko tallentaa muutokset? Muutokset tulevat voimaan, kun kirjaudut uudelleen sisään.</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="417"/>
         <source>Imperial UK</source>
-        <translation type="unfinished"></translation>
+        <translation>Brittiläinen</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="421"/>
         <source>Imperial US</source>
-        <translation type="unfinished"></translation>
+        <translation>Amerikkalainen</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="425"/>
         <source>Metric</source>
-        <translation type="unfinished"></translation>
+        <translation>Metrinen</translation>
     </message>
 </context>
 <context>
@@ -99,12 +99,12 @@
     <message>
         <location filename="../main.cpp" line="50"/>
         <source>LXQt Locale Configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt:n maa-asetukset</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="55"/>
         <source>Locale Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Maa-asetukset</translation>
     </message>
 </context>
 </TS>
