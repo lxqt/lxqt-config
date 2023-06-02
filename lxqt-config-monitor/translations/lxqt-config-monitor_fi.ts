868cddb0182ba3445575f8554f05e72b1131a583
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-06-02 22:03:16 +0200

    Weblate commits (#946)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/fi/
    Translation: LXQt Configuration/lxqt-config-monitor
    
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>

--- lxqt-config-monitor/translations/lxqt-config-monitor_fi.ts
+++ lxqt-config-monitor/translations/lxqt-config-monitor_fi.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../fastmenu.ui" line="20"/>
         <source>Fast options</source>
-        <translation type="unfinished"></translation>
+        <translation>Pika-asetukset</translation>
     </message>
     <message>
         <location filename="../fastmenu.ui" line="40"/>
         <source> ❶ ❷ Extended view</source>
-        <translation type="unfinished"></translation>
+        <translation> ❶ ❷ Laajennettu näkymä</translation>
     </message>
     <message>
         <location filename="../fastmenu.ui" line="45"/>
         <source> ❶ ❶ Unified view</source>
-        <translation type="unfinished"></translation>
+        <translation> ❶ ❶ Yhdistetty näkymä</translation>
     </message>
     <message>
         <location filename="../fastmenu.ui" line="50"/>
         <source> ❶     Only first</source>
-        <translation type="unfinished"></translation>
+        <translation> ❶     Vain ensimmäinen</translation>
     </message>
     <message>
         <location filename="../fastmenu.ui" line="55"/>
         <source>     ❷ Only second</source>
-        <translation type="unfinished"></translation>
+        <translation>     ❷ Vain toinen</translation>
     </message>
 </context>
 <context>
@@ -34,7 +34,7 @@
     <message>
         <location filename="../loadsettings.cpp" line="98"/>
         <source>Default monitor settings has been applied. If you want change monitors settings, please, use lxqt-config-monitor.</source>
-        <translation type="unfinished"></translation>
+        <translation>Näytön oletusasetukset on otettu käyttöön. Jos haluat muuttaa näytön asetuksia, käytä lxqt-config-monitor.</translation>
     </message>
 </context>
 <context>
@@ -42,69 +42,69 @@
     <message>
         <location filename="../managesavedsettings.ui" line="14"/>
         <source>Monitor Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Näytön asetukset</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.ui" line="27"/>
         <source>Saved settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Tallennetut asetukset</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.ui" line="38"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Uudelleennimeä</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.ui" line="45"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Poista</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.ui" line="52"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Käytä</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="62"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Ei</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="65"/>
         <source>Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Vastapäivään</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="68"/>
         <source>Inverted</source>
-        <translation type="unfinished"></translation>
+        <translation>Ylösalaisin</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="71"/>
         <source>Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Myötäpäivään</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="75"/>
         <location filename="../managesavedsettings.cpp" line="76"/>
         <source>True</source>
-        <translation type="unfinished"></translation>
+        <translation>Kyllä</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="75"/>
         <location filename="../managesavedsettings.cpp" line="76"/>
         <source>False</source>
-        <translation type="unfinished"></translation>
+        <translation>Ei</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="136"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nimi</translation>
     </message>
     <message>
         <location filename="../managesavedsettings.cpp" line="136"/>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nimi:</translation>
     </message>
 </context>
 <context>
@@ -112,7 +112,7 @@
     <message>
         <location filename="../monitorpicture.ui" line="27"/>
         <source>Keep monitors attached</source>
-        <translation type="unfinished"></translation>
+        <translation>Muista järjestys</translation>
     </message>
 </context>
 <context>
@@ -120,27 +120,27 @@
     <message>
         <location filename="../monitorsettingsdialog.ui" line="14"/>
         <source>Monitor Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Näytön asetukset</translation>
     </message>
     <message>
         <location filename="../monitorsettingsdialog.ui" line="70"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetukset</translation>
     </message>
     <message>
         <location filename="../monitorsettingsdialog.cpp" line="106"/>
         <source>Fast Menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Pikavalikko</translation>
     </message>
     <message>
         <location filename="../monitorsettingsdialog.cpp" line="110"/>
         <source>Set position</source>
-        <translation type="unfinished"></translation>
+        <translation>Määritä sijainnit</translation>
     </message>
     <message>
         <location filename="../monitorsettingsdialog.cpp" line="229"/>
         <source>Advanced settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisäasetukset</translation>
     </message>
 </context>
 <context>
@@ -148,123 +148,129 @@
     <message>
         <location filename="../monitorwidget.ui" line="21"/>
         <source>Setup</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetukset</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="29"/>
         <source> (x)</source>
-        <translation type="unfinished"></translation>
+        <translation> (x)</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="45"/>
         <source> (y)</source>
-        <translation type="unfinished"></translation>
+        <translation> (y)</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="78"/>
         <source>Resolution:</source>
-        <translation type="unfinished"></translation>
+        <translation>Resoluutio:</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="90"/>
         <source>Enable this display</source>
-        <translation type="unfinished"></translation>
+        <translation>Ota käyttöön</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="98"/>
         <source>This is my primary display</source>
-        <translation type="unfinished"></translation>
+        <translation>Tämä on ensisijainen näyttö</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="103"/>
         <source>This screen extends another display</source>
-        <translation type="unfinished"></translation>
+        <translation>Tämä laajentaa toista näyttöä</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="125"/>
         <source>Advanced</source>
-        <translation type="unfinished"></translation>
+        <translation>Lisäasetukset</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="146"/>
         <source>Refresh rate:</source>
-        <translation type="unfinished"></translation>
+        <translation>Virkistystaajuus:</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="153"/>
         <source>Rotation:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kierto:</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="176"/>
         <source>Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Tietoja</translation>
     </message>
     <message>
         <location filename="../monitorwidget.ui" line="182"/>
         <source>Display information</source>
-        <translation type="unfinished"></translation>
+        <translation>Näytön tiedot</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="129"/>
         <source>Name: %1
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Nimi: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="130"/>
         <source>Vendor: %1
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Valmistaja: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="131"/>
         <source>Serial: %1
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Sarja: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="132"/>
         <source>Display size: %1cm x %2cm
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Näytön koko: %1cm x %2cm
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="133"/>
         <source>Serial number: %1
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Sarjanumero: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="134"/>
         <source>EISA device ID: %1
 </source>
-        <translation type="unfinished"></translation>
+        <translation>EISA-laitetunnus: %1
+</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="154"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Ei</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="155"/>
         <source>Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Vastapäivään</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="156"/>
         <source>Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Myötäpäivään</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="157"/>
         <source>Inverted</source>
-        <translation type="unfinished"></translation>
+        <translation>Ylösalaisin</translation>
     </message>
     <message>
         <location filename="../monitorwidget.cpp" line="245"/>
         <source>%1 Hz</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Hz</translation>
     </message>
 </context>
 <context>
@@ -272,7 +278,7 @@
     <message>
         <location filename="../settingsdialog.cpp" line="33"/>
         <source>Manage Saved Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Hallitse tallennettuja asetuksia</translation>
     </message>
 </context>
 <context>
@@ -280,12 +286,12 @@
     <message>
         <location filename="../savesettings.cpp" line="87"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nimi</translation>
     </message>
     <message>
         <location filename="../savesettings.cpp" line="87"/>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nimi:</translation>
     </message>
 </context>
 <context>
@@ -293,19 +299,19 @@
     <message>
         <location filename="../timeoutdialog.ui" line="14"/>
         <source>Settings are changed</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetuksia muutettiin</translation>
     </message>
     <message>
         <location filename="../timeoutdialog.ui" line="46"/>
         <source>Are the current settings OK for you?</source>
-        <translation type="unfinished"></translation>
+        <translation>Otetaanko käyttöön nämä asetukset?</translation>
     </message>
     <message numerus="yes">
         <location filename="../timeoutdialog.cpp" line="63"/>
         <source>%n second(s) remaining</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n sekunti jäljellä</numerusform>
+            <numerusform>%n sekuntia jäljellä</numerusform>
         </translation>
     </message>
 </context>
