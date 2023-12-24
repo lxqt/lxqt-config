d094a330c5952901cbb4f2ec5ae34ad521c8df33
Weblate <noreply@weblate.org>
2023-12-24 20:23:05 +0100

    Weblate commits
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Co-authored-by: Roberalz <lumintnian@outlook.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/liblxqt-config-cursor/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/eu/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/eu/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locate-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor-menu-entry/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config/pa/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-configuration-center-menu-entry/pa/
    Translation: LXQt Configuration/LXQt Configuration Center (Menu Entry)
    Translation: LXQt Configuration/liblxqt-config-cursor
    Translation: LXQt Configuration/lxqt-config
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-brightness Menu Entry
    Translation: LXQt Configuration/lxqt-config-file-associations Menu Entry
    Translation: LXQt Configuration/lxqt-config-input
    Translation: LXQt Configuration/lxqt-config-input Menu Entry
    Translation: LXQt Configuration/lxqt-config-locale
    Translation: LXQt Configuration/lxqt-config-locale Menu Entry
    Translation: LXQt Configuration/lxqt-config-monitor
    Translation: LXQt Configuration/lxqt-config-monitor Menu Entry

---
+++ lxqt-config-locale/translations/lxqt-config-locale_pa.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pa">
+<context>
+    <name>LocaleConfig</name>
+    <message>
+        <location filename="../localeconfig.ui" line="31"/>
+        <source>Re&amp;gion:</source>
+        <translation>ਖੇਤਰ(&amp;g):</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="60"/>
+        <source>De&amp;tailed Settings</source>
+        <translation>ਵੇਰਵੇ ਸਮੇਤ ਸੈਟਿੰਗਾਂ(&amp;t)</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="78"/>
+        <source>&amp;Numbers:</source>
+        <translation>ਨੰਬਰ(&amp;N):</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="107"/>
+        <source>&amp;Time:</source>
+        <translation>ਸਮਾਂ(&amp;T):</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="136"/>
+        <source>Currenc&amp;y:</source>
+        <translation>ਕਰੰਸੀ(&amp;y):</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="165"/>
+        <source>Measurement &amp;Units:</source>
+        <translation>ਮਾਪ ਇਕਾਈਆਂ(&amp;U):</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="194"/>
+        <source>Co&amp;llation and Sorting:</source>
+        <translation>ਲੜੀਬੱਧ(&amp;l):</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="226"/>
+        <source>Examples</source>
+        <translation>ਮਿਸਾਲਾਂ</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="238"/>
+        <source>Numbers:</source>
+        <translation>ਅੰਕ:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="258"/>
+        <source>Time:</source>
+        <translation>ਸਮਾਂ:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="278"/>
+        <source>Currency:</source>
+        <translation>ਕਰੰਸੀ:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.ui" line="298"/>
+        <source>Measurement Units:</source>
+        <translation>ਮਾਪ ਇਕਾਈਆਂ:</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="126"/>
+        <source>No change</source>
+        <translation>ਕੋਈ ਤਬਦੀਲੀ ਨਹੀਂ</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="313"/>
+        <source>Format Settings Changed</source>
+        <translation>ਫਾਰਮੈਟ ਸੈਟਿੰਗਾਂ ਬਦਲੀਆਂ</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="314"/>
+        <source>Do you want to save your changes? They will take effect the next time you log in.</source>
+        <translation>ਕੀ ਤੁਸੀਂ ਆਪਣੀਆਂ ਤਬਦੀਲੀਆਂ ਨੂੰ ਸੰਭਾਲਣਾ ਚਾਹੁੰਦੇ ਹੋ? ਇਸ ਤੁਹਾਡੇ ਅਗਲੀ ਵਾਰ ਲਾਗ ਇਨ ਕਰਨ ਉੱਤੇ ਪ੍ਰਭਾਵੀ ਹੋਣਗੀਆਂ।</translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="417"/>
+        <source>Imperial UK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="421"/>
+        <source>Imperial US</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../localeconfig.cpp" line="425"/>
+        <source>Metric</source>
+        <translation>ਮੈਟਰਿਕ</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="50"/>
+        <source>LXQt Locale Configuration</source>
+        <translation>LXQt ਲੋਕੇਲ ਸੰਰਚਨਾ</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="55"/>
+        <source>Locale Settings</source>
+        <translation>ਲੋਕੇਲ ਸੈਟਿੰਗਾਂ</translation>
+    </message>
+</context>
+</TS>
