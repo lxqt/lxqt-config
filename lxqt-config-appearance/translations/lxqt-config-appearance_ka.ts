95a7ccdb7699db5ee15c4396cf84a222e77bdea7
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-01-04 10:02:37 +0100

    Weblate commits (#1084)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/liblxqt-config-cursor/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-file-associations/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locate-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor-menu-entry/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-monitor/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-configuration-center-menu-entry/ka/
    Translation: LXQt Configuration/LXQt Configuration Center (Menu Entry)
    Translation: LXQt Configuration/liblxqt-config-cursor
    Translation: LXQt Configuration/lxqt-config
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-appearance Menu Entry
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-brightness Menu Entry
    Translation: LXQt Configuration/lxqt-config-file-associations
    Translation: LXQt Configuration/lxqt-config-file-associations Menu Entry
    Translation: LXQt Configuration/lxqt-config-input
    Translation: LXQt Configuration/lxqt-config-input Menu Entry
    Translation: LXQt Configuration/lxqt-config-locale
    Translation: LXQt Configuration/lxqt-config-locale Menu Entry
    Translation: LXQt Configuration/lxqt-config-monitor
    Translation: LXQt Configuration/lxqt-config-monitor Menu Entry
    
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>

---
+++ lxqt-config-appearance/translations/lxqt-config-appearance_ka.ts
@@ -0,0 +1,496 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>ColorLabel</name>
+    <message>
+        <location filename="../colorLabel.cpp" line="37"/>
+        <source>Click to change color.</source>
+        <translation>დააწკაპუნეთ ფერის შესაცვლელად.</translation>
+    </message>
+    <message>
+        <location filename="../colorLabel.cpp" line="61"/>
+        <source>Select Color</source>
+        <translation>ფერი</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigOtherToolKits</name>
+    <message>
+        <location filename="../configothertoolkits.cpp" line="178"/>
+        <source>GTK themes</source>
+        <translation>GTK-ის თემები</translation>
+    </message>
+    <message>
+        <location filename="../configothertoolkits.cpp" line="179"/>
+        <source>&lt;p&gt;&apos;%1&apos; has been overwritten.&lt;/p&gt;&lt;p&gt;You can find a copy of your old settings in &apos;%2&apos;&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&apos;%1&apos;-ს თავზე გადაეწერა.&lt;/p&gt;&lt;p&gt;ძველი პარამეტრების ასლი შეგიძლიათ იპოვოთ აქ: &apos;%2&apos;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../configothertoolkits.cpp" line="244"/>
+        <source>Error</source>
+        <translation>შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../configothertoolkits.cpp" line="244"/>
+        <source>Error: gsettings cannot be run</source>
+        <translation>შეცდომა: gsettings-ის გაშვება შეუძლებელია</translation>
+    </message>
+</context>
+<context>
+    <name>FontsConfig</name>
+    <message>
+        <location filename="../fontsconfig.ui" line="23"/>
+        <source>Font</source>
+        <translation>ფონტი</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="30"/>
+        <source>Default font for user interface</source>
+        <translation>ნაგულისხმევი ფონტი მომხმარებლის ინტერფეისისთვის</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="36"/>
+        <source>Font name:</source>
+        <translation>ფონტის სახელი:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="46"/>
+        <source>Style:</source>
+        <translation>სტილი:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="77"/>
+        <source>Point size:</source>
+        <translation>წერტილის ზომა:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="54"/>
+        <source>Normal</source>
+        <translation>ნორმალური</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="59"/>
+        <source>Bold</source>
+        <translation>გასქელება</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="64"/>
+        <source>Italic</source>
+        <translation>დახრა</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="69"/>
+        <source>Bold Italic</source>
+        <translation>დახრილი მუქი</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="94"/>
+        <source>The following settings only affect newly started applications</source>
+        <translation>ეს პარამეტრები, მხოლოდ, ახლად გაშვებულ აპლიკაციებს შეეხება</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="100"/>
+        <source>Use antialias fonts</source>
+        <translation>მოგლუვებული ფონტების გამოყენება</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="153"/>
+        <source>Font hinting style:</source>
+        <translation>ფონტის ჰინტინგის სტილი:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="118"/>
+        <location filename="../fontsconfig.ui" line="164"/>
+        <source>None</source>
+        <translation>არაფერი</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="169"/>
+        <source>Slight</source>
+        <translation>ოდნავ</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="174"/>
+        <source>Medium</source>
+        <translation>საშუალო</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="179"/>
+        <source>Full</source>
+        <translation>სრული</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="146"/>
+        <source>Font hinting</source>
+        <translation>ფონტის ჰინტინგი</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="187"/>
+        <source>Resolution (DPI):</source>
+        <translation>გაფართოება (DPI):</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="204"/>
+        <source>Autohint</source>
+        <translation>ავტოჰინტინგი</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="107"/>
+        <source>Subpixel antialiasing:</source>
+        <translation>ქვეპიქსელის მოგლუვება:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="123"/>
+        <source>RGB</source>
+        <translation>RGB</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="128"/>
+        <source>BGR</source>
+        <translation>BGR</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="133"/>
+        <source>VRGB</source>
+        <translation>VRGB</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="138"/>
+        <source>VBGR</source>
+        <translation>VBGR</translation>
+    </message>
+</context>
+<context>
+    <name>GTKConfig</name>
+    <message>
+        <location filename="../gtkconfig.ui" line="44"/>
+        <source>GTK Style</source>
+        <translation>GTK-ის სტილი</translation>
+    </message>
+    <message>
+        <location filename="../gtkconfig.ui" line="63"/>
+        <source>Set GTK themes (GTK configuration files will be overwritten!)</source>
+        <translation>GTK-ის თემის დაყენება (GTK-ის კონფიგურაციის ფაილები შეიცვლება!)</translation>
+    </message>
+    <message>
+        <location filename="../gtkconfig.ui" line="93"/>
+        <source>GTK 3 Theme</source>
+        <translation>GTK 3-ის თემა</translation>
+    </message>
+    <message>
+        <location filename="../gtkconfig.ui" line="77"/>
+        <source>GTK 2 Theme</source>
+        <translation>GTK 2-ის თემა</translation>
+    </message>
+    <message>
+        <location filename="../gtkconfig.ui" line="103"/>
+        <source>To attempt uniform theming, either select similar style/theme (if available) across all lists, or select &apos;gtk2&apos; Qt style (if available) to mimic GTK themes.
+
+Make sure &apos;xsettingsd&apos; is installed to help GTK applications apply themes on the fly.</source>
+        <translation>იმისათვის, რომ თემები ერთგვაროვანი იყოს, აირჩიეთ მსგავსი სტილი/თემა (თუ ხელმისაწვდომია) ყველა სიაში, ან აირჩიეთ Qt-ის სტილი &apos;gtk2&apos;, რომ GTK-ი თემის მსგავსად მოვიქცე.
+
+დარწმუნდით, რომ პროგრამა &apos;xsettingsd&apos; დაყენებულია, რომ GTK-ის აპლიკაციის თემები გაშვებულობის დროსაც კი შეიცვალოს.</translation>
+    </message>
+</context>
+<context>
+    <name>IconThemeConfig</name>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="14"/>
+        <source>LXQt Appearance Configuration</source>
+        <translation>LXQt-ის გარეგნობის მორგება</translation>
+    </message>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="26"/>
+        <source>Icons Theme</source>
+        <translation>ხატულების თემა</translation>
+    </message>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="87"/>
+        <source>The KDE extension of XDG icon themes -&gt; FollowsColorScheme</source>
+        <translation>KDE-ის გაფართოება XDG-ის ხატულის თემებისთვის -&gt; FollowsColorScheme</translation>
+    </message>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="90"/>
+        <source>Colorize icons based on widget style (palette)</source>
+        <translation>ხატულების გაფერადება ვიჯეტის სტილის მიხედვით (პალიტრა)</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtThemeConfig</name>
+    <message>
+        <location filename="../lxqtthemeconfig.ui" line="23"/>
+        <source>LXQt Theme</source>
+        <translation>LXQt-ის თემა</translation>
+    </message>
+    <message>
+        <location filename="../lxqtthemeconfig.ui" line="57"/>
+        <source>Use wallpaper provided by theme</source>
+        <translation>თემის მოწოდებული ფონის სურათის გამოყენება</translation>
+    </message>
+    <message>
+        <location filename="../lxqtthemeconfig.ui" line="64"/>
+        <source>Use palette provided by theme</source>
+        <translation>თემის მოწოდებული პალიტრის გამოყენება</translation>
+    </message>
+    <message>
+        <location filename="../lxqtthemeconfig.cpp" line="83"/>
+        <source>(User Theme)</source>
+        <translation>(მომხმარებლის თემა)</translation>
+    </message>
+    <message>
+        <location filename="../lxqtthemeconfig.cpp" line="185"/>
+        <source>Open theme folder</source>
+        <translation>თემების საქაღალდის გახსნა</translation>
+    </message>
+</context>
+<context>
+    <name>PalettesDialog</name>
+    <message>
+        <location filename="../palettes.ui" line="14"/>
+        <location filename="../palettes.ui" line="20"/>
+        <source>Palettes</source>
+        <translation>პალიტრები</translation>
+    </message>
+    <message>
+        <location filename="../palettes.ui" line="33"/>
+        <source>Filter...</source>
+        <translation>გაფილტვრა...</translation>
+    </message>
+    <message>
+        <location filename="../palettes.ui" line="43"/>
+        <source>&amp;Remove</source>
+        <translation>&amp;წაშლა</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="61"/>
+        <source>LXQt Appearance Configuration</source>
+        <translation>LXQt-ის გარეგნობის მორგება</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="74"/>
+        <source>Widget Style</source>
+        <translation>ვიჯეტის სტილი</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="83"/>
+        <source>Icons Theme</source>
+        <translation>ხატულების თემა</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="92"/>
+        <source>LXQt Theme</source>
+        <translation>LXQt-ის თემა</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="100"/>
+        <source>Font</source>
+        <translation>ფონტი</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="110"/>
+        <source>Cursor</source>
+        <translation>კურსორი</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="117"/>
+        <source>GTK Style</source>
+        <translation>GTK-ის სტილი</translation>
+    </message>
+</context>
+<context>
+    <name>StyleConfig</name>
+    <message>
+        <location filename="../styleconfig.ui" line="33"/>
+        <source>Widget Style</source>
+        <translation>ვიჯეტის სტილი</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="63"/>
+        <source>Qt Palette</source>
+        <translation>Qt-ის პალიტრა</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="77"/>
+        <source>Some Qt styles may ignore these colors.</source>
+        <translation>ზოგიერთმა Qt-ის სტილმა, შეიძლება, ეს ფერები გამოტოვოს.</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="102"/>
+        <source>Window:</source>
+        <translation>ფანჯარა:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="112"/>
+        <source>View:</source>
+        <translation>ხედი:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="122"/>
+        <source>Selection:</source>
+        <translation>არჩევანი:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="132"/>
+        <source>Tooltip:</source>
+        <translation>მინიშნება:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="146"/>
+        <source>Link:</source>
+        <translation>ბმული:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="166"/>
+        <source>Window Text:</source>
+        <translation>ფანჯრის ტექსტი:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="176"/>
+        <source>View Text:</source>
+        <translation>ტექსტის ნახვა:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="186"/>
+        <source>Selected Text:</source>
+        <translation>მონიშნული ტექსტი:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="196"/>
+        <source>Tooltip Text:</source>
+        <translation>მინიშნების ტექსტი:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="210"/>
+        <source>Visited Link:</source>
+        <translation>ნანახი ბმული:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="242"/>
+        <source>&amp;Save Palette</source>
+        <translation>პალიტრის &amp;შენახვა</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="249"/>
+        <source>&amp;Load Palette</source>
+        <translation>&amp;პალიტრის ჩატვირთვა</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="269"/>
+        <source>&amp;Default Palette</source>
+        <translation>&amp;ნაგულისხმევი პალიტრა</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="284"/>
+        <location filename="../styleconfig.ui" line="294"/>
+        <location filename="../styleconfig.ui" line="326"/>
+        <location filename="../styleconfig.ui" line="336"/>
+        <location filename="../styleconfig.ui" line="368"/>
+        <source>May be overridden by Qt style or app</source>
+        <translation>გადაიფარება Qt-ის სტილით ან აპით</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="287"/>
+        <source>Toolbar button style:</source>
+        <translation>ხელსაწყოების პანელის ღილაკის სტილი:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="298"/>
+        <source>Only display the icon</source>
+        <translation>მხოლოდ, ხატულის ჩვენება</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="303"/>
+        <source>Only display the text</source>
+        <translation>მხოლოდ ტექსტის ჩვენება</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="308"/>
+        <source>The text appears beside the icon</source>
+        <translation>ტექსტი ხატულის გვერდით გამოჩნდება</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="313"/>
+        <source>The text appears under the icon</source>
+        <translation>ხატულის ქვემოთ ტექსტი გამოჩნდება</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="318"/>
+        <source>Default</source>
+        <translation>ნაგულისხმევი</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="329"/>
+        <source>Toolbar icon size:</source>
+        <translation>ხელსაწყოების პანელის ხატულის ზომა:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="340"/>
+        <source>16</source>
+        <translation>16</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="345"/>
+        <source>22</source>
+        <translation>22</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="350"/>
+        <source>24</source>
+        <translation>24</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="355"/>
+        <source>32</source>
+        <translation>32</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="360"/>
+        <source>48</source>
+        <translation>48</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="371"/>
+        <source>Activate item on single click</source>
+        <translation>ელემენტის გააქტიურება ერთი წკაპით</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="51"/>
+        <source>Qt Style</source>
+        <translation>Qt-ის სტილი</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.cpp" line="330"/>
+        <location filename="../styleconfig.cpp" line="340"/>
+        <source>Save Palette</source>
+        <translation>პალიტრის შენახვა</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.cpp" line="330"/>
+        <source>Palette name:</source>
+        <translation>პალიტრის სახელი:</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.cpp" line="341"/>
+        <source>A palette with the same name exists.
+Do you want to replace it?</source>
+        <translation>პალიტრა ამ სახელით უკვე არსებობს.
+გნებავთ, ჩაანაცვლოთ ის?</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.cpp" line="484"/>
+        <source>Remove Palettes</source>
+        <translation>პალიტრების წაშლა</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.cpp" line="485"/>
+        <source>Do you really want to remove selected palette(s)?
+Root palettes will remain intact if existing.</source>
+        <translation>მართლა გნებავთ მონიშნული პალიტრების წაშლა?
+ძირითადი პალიტრები, თუ არსებობს, არ წაიშლება.</translation>
+    </message>
+</context>
+</TS>
---
+++ lxqt-config-file-associations/translations/lxqt-config-file-associations_ka.ts
@@ -0,0 +1,202 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>ApplicationChooser</name>
+    <message>
+        <location filename="../applicationchooser.ui" line="14"/>
+        <source>Application Chooser</source>
+        <translation>აპლიკაციის ამრჩევი</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="46"/>
+        <source>Pick an application for:</source>
+        <translation>აპლიკაციის არჩევა რისთვის:</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="110"/>
+        <source>1</source>
+        <translation>1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="115"/>
+        <source>Applications that handle JPEG</source>
+        <translation>აპლიკაცია, რომელსაც ესმის JPEG</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="124"/>
+        <location filename="../applicationchooser.ui" line="129"/>
+        <location filename="../applicationchooser.ui" line="139"/>
+        <location filename="../applicationchooser.ui" line="144"/>
+        <source>New Subitem</source>
+        <translation>ახალი ქვეელემენტი</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.ui" line="135"/>
+        <location filename="../applicationchooser.cpp" line="191"/>
+        <location filename="../applicationchooser.cpp" line="217"/>
+        <source>Other applications</source>
+        <translation>სხვა აპლიკაციები</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="147"/>
+        <source>Web browsers</source>
+        <translation>ბრაუზერები</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="152"/>
+        <source>Email clients</source>
+        <translation>ელფოსტის კლიენტები</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="157"/>
+        <source>File managers</source>
+        <translation>ფაილის მმართველები</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="193"/>
+        <location filename="../applicationchooser.cpp" line="219"/>
+        <source>Other applications that handle %1</source>
+        <translation>სხვა აპლიკაციები, რომლებსაც შეუძლიათ, გახსნან %1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="194"/>
+        <location filename="../applicationchooser.cpp" line="220"/>
+        <source>Applications that handle %1</source>
+        <translation>აპლიკაციები, რომლებსაც შეუძლიათ, გახსნან %1</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="305"/>
+        <source>No applications found</source>
+        <translation>აპლიკაციების გარეშე</translation>
+    </message>
+    <message>
+        <location filename="../applicationchooser.cpp" line="310"/>
+        <source>No more applications found</source>
+        <translation>მეტი აპლიკაცია აღმოჩენილი არაა</translation>
+    </message>
+</context>
+<context>
+    <name>MimetypeViewer</name>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="271"/>
+        <location filename="../mimetypeviewer.cpp" line="290"/>
+        <location filename="../mimetypeviewer.cpp" line="309"/>
+        <source>Change...</source>
+        <translation>შეცვლა...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="275"/>
+        <location filename="../mimetypeviewer.cpp" line="294"/>
+        <location filename="../mimetypeviewer.cpp" line="313"/>
+        <location filename="../mimetypeviewer.cpp" line="327"/>
+        <location filename="../mimetypeviewer.cpp" line="388"/>
+        <source>None</source>
+        <translation>არცერთი</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="276"/>
+        <location filename="../mimetypeviewer.cpp" line="295"/>
+        <location filename="../mimetypeviewer.cpp" line="314"/>
+        <source>Choose...</source>
+        <translation>არჩევა...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="384"/>
+        <source>&amp;Change...</source>
+        <translation>&amp;შეცვლა...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.cpp" line="389"/>
+        <source>&amp;Choose...</source>
+        <translation>&amp;არჩევა...</translation>
+    </message>
+</context>
+<context>
+    <name>mimetypeviewer</name>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="20"/>
+        <source>File Associations</source>
+        <translation>ფაილის ასოციაციები</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="30"/>
+        <source>Associations</source>
+        <translation>ასოციაციები</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="206"/>
+        <source>Default application</source>
+        <translation>ნაგულისხმევი აპლიკაცია</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="271"/>
+        <source>&amp;Choose...</source>
+        <translation>&amp;არჩევა...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="305"/>
+        <source>Default Applications</source>
+        <translation>ნაგულისხმევი აპლიკაციები</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="347"/>
+        <source>Web Browser</source>
+        <translation>ბრაუზერი</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="412"/>
+        <location filename="../mimetypeviewer.ui" line="487"/>
+        <location filename="../mimetypeviewer.ui" line="562"/>
+        <source>Choose...</source>
+        <translation>არჩევა...</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="422"/>
+        <source>Email Client</source>
+        <translation>ელფოსტის კლიენტი</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="497"/>
+        <source>File Manager</source>
+        <translation>ფაილების მმართველი</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="255"/>
+        <location filename="../mimetypeviewer.ui" line="396"/>
+        <location filename="../mimetypeviewer.ui" line="471"/>
+        <location filename="../mimetypeviewer.ui" line="546"/>
+        <source>None</source>
+        <translation>არცერთი</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="187"/>
+        <source>Patterns</source>
+        <translation>ნიმუშები</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="193"/>
+        <source>*.txt *.xml</source>
+        <translation>*.txt *.xml</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="146"/>
+        <source>MIME type</source>
+        <translation>MIME ტიპი</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="164"/>
+        <location filename="../mimetypeviewer.ui" line="248"/>
+        <location filename="../mimetypeviewer.ui" line="389"/>
+        <location filename="../mimetypeviewer.ui" line="464"/>
+        <location filename="../mimetypeviewer.ui" line="539"/>
+        <source>Icon</source>
+        <translation>ხატულა</translation>
+    </message>
+    <message>
+        <location filename="../mimetypeviewer.ui" line="171"/>
+        <source>Description</source>
+        <translation>აღწერა</translation>
+    </message>
+</context>
+</TS>
