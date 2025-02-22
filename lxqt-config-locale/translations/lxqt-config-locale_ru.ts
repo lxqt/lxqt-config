fcebec5759a4f405ed16fb66612455cd376856c1
Weblate <noreply@weblate.org>
2025-02-22 08:42:41 +0100

    Weblate commits
    Co-authored-by: Azasis <lionovdmitrij386@gmail.com>
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/pt/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-locale/ru/
    Translation: LXQt Configuration/lxqt-config-locale

--- lxqt-config-locale/translations/lxqt-config-locale_ru.ts
+++ lxqt-config-locale/translations/lxqt-config-locale_ru.ts
@@ -66,12 +66,12 @@
     <message>
         <location filename="../localeconfig.cpp" line="164"/>
         <source>Unset</source>
-        <translation type="unfinished"></translation>
+        <translation>Не задано</translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="226"/>
         <source>Current: </source>
-        <translation type="unfinished"></translation>
+        <translation>Текущий: </translation>
     </message>
     <message>
         <location filename="../localeconfig.cpp" line="370"/>
