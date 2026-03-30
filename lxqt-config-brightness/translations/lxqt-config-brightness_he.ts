a00b6ba210a174266b84e8f90a0f454d5c3f7d2b
Weblate <noreply@weblate.org>
2026-03-30 22:24:46 +0200

    Weblate commits
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: Yaron Shahrabani <sh.yaron@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-appearance/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-brightness/he/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-config-input/he/
    Translation: LXQt Configuration/lxqt-config-appearance
    Translation: LXQt Configuration/lxqt-config-brightness
    Translation: LXQt Configuration/lxqt-config-input

--- lxqt-config-brightness/translations/lxqt-config-brightness_he.ts
+++ lxqt-config-brightness/translations/lxqt-config-brightness_he.ts
@@ -42,7 +42,7 @@
     <message>
         <location filename="../brightnesssettings.cpp" line="54"/>
         <source>Currently unsupported under Wayland</source>
-        <translation type="unfinished"></translation>
+        <translation>לא נתמך כרגע תחת Wayland</translation>
     </message>
     <message>
         <location filename="../brightnesssettings.cpp" line="122"/>
